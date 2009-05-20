a296fc533a54159891e0a99b9d1c50fdfdbe0c57
Jasper Horrell <jasper@ska.ac.za>
2009-05-20 09:02:48 +0200

    changed to scan ra and dec
    git-svn-id: https://katfs.kat.ac.za/svnDS/ffinder@1312 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- print_ant_status.py
+++ print_ant_status.py
@@ -29,13 +29,13 @@ if __name__ == "__main__":
             scan = ant.sensor_scan_status.value
             desired_az = float(ant.sensor_pos_request_scan_azim.value)
             desired_el = float(ant.sensor_pos_request_scan_elev.value)
-            desired_ra = float(ant.sensor_pos_request_base_ra.value)
-            desired_dec = float(ant.sensor_pos_request_base_dec.value)
+            desired_ra = float(ant.sensor_pos_request_scan_ra.value)
+            desired_dec = float(ant.sensor_pos_request_scan_dec.value)
             actual_az = float(ant.sensor_pos_actual_scan_azim.value)
             actual_el = float(ant.sensor_pos_actual_scan_elev.value)
             error_az = abs(actual_az - desired_az)
             error_el = abs(actual_el - desired_el)
-            status = "\r%s: %s %s Mode:\033[34m%s\033[0m Scan:\033[34m%s\033[0m Lock:\033[34m%s\033[0m  Base[Ra:\033[32m%.2F\033[0m Dec:\033[32m%.2F\033[0m] Req[Az:\033[32m%.2F\033[0m El:\033[32m%.2F\033[0m]  Act[Az:\033[33m%.2F\033[0m El:\033[33m%.2F\033[0m]  Err[Az:\033[31m%.2F\033[0m El:\033[31m%.2F\033[0m]" % (opts.ant, state[period_count % 4], time.ctime().split(" ")[3], mode, scan, lock, desired_ra, desired_dec, desired_az, desired_el, actual_az, actual_el, error_az, error_el)
+            status = "\r%s: %s %s Mode:\033[34m%s\033[0m Scan:\033[34m%s\033[0m Lock:\033[34m%s\033[0m  Req[Ra:\033[32m%.2F\033[0m Dec:\033[32m%.2F\033[0m] Req[Az:\033[32m%.2F\033[0m El:\033[32m%.2F\033[0m]  Act[Az:\033[33m%.2F\033[0m El:\033[33m%.2F\033[0m]  Err[Az:\033[31m%.2F\033[0m El:\033[31m%.2F\033[0m]" % (opts.ant, state[period_count % 4], time.ctime().split(" ")[3], mode, scan, lock, desired_ra, desired_dec, desired_az, desired_el, actual_az, actual_el, error_az, error_el)
 
             sys.stdout.write(status)
             sys.stdout.flush()
