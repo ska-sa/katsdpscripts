09a818a6a364c4590285c2ae34d10031eb88b050
Jasper Horrell <jasper@ska.ac.za>
2009-06-22 13:01:00 +0200

    changed cross scans scripts to ant1
    git-svn-id: https://katfs.kat.ac.za/svnDS/ffinder@1855 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- source_cross_scans.py
+++ source_cross_scans.py
@@ -18,21 +18,21 @@ up_sources = ff.sources.iterfilter(tags="CAL",el_limit_deg=[20,55])
 
 for source in up_sources:
     print "Pointing Scan: ",source.name
-    ff.ant2.req_target(source.get_description())
+    ff.ant1.req_target(source.get_description())
      # send this target to the antenna. No time offset
-    ff.ant2.req_mode("POINT")
+    ff.ant1.req_mode("POINT")
      # set mode to point
-    ff.ant2.wait("lock","1",300)
+    ff.ant1.wait("lock","1",300)
      # wait for lock
     scan_count = 0
     
     for scan in scans:
         print "Scan Progress:",int((float(scan_count) / len(scans))*100),"%"
-        ff.ant2.req_scan_sym(scan[0],scan[1],scan_duration)
-        ff.ant2.wait("lock","1",300)
+        ff.ant1.req_scan_sym(scan[0],scan[1],scan_duration)
+        ff.ant1.wait("lock","1",300)
          # ready to start scan
-        ff.ant2.req_mode("SCAN")
-        ff.ant2.wait("scan_status","after",300)
+        ff.ant1.req_mode("SCAN")
+        ff.ant1.wait("scan_status","after",300)
          # wait for the scan to complete
         scan_count += 1
     print "Scan complete."
