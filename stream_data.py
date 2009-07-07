c26e932fcc2a555e35a4efdb2864a56f61de7eaa
Lize van den Heever <lvdheever@ska.ac.za>
2009-07-07 15:19:53 +0200

    Use dbe for dbe proxy instead of db
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@2120 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- stream_data.py
+++ stream_data.py
@@ -10,14 +10,14 @@ ff = ffui.tbuild("cfg-telescope.ini", "local-simulated-ff")
 ff.k7w.req_capture_start()
  # startup the k7 capture process
 
-ff.db.req_dbe_packet_count(5000)
+ff.dbe.req_dbe_packet_count(5000)
  # stream 5000 packets of data
-ff.db.req_dbe_rate(300)
+ff.dbe.req_dbe_rate(300)
  # stream data at 300 kbps. Approx 1s per integration
 
-ff.db.req_dbe_capture_destination("stream","127.0.0.1:7010")
+ff.dbe.req_dbe_capture_destination("stream","127.0.0.1:7010")
  # create a new data source labelled "stream". Send data to localhost on port 7010
-ff.db.req_dbe_capture_start("stream")
+ff.dbe.req_dbe_capture_start("stream")
  # start emitting data on stream "stream"
  
  # we leave the stream running and disconnect from the various devices
