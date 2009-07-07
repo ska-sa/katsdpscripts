32ccc4188ee873a74d6941a18d6dd8e77d4d14cc
Lize van den Heever <lvdheever@ska.ac.za>
2009-07-07 13:21:08 +0200

    Change to tbuild from cbuild with local-ant-only configuration
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@2093 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- canned_plot.py
+++ canned_plot.py
@@ -3,7 +3,7 @@
 
 import ffuilib as ffui
 
-ff = ffui.cbuild("ffuilib.ant_only.rc")
+ff = ffui.tbuild("cfg-telescope.ini","local-ant-only")
  # make fringe fingder connections
 
 ff.ant2.req_target_azel(20.31,30.45)
