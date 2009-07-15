2e1cd3f3519d4fc65226e55e2726f58afe1c35f3
Jasper Horrell <jasper@ska.ac.za>
2009-07-15 17:50:54 +0200

    updated data raster scan with various scan tags etc. Produces hdf5 file, but some issue since capture stop does not seem to stop the file increasing in size yet. Still need to get the augment to work. Moved code comments above code.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@2236 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- data_raster_scan.py
+++ data_raster_scan.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Raster scan across a simulated target
+# Raster scan across a simulated target producing scan data
 
 # Startup: 
 # start C dbe simulator: ~/svnDS/code/ffinder/trunk/src/simulators/dbe/dbe_server2 8000
@@ -11,51 +11,77 @@
 import ffuilib as ffui
 import time
 
+# make fringe finder connections
 ff = ffui.tbuild("cfg-user.ini", "local_ff_client_sim")
- # make fringe finder connections
 
 ff.dbesim.req_capture_stop()
 
 time.sleep(2)
 
+# write data into hdf5 file
+ff.k7w.req_write_hdf5(1)
+#ff.k7w.req_output_directory("~") # this does not work at present (defaults to current dir from which k7w is launched)
+
+# startup the k7 capture process
 ff.k7w.req_capture_start()
- # startup the k7 capture process
+ff.k7w.req_compound_scan_id(0)
+ff.k7w.req_compound_scan_tag("Raster scan of hard-coded dbe sim target at 20,30 az/el")
 
+# stream n packets of data
 ff.dbe.req_dbe_packet_count(50000)
- # stream 5000 packets of data
-ff.dbe.req_dbe_rate(300)
- # stream data at 300 kbps. Approx 1s per integration
-ff.dbe.req_dbe_capture_destination("stream","127.0.0.1:7010")
- # create a new data source labelled "stream". Send data to localhost on port 7010
-ff.dbe.req_dbe_capture_start("stream")
- # start emitting data on stream "stream"
 
+# stream data at 300 kbps. Approx 1s per integration
+ff.dbe.req_dbe_rate(300)
+
+# create a new data source labelled "stream". Send data to localhost on port 7010
+ff.dbe.req_dbe_capture_destination("stream","127.0.0.1:7010")
+
+# start emitting data on stream "stream"
+ff.dbe.req_dbe_capture_start("stream")
+
+# when the sensor value changes send an update to the listening function. Rate limited to 0.5 second updates.
 ff.ant1.sensor_pos_actual_scan_azim.register_listener(ff.dbesim.req_pointing_az, 0.5)
 ff.ant1.sensor_pos_actual_scan_elev.register_listener(ff.dbesim.req_pointing_el, 0.5)
- # when the sensor value changes send an update to the listening function. Rate limited to 0.5 second updates.
 
-scans = [ (2,2) , (2,1) , (2,0) , (2,-1) , (2,-2)]
- # raster scan
+scans = [ (2,2) , (-2,1) , (2,0) , (-2,-1) , (2,-2)]
+#scans = [ (2,2) ]
+
+# raster scan
+
 scan_duration = 60
+#scan_duration = 20
 
+# send this target to the antenna. No time offset (hardcoded target in DBE sim at 20 az,30 el)
 ff.ant1.req_target_azel(20,30)
-  # send this target to the antenna. No time offset (hardcoded target in DBE sim at 20 az,30 el)
+
+# set mode to point
 ff.ant1.req_mode("POINT")
- # set mode to point
 ff.ant1.wait("lock",True,300)
 scan_count =  0
 
 for scan in scans:
     print "Scan Progress:",int((float(scan_count) / len(scans))*100),"%"
+    ff.k7w.req_scan_id(scan_count)
+    ff.k7w.req_scan_tag("scan ", str(scan_count), " of raster scan")
     ff.ant1.req_scan_asym(-scan[0],scan[1],scan[0],scan[1],scan_duration)
+
+    # ready to start scan
     ff.ant1.wait("lock",True,300)
-     # ready to start scan
     ff.ant1.req_mode("SCAN")
+
+    # wait for the scan to complete
     ff.ant1.wait("scan_status","after",300)
-     # wait for the scan to complete
     scan_count += 1
 print "Scan complete."
 
+#stop capture
+ff.dbesim.req_dbe_capture_stop()
+ff.k7w.req_capture_stop()            
+
 ff.disconnect()
 
+# now augment the hdf5 file with metadata (TODO) 
+# ~/svnDS/code/ffinder/trunk/src/streaming/k7augment/augment.py -h
+
+# load into scape (TODO)....
 
