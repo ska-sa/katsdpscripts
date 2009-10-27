8ae38b3a8b5a8d6a9de145b3e827b07165b76901
Jasper Horrell <jasper@ska.ac.za>
2009-10-27 12:42:17 +0200

    adding new script to check system defaults and reset system to the defaults, if requested. Currently geared towards the integration lab needs and only a few sensors are currently of interest. Highlights in colour where there are differences between current value and default value.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@3548 b56c7a7d-ac47-0410-a905-b6f7de78896a

---
+++ check_defaults.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# check the system against the expecte default values and optionally reset to these defaults.
+
+import ffuilib as ffui
+from optparse import OptionParser
+from ansi import col
+import time
+
+defaults = [ # structure is list of tuples with (sensor, default value, command to set default)
+("ff.rfe.sensor.rfe31_psu_on", 1, "ff.rfe.req.rfe3_psu_on('rfe31',1)"),
+("ff.rfe.sensor.rfe31_rf15_stage1_lna_psu_on", 1, "ff.rfe.req.rfe3_rf15_stage1_lna_psu_on('rfe31',1)"),
+("ff.rfe.sensor.rfe51_attenuator_horizontal", 5.0, "ff.rfe.req.rfe5_attenuation('rfe51','h',5.0)"),
+("ff.rfe.sensor.rfe51_attenuator_vertical", 5.0, "ff.rfe.req.rfe5_attenuation('rfe51','v',5.0)"),
+("ff.rfe.sensor.rfe7_downconverter_ant1_h_attenuation", 5.5, "ff.rfe.req.rfe7_downconverter_attenuation('1','h',5.5)" ),
+("ff.rfe.sensor.rfe7_downconverter_ant1_v_attenuation", 5.5, "ff.rfe.req.rfe7_downconverter_attenuation('1','v',5.5)" ),
+]
+
+def check_sensors(ff):
+    # check current system setting and compare with defaults as specified above
+    print "%s %s %s" % ("Sensor".ljust(55), "Current Value".ljust(25),"Default Value".ljust(25))
+    current_vals = []
+    for i in range(len(defaults)):
+        current_vals.append(str(eval(defaults[i][0]+".get_value()")))
+        if current_vals[i] <> str(defaults[i][1]):
+            print "%s %s %s" % (col("red")+str(defaults[i][0]).ljust(55),str(current_vals[i]).ljust(25),str(defaults[i][1]).ljust(25)+col("black"))
+        else:
+            print "%s %s %s" % (str(defaults[i][0]).ljust(55),str(current_vals[i]).ljust(25),str(defaults[i][1]).ljust(25))
+    return
+
+def reset_defaults(ff):
+    # reset system to default setting as specified by commands above
+    for i in range(len(defaults)):
+        eval(defaults[i][2])
+    return
+
+if __name__ == "__main__":
+
+    usage = "usage: %prog [options]"
+    description = "check the system against the expecte default values and optionally reset to these defaults."
+    parser = OptionParser(usage=usage)
+
+    parser.add_option('-i', '--ini_file', dest='ini_file', type="string", default="cfg-user-lab.ini", metavar='INI',
+                      help='Telescope configuration file to use in conf directory (default="%default")')
+    parser.add_option('-s', '--selected_config', dest='selected_config', type="string", default="integration_client_ff", metavar='SELECTED',
+                      help='Selected configuration to use (default="%default")')
+    parser.add_option('-r', '--reset', dest='reset', action='store_true',default=False,
+                      help='Reset system to default values, if include this switch (default="%default")')
+    (opts, args) = parser.parse_args()
+
+try:
+    ff = ffui.tbuild(opts.ini_file, opts.selected_config)
+
+    print "Checking current settings....."
+    check_sensors(ff)
+
+    if opts.reset:
+        print "\nResetting to default settings..."
+        reset_defaults(ff)
+        print "\nRechecking settings...."
+        time.sleep(1.5) # wait a little time for sensor to update
+        check_sensors(ff)
+
+except Exception, e:
+    print "Exception: ", e
+    print 'Exception caught: attempting to exit cleanly...'
+finally:
+    ff.disconnect()
+
+
