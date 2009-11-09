70aaa609f332d21abdae83994b1ba40d3eaa9f6e
Lize van den Heever <lvdheever@ska.ac.za>
2009-11-09 14:02:38 +0200

    Authentication
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@3700 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- checkout_ffops_nq.sh
+++ checkout_ffops_nq.sh
@@ -2,45 +2,45 @@
 echo Checkout scripts
 mkdir -p ~/scripts
 cd ~/scripts
-svn co --no_auth_cache https://katfs.kat.ac.za/svnDS/code/ffinder/trunk/src/test/scripts .
+svn co --username readonly --password svnds\ fooz3r https://katfs.kat.ac.za/svnDS/code/ffinder/trunk/src/test/scripts .
 
 mkdir -p ~/svn
 [ "$FFSVN" ] || FFSVN="~/svn"
 echo "Getting local configuration files"
 mkdir -p /var/kat/conf
 cd /var/kat/conf
-svn co --no_auth_cache https://katfs.kat.ac.za/svnDS/code/conf/ff .
+svn co --username readonly --password svnds\ fooz3er https://katfs.kat.ac.za/svnDS/code/conf/ff .
 
 echo "Getting katcp libraries"
 mkdir -p $FFSVN/katcp
 cd $FFSVN/katcp
-svn co https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk .
-svn co https://katfs.kat.ac.za/svnROACH/sw/lib/katcp .
+svn co --username readonly --password svnds\ fooz3er https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk .
+svn co --username readonly --password svnds\ fooz3r https://katfs.kat.ac.za/svnROACH/sw/lib/katcp .
 
 echo "Getting scape"
 mkdir -p $FFSVN/scape
 cd $FFSVN/scape
-svn co https://katfs.kat.ac.za/svnDS/code/scape/trunk .
+svn co --username readonly --password svnds\ fooz3r https://katfs.kat.ac.za/svnDS/code/scape/trunk .
 
 echo "Getting katcore libraries"
 mkdir -p $FFSVN/katcore
 cd $FFSVN/katcore
-svn co https://katfs.kat.ac.za/svnDS/code/katcore/trunk .
+svn co --username readonly --password svnds\ fooz3r https://katfs.kat.ac.za/svnDS/code/katcore/trunk .
 
 echo "Getting katpoint library"
 mkdir -p $FFSVN/katpoint
 cd $FFSVN/katpoint
-svn co https://katfs.kat.ac.za/svnDS/code/katpoint/trunk .
+svn co --username readonly --password svnds\ fooz3r https://katfs.kat.ac.za/svnDS/code/katpoint/trunk .
 
 echo "Getting katconf library"
 mkdir -p $FFSVN/katconf
 cd $FFSVN/katconf
-svn co https://katfs.kat.ac.za/svnDS/code/katconf/trunk .
+svn co --username readonly --password svnds\ fooz3r https://katfs.kat.ac.za/svnDS/code/katconf/trunk .
 
 echo "Getting fringe finder libraries"
 cd $FFSVN/ffinder
 mkdir -p $FFSVN/ffinder
-svn co https://katfs.kat.ac.za/svnDS/code/ffinder/trunk .
+svn co --username readonly --password svnds\ fooz3er https://katfs.kat.ac.za/svnDS/code/ffinder/trunk .
 
 cd $FFSVN
 ls -la
