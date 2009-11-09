3e3ae67dcfda89edf03774ffd82297bf814a4d75
Lize van den Heever <lvdheever@ska.ac.za>
2009-11-09 12:56:14 +0200

    Minor change
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@3696 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- checkout_ffops_nq.sh
+++ checkout_ffops_nq.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-mkdir /home/ffuser/scripts
+mkdir -p /home/ffuser/scripts
 cd scripts
 svn co https://katfs.kat.ac.za/svnDS/code/ffinder/trunk/src/test/scripts .
 
-mkdir /home/ffuser/svn
+mkdir -p /home/ffuser/svn
 cd svn
 [ "$FFSVN" ] || FFSVN="/home/ffuser/svn"
 echo "Getting local configuration files"
-mkdir /var/kat/conf
+mkdir -p /var/kat/conf
 cd /var/kat/conf
 svn co https://katfs.kat.ac.za/svnDS/code/conf/ff .
 
 echo "Getting katcp libraries"
-mkdir $FFSVN/katcp
+mkdir -p $FFSVN/katcp
 cd $FFSVN/katcp
 svn co https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk .
 svn co https://katfs.kat.ac.za/svnROACH/sw/lib/katcp .
 
 echo "Getting scape"
-mkdir $FFSVN/scape
+mkdir -p $FFSVN/scape
 cd $FFSVN/scape
 svn co https://katfs.kat.ac.za/svnDS/code/scape/trunk .
 
 echo "Getting katcore libraries"
-mkdir $FFSVN/katcore
+mkdir -p $FFSVN/katcore
 cd $FFSVN/katcore
 svn co https://katfs.kat.ac.za/svnDS/code/katcore/trunk .
 
 echo "Getting katpoint library"
-mkdir $FFSVN/katpoint
+mkdir -p $FFSVN/katpoint
 cd $FFSVN/katpoint
 svn co https://katfs.kat.ac.za/svnDS/code/katpoint/trunk .
 
 echo "Getting katconf library"
-mkdir $FFSVN/katconf
+mkdir -p $FFSVN/katconf
 cd $FFSVN/katconf
 svn co https://katfs.kat.ac.za/svnDS/code/katconf/trunk .
 
 echo "Getting fringe finder libraries"
 cd $FFSVN/ffinder
-mkdir $FFSVN/ffinder
+mkdir -p $FFSVN/ffinder
 svn co https://katfs.kat.ac.za/svnDS/code/ffinder/trunk .
 
 cd $FFSVN
