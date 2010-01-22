90016fd74c130472dc3a38c87cc6db6c1646421e
Jasper Horrell <jasper@ska.ac.za>
2010-01-22 11:13:03 +0200

    beefed up and cleaned up the track_sources.py script a little so as to be more readily available to amuse tourists when a busload arrives :)
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@4320 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- track_sources.py
+++ track_sources.py
@@ -1,23 +1,58 @@
 #!/usr/bin/python
-# Track sources all around the sky
+# Track sources all around the sky (mostly to keep tourists or antennas amused)
 
-import ffuilib as ffui
+import ffuilib
+from optparse import OptionParser
+import sys
 import time
 
-# creates connection to two antennas and provides access to the catalogue
-ff = ffui.tbuild("cfg-local.ini", "local_ant_only")
+# Parse command-line options that allow the defaults to be overridden
+# Default FF configuration is *local*, to prevent inadvertent use of the real hardware
+parser = OptionParser(usage="usage: %prog [options]")
+parser.add_option('-i', '--ini_file', dest='ini_file', type="string", default="cfg-local.ini", metavar='INI',
+                  help='Telescope configuration file to use in conf directory (default="%default")')
+parser.add_option('-s', '--selected_config', dest='selected_config', type="string", default="local_ff", metavar='SELECTED',
+                  help='Selected configuration to use (default="%default")')
+parser.add_option('-a', '--ants', dest='ants', type="string", metavar='ANTS',
+                  help="Comma-separated list of antennas to include in scan (e.g. 'ant1,ant2')," +
+                       " or 'all' for all antennas - this MUST be specified (safety reasons)")
+(opts, args) = parser.parse_args()
+
+# Force antennas to be specified to sensitise the user to what will physically move
+if opts.ants is None:
+    print 'Please specify the antennas to use via -a option (yes, this is a non-optional option..., -h for help)'
+    sys.exit(1)
+
+# Build Fringe Finder configuration, as specified in user-facing config file
+# This connects to all the proxies and devices and queries their commands and sensors
+ff = ffuilib.tbuild(opts.ini_file, opts.selected_config)
+
+# Create a list of the specified antenna devices, and complain if they are not found
+if opts.ants.strip() == 'all':
+    ants = ff.ants
+else:
+    try:
+        ants = ffuilib.Array('ants', [getattr(ff, ant_x.strip()) for ant_x in opts.ants.split(",")])
+    except AttributeError:
+        raise ValueError("Antenna '%s' not found" % ant_x)
 
 # time to stay on each target (secs)
 on_target_duration = 10
 
 # set the drive strategy for how antenna moves between targets
 # (options are: "longest-track", the default, or "shortest-slew")
-ff.ant1.req.drive_strategy("shortest-slew")
+
+ants.req.drive_strategy("shortest-slew")
 
 # get sources from catalogue that are in specified elevation range. Antenna will get
 # as close as possible to targets which are out of drivable range.
-# Note: This returns an interator which recalculates the el limits each time a new object is requested
-up_sources = ff.sources.iterfilter(el_limit_deg=[0,90])
+cat = ff.sources
+# remove some very strong sources
+cat.remove('Sun')
+cat.remove('AFRISTAR')
+
+# Get an interator which recalculates the el limits each time a new object is requested
+up_sources = cat.iterfilter(el_limit_deg=[3,89])
 
 total_target_count = 0
 targets_tracked = 0
@@ -28,12 +63,15 @@ try:
         print "Target to track: ",source.name
 
         # send this target to the antenna.
-        ff.ant1.req.target(source.description)
-        ff.ant1.req.mode("POINT")
+        ants.req.target(source.description)
+        ants.req.mode("POINT")
 
-        # wait for lock
-        target_locked = ff.ant1.wait("lock","1",200)
-        if target_locked:
+        # wait for antennas to lock onto target
+        locks = 0
+        for ant_x in ants.devs:
+            if ant_x.wait("lock", True, 300): locks += 1
+
+        if len(ants.devs) == locks:
             targets_tracked += 1
             # continue tracking the target for specified time
             time.sleep(on_target_duration)
@@ -51,5 +89,5 @@ finally:
 
     # exit
     print "setting drive-strategy back to the default"
-    ff.ant1.req.drive_strategy("longest-track") # good practice
+    ants.req.drive_strategy("longest-track") # set back to the default
     ff.disconnect()
