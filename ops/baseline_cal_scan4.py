e59b349f8512c50d1c29397219d98f933854ebfa
Ludwig Schwardt <ludwig@ska.ac.za>
2010-02-04 17:15:56 +0200

    Segregated sources in good and great list, and use both for now. Add a try-finally clause to ensure the capture process shuts down cleanly. Added a loop around the catalogue iterator, so that the experiment keeps running for a minimum duration. Disentangled the printing code and made it aware of track duration when picking sources.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@4438 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- ops/baseline_cal_scan4.py
+++ ops/baseline_cal_scan4.py
@@ -25,23 +25,34 @@ if opts.ini_file is None or opts.selected_config is None:
 # This connects to all the proxies and devices and queries their commands and sensors
 ff = ffuilib.tbuild(opts.ini_file, opts.selected_config)
 
+# Load catalogue of point sources
 cat = katpoint.Catalogue(file('/var/kat/conf/source_list.csv'), add_specials=False, antenna=ff.sources.antenna)
-good_sources = ['Cygnus A', '3C123', 'Taurus A', 'Orion A', 'Hydra A', '3C273', 'Virgo A', 'Centaurus A', 'Hercules A', 'Sagittarius A', '3C353', 'J1819-6345', '3C279', '3C286','3C295','J2253+1608','Pictoris A','Fornax A','J0522-3627','Cassiopeia A','3C161','3C48','3C147']
-good_cat = katpoint.Catalogue([cat[src] for src in good_sources], add_specials=False, antenna=cat.antenna)
-
-if not opts.printonly:
-    ffobserve.setup(ff, ff.ants, centre_freq=1800.0)
+# Prune the catalogue to only contain sources that are good for baseline calibration
+great_sources = ['3C123', 'Taurus A', 'Orion A', 'Hydra A', '3C273', 'Virgo A', 'Centaurus A', 'Pictoris A']
+good_sources =  ['3C48', '3C84', 'J0408-6545', 'J0522-3627', '3C161', 'J1819-6345', 'J1939-6342', '3C433', 'J2253+1608']
+good_cat = katpoint.Catalogue([cat[src] for src in great_sources + good_sources], add_specials=False, antenna=cat.antenna)
 
 compscan_id = 0
-for target in good_cat.iterfilter(el_limit_deg=5):
-    if opts.printonly:
-        print "At compscan_id", compscan_id, "will observe",target
-    else:
-        ffobserve.track(ff, ff.ants, target.description, duration=120.0, compscan_id=compscan_id, drive_strategy='longest-track')
-    compscan_id += 1
+start_time = katpoint.Timestamp()
 
-if not opts.printonly:
-    ffobserve.shutdown(ff)
+if opts.printonly:
+    # Only list targets that will be visited
+    for target in good_cat.iterfilter(el_limit_deg=5, timestamp=start_time):
+        print "Compound scan", compscan_id, "will observe", target.name
+        compscan_id += 1
+        start_time += 120.0
+else:
+    try:
+        # The real experiment
+        ffobserve.setup(ff, ff.ants, centre_freq=1800.0)
 
-ff.disconnect()
+        # Observe at least 1.5 hours
+        while katpoint.Timestamp() - start_time < 4500.0:
+            for target in good_cat.iterfilter(el_limit_deg=5):
+                ffobserve.track(ff, ff.ants, target, duration=120.0, compscan_id=compscan_id, drive_strategy='longest-track')
+                compscan_id += 1
 
+    finally:
+        # ALWAYS run shutdown, even on an error or Keyboard Interrupt, as your hard drive will fill up otherwise...
+        ffobserve.shutdown(ff)
+        ff.disconnect()
