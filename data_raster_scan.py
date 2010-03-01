81244d0cb07d9f41c9cebe176d30859d62192e64
Jasper Horrell <jasper@ska.ac.za>
2010-03-01 13:35:06 +0200

    major revamp of this script using mostly cut-and-paste lower level functionality from observe.py
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@4654 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- data_raster_scan.py
+++ data_raster_scan.py
@@ -1,138 +1,140 @@
 #!/usr/bin/python
-# Raster scan across a simulated target producing scan data for signal displays and loading into scape
+# Raster scan across a simulated target producing scan data for signal displays and loading into scape.
+# This script is basically a cut and paste from some of the underlying functionality provided by
+# katuilb's observe.py in order to show how scripts can also be created using the lower-level
+# functionality if more explicit control is needed. Usually, it is much simpler to use observe.py
+# (see simscan.py script, for example, which does much the same as this script).
 
-# Once-off setup:
-# For the augment part to work, need some webserver setup from cmd line:
-#   cd /Library/WebServer/Documents/
-#   sudo ln -s /var/kat/central_monitoring/ central_monitoring
-#   sudo apachectl start
-# ensure hat you have a data dir e.g. /var/kat/data below
-
-# Startup in different terminals:
-# start C dbe simulator: ~/svnDS/code/ffinder/trunk/src/simulators/dbe/dbe_server 8000
-# start k7 writer: cd /var/kat/data; ~/svnDS/code/ffinder/trunk/src/streaming/k7writer/k7w_server 8001
-# start the system: kat-launch.py
-# start the central monitor: ~/svnDS/code/ffinder/trunk/src/services/monitoring/central_monitor.py
-# and signal display server: ~/svnDS/code/ffinder/trunk/src/streaming/sdisp/ffsocket.py
-# (kat-launch.py and ffsocket.py use the defaults: -i cfg-local.ini -s local_ff)
-
-import katuilib as katui
+import katuilib
 import time
+import uuid
+import numpy as np
 from optparse import OptionParser
 
+usage = "usage: %prog [options]"
+parser = OptionParser(usage=usage)
 
+parser.add_option('-i', '--ini_file', dest='ini_file', type="string", default="cfg-local.ini", metavar='INI',
+                  help='Telescope configuration file to use in conf directory (default="%default")')
+parser.add_option('-s', '--selected_config', dest='selected_config', type="string", default="local_ff", metavar='SELECTED',
+                  help='Selected configuration to use (default="%default")')
+parser.add_option('-a', '--ants', dest='ants', type="string", default="ant1,ant2", metavar='ANTS',
+                  help='Comma separated list of antennas to include in scan (default="%default")')
+(opts, args) = parser.parse_args()
 
-if __name__ == "__main__":
+experiment_id = str(uuid.uuid1()) # generate a unique experiment ID
+observer = 'nobody'
+description = 'data raster scan using simulated target'
+centre_freq=1800.0
+dump_rate=1.0
+target = 'Takreem,azel,20,30' # if change, also update in line kat.dbe.req.dbe_test_target()
+num_scans=3
+scan_duration=20.0
+scan_extent=4.0
+scan_spacing=0.5
+scan_in_azimuth=True
+drive_strategy='shortest-slew'
+label='raster'
 
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage=usage)
+built_kat = False
 
-    parser.add_option('-i', '--ini_file', dest='ini_file', type="string", default="cfg-local.ini", metavar='INI',
-                      help='Telescope configuration file to use in conf directory (default="%default")')
-    parser.add_option('-s', '--selected_config', dest='selected_config', type="string", default="local_ff", metavar='SELECTED',
-                      help='Selected configuration to use (default="%default")')
-    parser.add_option('-a', '--ants', dest='ants', type="string", default="ant1,ant2", metavar='ANTS',
-                      help='Comma seperated list of antennas to include in scan (default="%default")')
-    (opts, args) = parser.parse_args()
+try:
+    kat = katuilib.tbuild(opts.ini_file, opts.selected_config)
+    built_kat = True
 
-
-    kat = katui.tbuild(opts.ini_file, opts.selected_config)
-    # make fringe fingder connections
-
-    tgt = 'Takreem,azel,20,30'
-    # the virtual target that the dbe simulator contains
-
-    ants = []
-    print "====================Including antennas - ",
-    for ant_x in opts.ants.split(","):
-        ant = eval("kat."+ant_x)
-        ants.append(ant)
-        print ant.name,
-    print
-
-    kat.dbe.req.dbe_capture_stop()
-    for ant_x in ants:
-        ant_x.req.mode("STOP")
-    time.sleep(0.5)
-    # cleanup any existing experiment
+    # create an array using the specified antennas
+    ants = katuilib.Array("ants", [getattr(kat, ant.strip()) for ant in opts.ants.split(',')])
 
     # Start with a clean state, by stopping the DBE
     kat.dbe.req.capture_stop()
 
+    # Set centre frequency in RFE stage 7
+    kat.rfe7.req.rfe7_lo1_frequency(4200.0 + centre_freq, 'MHz')
+    effective_lo_freq = (centre_freq - 200.0) * 1e6
+
     # Set data output directory (typically on ff-dc machine)
     kat.dbe.req.k7w_output_directory("/var/kat/data")
-    # Tell k7_writer to write the selected baselines to HDF5 files
-    baselines = [1, 2, 3]
-    kat.dbe.req.k7w_baseline_mask(*baselines)
+    # Enable output to HDF5 file, and set basic experimental info
     kat.dbe.req.k7w_write_hdf5(1)
+    kat.dbe.req.k7w_experiment_info(experiment_id, observer, description)
 
     # This is a precaution to prevent bad timestamps from the correlator
     kat.dbe.req.dbe_sync_now()
-    # The DBE proxy needs to know the dump rate as well as the effective LO freq, which is used for fringe stopping (eventually)
-    dump_rate = 1.0
-    centre_freq = 1800
-    effective_lo_freq = (centre_freq - 200.0) * 1e6
+    # The DBE proxy needs to know the dump period (in ms) as well as the effective LO freq,
+    # which is used for fringe stopping (eventually). This sets the delay model and other
+    # correlator parameters, such as the dump rate, and instructs the correlator to pass
+    # its data to the k7writer daemon (set via configuration)
     kat.dbe.req.capture_setup(1000.0 / dump_rate, effective_lo_freq)
-    kat.dbe.req.capture_start()
 
-    scans = [ (-2,0.5) , (2,0) , (-2,-0.5) ]
-    # raster scan
-    scan_duration = 240
+    print "New data capturing session"
+    print "--------------------------"
+    print "Experiment ID =", experiment_id
+    print "Observer =", observer
+    print "Description ='%s'" % description
+    print "RF centre frequency = %g MHz, dump rate = %g Hz" % (centre_freq, dump_rate)
 
-    for ant_x in ants:
-        ant_x.req.target(tgt)
-        # send this target to the antenna. No time offset
-        ant_x.req.mode("POINT")
-        # set mode to point
+    # Set the drive strategy for how antenna moves between targets
+    ants.req.drive_strategy(drive_strategy)
+    # Set the antenna target
+    ants.req.target(target)
+    # Provide target to the DBE proxy, which will use it as delay-tracking center
+    kat.dbe.req.target(target)
 
-    for ant_x in ants:
-        ant_x.wait("lock",True,300)
+    # tell the dbe sim to make a test target at specified az and el
+    kat.dbe.req.dbe_test_target(20,30,100)
 
-    try:
-        kat.dbe.req.k7w_compound_scan_id(1)
-    except RuntimeError, err:
-        print "  k7w compund scan failed (", err, " )"
+    # tell the dbe simulator where the antenna is so that is can generate target flux at the right time
+    kat.ant1.sensor.pos_actual_scan_azim.register_listener(kat.dbe.req.dbe_pointing_az, 0.5)
+    kat.ant1.sensor.pos_actual_scan_elev.register_listener(kat.dbe.req.dbe_pointing_el, 0.5)
 
+    # Create new CompoundScan group in HDF5 file, which automatically also creates a Scan group with label 'slew'
+    kat.dbe.req.k7w_new_compound_scan(target, label, 'slew')
 
-    # once we are on the target begin a new compound scan
-    # (compound scan 0 will be the slew to the target, default scan tag is "slew")
-    scan_count = 1
+    # Create start positions of each scan, based on scan parameters
+    scan_steps = np.arange(-(num_scans // 2), num_scans // 2 + 1)
+    scanning_coord = (scan_extent / 2.0) * (-1) ** scan_steps
+    stepping_coord = scan_spacing * scan_steps
+    # These minus signs ensure that the first scan always starts at the top left of target
+    scan_starts = zip(scanning_coord, -stepping_coord) if scan_in_azimuth else zip(stepping_coord, -scanning_coord)
 
-    for scan in scans:
-        print "Scan Progress:",int((float(scan_count) / len(scans)*2)*100),"%"
-        for ant_x in ants:
-            ant_x.req.scan_asym(-scan[0],scan[1],scan[0],scan[1],scan_duration)
-        for ant_x in ants:
-            ant_x.wait("lock",True,300)
+    # Iterate through the scans across the target
+    for scan_count, scan in enumerate(scan_starts):
 
-        kat.dbe.req.k7w_scan_id(scan_count,"scan")
-        # mark this section as valid scan data
-        for ant_x in ants:
-            ant_x.req.mode("SCAN")
-        for ant_x in ants:
-            ant_x.wait("scan_status","after",300)
-        kat.dbe.req.k7w_scan_id(scan_count+1,"slew")
-        # slewing to next raster pointg
-        scan_count += 2
-    print "Scan complete."
+        print "Slewing to start of scan %d of %d on target '%s'" % (scan_count + 1, len(scan_starts),target)
+        # Create a new Scan group in HDF5 file, with 'slew' label (not necessary the first time)
+        if scan_count > 0:
+            kat.dbe.req.k7w_new_scan('slew')
+        # If we haven't yet, start recording data from the correlator
+        if kat.dbe.sensor.capturing.get_value() == '0':
+            kat.dbe.req.capture_start()
+        # Move each antenna to the start position of the next scan
+        if scan_in_azimuth:
+            ants.req.scan_asym(scan[0], scan[1], -scan[0], scan[1], scan_duration)
+        else:
+            ants.req.scan_asym(scan[0], scan[1], scan[0], -scan[1], scan_duration)
+        ants.req.mode('POINT')
+        # Wait until they are all in position (with 5 minute timeout)
+        ants.wait('lock', True, 300)
 
+        print "Starting scan %d of %d on target '%s'" % (scan_count + 1, len(scan_starts), target)
+        # Start a new Scan group in the HDF5 file, this time labelled as a proper 'scan'
+        kat.dbe.req.k7w_new_scan('scan')
+        # Start scanning the antennas
+        ants.req.mode('SCAN')
+        # Wait until they are all finished scanning (with 5 minute timeout)
+        ants.wait('scan_status', 'after', 300)
+
+    # Obtain the names of the files currently being written to
     files = kat.dbe.req.k7w_get_current_files(tuple=True)[1][2]
-    print "Data captured to",files
-    time.sleep(2)
-    kat.dbe.req.capture_stop()
-    kat.disconnect()
+    print 'Scans complete, data captured to', files
 
-# now augment the hdf5 file with metadata (pointing info etc):
-#   ~/svnDS/code/ffinder/trunk/src/streaming/k7augment/augment.py -d /var/kat/data -f [xxx.h5]
+except Exception, e:
+    print "Exception: ", e
+    print 'Exception caught: attempting to exit cleanly...'
+finally:
+    if built_kat:
+        # Stop the DBE data flow (this indirectly stops k7writer via a stop packet, which then closes the HDF5 file)
+        kat.dbe.req.capture_stop()
+        print 'Ended data capturing session with experiment ID', experiment_id
 
-# load into scape from within python:
-#   import scape
-#   import pylab as pl
-#   d = scape.DataSet("[xxx.h5]") # load data from file into dataset
-#   print d
-#   scape.plot_compound_scan_on_target(d.compscans[1])
-#   pl.show()
-#   d = d.select(labelkeep="scan") # get rid of the slew data from the dataset
-#   print d
-#   print d.compscans[0]
-#   scape.plot_compound_scan_on_target(d.compscans[0])
+        kat.disconnect()
