986714bfc64d69df710fcc25133ccfb2af09b89c
Ludwig Schwardt <ludwig@ska.ac.za>
2010-03-31 21:35:55 +0200

    Celebrated script used for ministerial visit.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@5002 b56c7a7d-ac47-0410-a905-b6f7de78896a

---
+++ point_to_12m.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# Silly script to point 4 antennas at the 12-m container
+
+import katuilib
+
+kat = katuilib.tbuild('cfg-karoo.ini', 'karoo_ff_4ants')
+
+print "Stowing all dishes"
+kat.ants.req.target_azel(0.0, 90.0)
+kat.ants.req.mode('POINT')
+kat.ants.wait('lock', True, 300)
+
+print "Pointing dishes at 12-m container"
+kat.ant1.req.target_azel(145., 2.)
+kat.ant2.req.target_azel(-145., 2.)
+kat.ant3.req.target_azel(146., 2.)
+kat.ant4.req.target_azel(136., 2.)
+
+kat.ants.req.mode('POINT')
