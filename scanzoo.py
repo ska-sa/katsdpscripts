886947fc9ae1165ce0d0fd9233596441f853f75c
Ludwig Schwardt <ludwig@ska.ac.za>
2010-10-05 11:00:44 +0200

    Updated standard scripts to use the new helper functions in observe.py. This shortens these scripts considerably, while also providing all scripts with all standard options (such as dump rate and noise diode regime). All noise diode firings are now done implicitly via nd_params. Also shortened the way command-line options are passed to CaptureSession via the vars() builtin function.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@7009 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scanzoo.py
+++ scanzoo.py
@@ -7,14 +7,13 @@ import katuilib
 
 target = 'Takreem,azel,45,10'
 
-with katuilib.tbuild('systems/local.conf') as kat:
+with katuilib.tbuild() as kat:
 
     # Tell the DBE sim to make a test target at specfied az and el and with specified flux
     kat.dbe.req.dbe_test_target(45, 10, 100)
     nd_params = {'diode' : 'coupler', 'on_duration' : 3.0, 'off_duration' : 3.0, 'period' : 40.}
 
-    with katuilib.CaptureSession(kat, 'id', 'nobody', 'The scan zoo', kat.ants,
-                                 record_slews=True, nd_params=nd_params) as session:
+    with katuilib.CaptureSession(kat, 'id', 'nobody', 'The scan zoo', kat.ants, 1822., 1., True, nd_params) as session:
         session.track(target, duration=5.0)
         session.fire_noise_diode('coupler', 5.0, 5.0)
         session.scan(target, duration=20.0)
