cb7d4fe8a6b8c8a301767b0e90fd5c029e3ac43d
Ludwig Schwardt <ludwig@ska.ac.za>
2011-01-10 16:36:24 +0200

    Now removes any skipped sources from the point source list before starting the observation, instead of skipping them inside the loop. If all sources happen to be skipped, this would otherwise cause the script to hang. Now quits the script when there are no sources to observe (either because they are all skipped or below the horizon). This also prevents long stalls in the script while it waits for a source to rise. Added an option to specify the horizon in degrees of elevation.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@8424 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- point_source_scan.py
+++ point_source_scan.py
@@ -4,32 +4,33 @@
 # The *with* keyword is standard in Python 2.6, but has to be explicitly imported in Python 2.5
 from __future__ import with_statement
 
-import datetime
-import os
-import time
+import os.path
 from cStringIO import StringIO
+import datetime
+import time
 
 from katuilib.observe import standard_script_options, verify_and_connect, CaptureSession, TimeSession, user_logger
 import katpoint
 
 # Set up standard script options
 parser = standard_script_options(usage="%prog [options] [<catalogue files>]",
-                                 description="Perform mini (Zorro) raster scans across (point) sources for pointing \
-                                              model fits and gain curve calculation. Use the specified catalogue(s) \
-                                              or the default. This script is aimed at fast scans across a large range \
-                                              of sources. Some options are **required**.")
+                                 description="Perform mini (Zorro) raster scans across (point) sources for pointing "
+                                             "model fits and gain curve calculation. Use the specified catalogue(s) "
+                                             "or the default. This script is aimed at fast scans across a large range "
+                                             "of sources. Some options are **required**.")
 # Add experiment-specific options
 parser.add_option('-e', '--scan_in_elevation', action="store_true", default=False,
                   help="Scan in elevation rather than in azimuth (default=%default)")
 parser.add_option('-m', '--min_time', type="float", default=-1.0,
                   help="Minimum duration to run experiment, in seconds (default=one loop through sources)")
 # Set default value for any option (both standard and experiment-specific options)
-parser.add_option('-z', '--skip-catalogue', dest='skip_catalogue', type="string", default=None,
-                  help="Name of file containing catalogue of targets to skip (default is not to skip any targets).")
-parser.add_option('--source-strength', dest='source_strength', type="choice", default="auto",
-                  choices=['strong', 'weak', 'auto'],
-                  help="Select scanning strategy based on strength of sources. Options are 'strong' or 'weak' or 'auto'. Auto is based "
-                       "on flux density specified in catalogue")
+parser.add_option('-z', '--skip-catalogue',
+                  help="Name of file containing catalogue of targets to skip (updated with observed targets). "
+                       "The default is not to skip any targets.")
+parser.add_option('--source-strength', type="choice", default="auto", choices=['strong', 'weak', 'auto'],
+                  help="Select scanning strategy based on strength of sources. Options are 'strong' or 'weak' or 'auto'."
+                       "Auto is based on flux density specified in catalogue.")
+parser.add_option('--horizon', type="float", default=5.0, help="Horizon limit in degrees (default=%default)")
 parser.set_defaults(description='Point source scan')
 # Parse the command line
 opts, args = parser.parse_args()
@@ -44,46 +45,57 @@ with verify_and_connect(opts) as kat:
     else:
         # Default catalogue contains the radec sources in the standard kat database
         pointing_sources = kat.sources.filter(tags='radec')
+    user_logger.info("Loaded point source catalogue with %d targets" % (len(pointing_sources.targets),))
 
-    skip_sources = katpoint.Catalogue(add_specials=False, antenna=kat.sources.antenna)
+    # Remove sources in skip catalogue file, if provided
     if opts.skip_catalogue is not None and os.path.exists(opts.skip_catalogue):
-        skip_sources.add(file(opts.skip_catalogue))
+        skip_sources = katpoint.Catalogue(file(opts.skip_catalogue))
+        for target in skip_sources:
+            pointing_sources.remove(target.name)
+        user_logger.info("After skipping, %d targets are left" % (len(pointing_sources.targets),))
 
-    if opts.skip_catalogue is not None and not opts.dry_run:
-        skip_file = file(opts.skip_catalogue, "a")
+    # Quit early if there are no sources to observe
+    if len(pointing_sources.targets) == 0:
+        user_logger.warning("Empty point source catalogue or all targets are skipped")
+    elif len(pointing_sources.filter(el_limit_deg=opts.horizon).targets) == 0:
+        user_logger.warning("No targets in catalogue are currently visible - please re-run the script later")
     else:
-        skip_file = StringIO()
+        # Observed targets will be written back to catalogue file, or into the void
+        skip_file = file(opts.skip_catalogue, "a") if opts.skip_catalogue is not None and not opts.dry_run else StringIO()
 
-    # Select either a CaptureSession for the real experiment, or a fake TimeSession
-    Session = TimeSession if opts.dry_run else CaptureSession
-    with Session(kat, **vars(opts)) as session:
-        session.standard_setup(**vars(opts))
-        start_time = time.time()
-        targets_observed = []
-        # Keep going until the time is up
-        keep_going = True
-        skip_file.write("# Record of targets observed on %s by %s\n" % (datetime.datetime.now(), opts.observer))
-        while keep_going:
-            # Iterate through source list, picking the next one that is up
-            for target in pointing_sources.iterfilter(el_limit_deg=5):
-                # Do different raster scan on strong and weak targets
-                if target.name in skip_sources:
-                    continue
-                if (opts.source_strength == 'strong' or
-                    (opts.source_strength == 'auto' and target.flux_density(opts.centre_freq) > 25.0)):
-                    session.raster_scan(target, num_scans=5, scan_duration=30, scan_extent=6.0,
-                                        scan_spacing=0.25, scan_in_azimuth=not opts.scan_in_elevation)
-                else:
-                    session.raster_scan(target, num_scans=5, scan_duration=60, scan_extent=4.0,
-                                        scan_spacing=0.25, scan_in_azimuth=not opts.scan_in_elevation)
-                targets_observed.append(target.name)
-                skip_file.write(target.description + "\n")
-                # The default is to do only one iteration through source list
-                if opts.min_time <= 0.0:
+        # Select either a CaptureSession for the real experiment, or a fake TimeSession
+        Session = TimeSession if opts.dry_run else CaptureSession
+        with Session(kat, **vars(opts)) as session:
+            session.standard_setup(**vars(opts))
+            start_time = time.time()
+            targets_observed = []
+            # Keep going until the time is up
+            keep_going = True
+            skip_file.write("# Record of targets observed on %s by %s\n" % (datetime.datetime.now(), opts.observer))
+            while keep_going:
+                targets_before_loop = len(targets_observed)
+                # Iterate through source list, picking the next one that is up
+                for target in pointing_sources.iterfilter(el_limit_deg=opts.horizon):
+                    # Do different raster scan on strong and weak targets
+                    if (opts.source_strength == 'strong' or
+                        (opts.source_strength == 'auto' and target.flux_density(opts.centre_freq) > 25.0)):
+                        session.raster_scan(target, num_scans=5, scan_duration=30, scan_extent=6.0,
+                                            scan_spacing=0.25, scan_in_azimuth=not opts.scan_in_elevation)
+                    else:
+                        session.raster_scan(target, num_scans=5, scan_duration=60, scan_extent=4.0,
+                                            scan_spacing=0.25, scan_in_azimuth=not opts.scan_in_elevation)
+                    targets_observed.append(target.name)
+                    skip_file.write(target.description + "\n")
+                    # The default is to do only one iteration through source list
+                    if opts.min_time <= 0.0:
+                        keep_going = False
+                    # If the time is up, stop immediately
+                    elif time.time() - start_time >= opts.min_time:
+                        keep_going = False
+                        break
+                if len(targets_observed) == targets_before_loop:
+                    user_logger.warning("No targets are currently visible - stopping script instead of hanging around")
                     keep_going = False
-                # If the time is up, stop immediately
-                elif time.time() - start_time >= opts.min_time:
-                    keep_going = False
-                    break
-        user_logger.info("Targets observed : %d (%d unique)" % (len(targets_observed), len(set(targets_observed))))
+            user_logger.info("Targets observed : %d (%d unique)" % (len(targets_observed), len(set(targets_observed))))
+
         skip_file.close()
