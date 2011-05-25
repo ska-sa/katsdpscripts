95f00d249d61b8218b3b8e10d753d9c2e81a4781
Ludwig Schwardt <ludwig@ska.ac.za>
2011-05-25 16:01:13 +0200

    Major reshuffle of scripts folder. Separated scripts in observation, reduction, unmaintained and holography. Unmaintained scripts typically do not work on the latest FF system. Also deleted some really old and silly scripts. Removed old noise diode models, as these are now in the config system.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/ffinder/trunk@9693 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- start_to_finish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Capturing data and moving antennas and ends with a reduced beamfitted plot in scape :)
-
-echo "Running raster scan..."
-~/scripts/simscan.py
-echo "Staging data file..."
-/usr/local/bin/augment4.py
-echo "Reducing raster scan..."
-~/scripts/raster_scan_reduction.py
