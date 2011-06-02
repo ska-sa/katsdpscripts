91615b664e730fe3f7bb5eb9c67b8e3adff08cfc
Ludwig Schwardt <ludwig@ska.ac.za>
2011-06-02 16:32:21 +0200

    Half-baked upgrade to KAT-7, passed on to simonr.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@9886 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- observation/auto_attenuate.py
+++ observation/auto_attenuate.py
@@ -17,47 +17,66 @@ from katuilib.observe import verify_and_connect, ant_array, lookup_targets, user
 from katuilib import colors
 import katpoint
 
+def ant_pedestal(kat, ant_name):
+    """Pedestal device associated with antenna device."""
+    # This assumes that antenna names have the format 'antx', where x is an integer (the antenna number)
+    ant_num = int(ant_name.strip()[3:])
+    return getattr(kat, 'ped%d' % (ant_num,))
+
 ###################### RFE Stage 5 getters and setters ########################
 
 # RFE5 minimum attenuation, maximum attenuation, finest resolution to which attenuation can be set, all in dB
 rfe5_min_att, rfe5_max_att, rfe5_att_step = 0.0, 63.5, 0.5
 rfe5_power_range = 2.
 
-def get_rfe5_input_power(kat, ant_num, pol):
-    pedestal = getattr(kat, 'ped%d' % (ant_num,))
-    sensor = getattr(pedestal.sensor, 'rfe5_%s_power_in' % ('horizontal' if pol == 'h' else 'vertical',))
+def get_rfe5_input_power(kat, ant_name, pol):
+    ped = ant_pedestal(kat, ant_name)
+    sensor = getattr(ped.sensor, 'rfe5_%s_power_in' % ('horizontal' if pol == 'h' else 'vertical',))
     return sensor.get_value()
 
-def get_rfe5_attenuation(kat, ant_num, pol):
-    pedestal = getattr(kat, 'ped%d' % (ant_num,))
-    sensor = getattr(pedestal.sensor, 'rfe5_attenuator_%s' % ('horizontal' if pol == 'h' else 'vertical',))
+def get_rfe5_attenuation(kat, ant_name, pol):
+    ped = ant_pedestal(kat, ant_name)
+    sensor = getattr(ped.sensor, 'rfe5_attenuator_%s' % ('horizontal' if pol == 'h' else 'vertical',))
     return sensor.get_value()
 
-def set_rfe5_attenuation(kat, ant_num, pol, value):
-    pedestal = getattr(kat, 'ped%d' % (ant_num,))
-    pedestal.req.rfe5_attenuation(pol, value)
+def set_rfe5_attenuation(kat, ant_name, pol, value):
+    ped = ant_pedestal(kat, ant_name)
+    ped.req.rfe5_attenuation(pol, value)
 
-def get_rfe5_output_power(kat, ant_num, pol):
-    pedestal = getattr(kat, 'ped%d' % (ant_num,))
-    sensor = getattr(pedestal.sensor, 'rfe5_%s_power_out' % ('horizontal' if pol == 'h' else 'vertical',))
+def get_rfe5_output_power(kat, ant_name, pol):
+    ped = ant_pedestal(kat, ant_name)
+    sensor = getattr(ped.sensor, 'rfe5_%s_power_out' % ('horizontal' if pol == 'h' else 'vertical',))
     return sensor.get_value()
 
-################### RFE Stage 7 / DBE getters and setters #####################
+###################### RFE Stage 7 getters and setters ########################
 
 # RFE7 minimum attenuation, maximum attenuation, finest resolution to which attenuation can be set, all in dB
 rfe7_min_att, rfe7_max_att, rfe7_att_step = 0.0, 31.5, 0.5
+
+def get_rfe7_attenuation(kat, ant_name, pol):
+    sensor = getattr(kat.rfe7.sensor, 'rfe7_downconverter_%s_%s_attenuation' % (ant_name, pol))
+    return sensor.get_value()
+
+def set_rfe7_attenuation(kat, ant_name, pol, value):
+    # This assumes that antenna names have the format 'antx', where x is an integer (the antenna number)
+    ant_num = int(ant_name.strip()[3:])
+    kat.rfe7.req.rfe7_downconverter_attenuation(str(ant_num), pol, value)
+
+################################ DBE getters ##################################
+
 snapshot_length = 8192
 connected_antpols = {}
 
-def get_rfe7_attenuation(kat, ant_num, pol):
-    sensor = getattr(kat.rfe7.sensor, 'rfe7_downconverter_ant%d_%s_attenuation' % (ant_num, pol))
-    return sensor.get_value()
+def get_ff_dbe_input_power(kat, ant_name, pol):
+    dbe_input = connected_antpols['%s, %s' % (ant_name, pol)]
+    PinFS_dBm, nbits = 0, 8 # 0 dBm for noise into iADC (J. Manley), may be different for KATADC
+    fullscale = 2 ** (nbits - 1)
+    gainADC_dB = 20 * np.log10(fullscale) - PinFS_dBm # Scale factor from dBm to numbers
+    voltage_samples = kat.dh.get_snapshot('adc', dbe_input)
+    return 10 * np.log10(np.var(voltage_samples)) - gainADC_dB
 
-def set_rfe7_attenuation(kat, ant_num, pol, value):
-    kat.rfe7.req.rfe7_downconverter_attenuation(str(ant_num), pol, value)
-
-def get_dbe_input_power(kat, ant_num, pol):
-    dbe_input = connected_antpols['ant%d, %s' % (ant_num, pol)]
+def get_kat7_dbe_input_power(kat, ant_name, pol):
+    dbe_input = ant_name + pol.upper()
     PinFS_dBm, nbits = 0, 8 # 0 dBm for noise into iADC (J. Manley), may be different for KATADC
     fullscale = 2 ** (nbits - 1)
     gainADC_dB = 20 * np.log10(fullscale) - PinFS_dBm # Scale factor from dBm to numbers
@@ -66,12 +85,12 @@ def get_dbe_input_power(kat, ant_num, pol):
 
 ####################### Generic iterative adjustment ##########################
 
-def adjust(kat, ant_num, pol, get_att, set_att, get_power, desired_power, min_att, max_att, att_step, wait=0.):
+def adjust(kat, ant_name, pol, get_att, set_att, get_power, desired_power, min_att, max_att, att_step, wait=0.):
     """Iteratively adjust attenuator to move power towards desired value."""
     # This should converge in a few iterations, else stop anyway
     for n in range(5):
-        att = get_att(kat, ant_num, pol)
-        power = get_power(kat, ant_num, pol)
+        att = get_att(kat, ant_name, pol)
+        power = get_power(kat, ant_name, pol)
         # The difference between actual and desired power is roughly the extra attenuation needed
         new_att = att + power - desired_power
         # Round desired attenuation to the nearest allowed one
@@ -81,13 +100,13 @@ def adjust(kat, ant_num, pol, get_att, set_att, get_power, desired_power, min_at
         # Stop once attenuator value has settled
         if new_att == att:
             break
-        set_att(kat, ant_num, pol, new_att)
+        set_att(kat, ant_name, pol, new_att)
         # Optionally wait until power has stabilised before making the next measurement
         time.sleep(wait)
         # Obtain latest measurements on the last iteration
         if n == 4:
-            att = get_att(kat, ant_num, pol)
-            power = get_power(kat, ant_num, pol)
+            att = get_att(kat, ant_name, pol)
+            power = get_power(kat, ant_name, pol)
     return att, power
 
 ############################### Main script ###################################
@@ -107,6 +126,8 @@ parser.add_option('--rfe5-desired', type='float', dest='rfe5_desired_power', def
                   help='Desired RFE5 output power, in dBm (default=%default)')
 parser.add_option('-d', '--dbe-desired', type='float', dest='dbe_desired_power', default=-27.0,
                   help='Desired DBE input power, in dBm (default=%default)')
+parser.add_option('--dbe', default='dbe7', help="DBE proxy / correlator to use for experiment "
+                                                "('dbe' for FF and 'dbe7' for KAT-7, default=%default)")
 # Parse the command line
 opts, args = parser.parse_args()
 # The standard option verification routine below requires this option (otherwise ignored)
@@ -114,14 +135,20 @@ opts.observer = 'Otto Attenuator'
 
 with verify_and_connect(opts) as kat:
 
+    selected_dbe = getattr(kat, opts.dbe)
+
     # Populate lookup table that maps ant+pol to DBE input
-    for dbe_input_sensor in [sensor for sensor in vars(kat.dbe.sensor) if sensor.startswith('input_mappings_')]:
+    for dbe_input_sensor in [sensor for sensor in vars(selected_dbe.sensor) if sensor.startswith('input_mappings_')]:
         ant_pol = getattr(kat.dbe.sensor, dbe_input_sensor).get_value()
         connected_antpols[ant_pol] = dbe_input_sensor[15:]
 
     # Create device array of antennas, based on specification string
     ants = ant_array(kat, opts.ants)
     user_logger.info('Using antennas: %s' % (' '.join([dev.name for dev in ants.devs]),))
+
+    # Switch data handler to requested DBE (but first remember current one)
+    old_dbe = kat.dh.dbe
+    kat.dh.register_dbe(selected_dbe)
 
     # If centre frequency is specified, set it accordingly
     if opts.centre_freq:
@@ -143,11 +170,9 @@ with verify_and_connect(opts) as kat:
     user_logger.info('Desired:      | RFE5 | %-4.1f | RFE7 | %-4.1f | DBE |' %
                      (opts.rfe5_desired_power, opts.dbe_desired_power))
     for ant in ants.devs:
-        # This assumes that antenna names have the format 'antx', where x is an integer (the antenna number)
-        ant_num = int(ant.name.strip()[3:])
         for pol in ('h', 'v'):
-            if 'ant%d, %s' % (ant_num, pol) not in connected_antpols:
-                user_logger.info('ant%d %s: not connected to DBE' % (ant_num, pol.upper()))
+            if '%s, %s' % (ant.name, pol) not in connected_antpols:
+                user_logger.info('%s %s: not connected to DBE' % (ant.name, pol.upper()))
                 continue
             rfe5_in = get_rfe5_input_power(kat, ant_num, pol)
             # Adjust RFE stage 5 attenuation to give desired output power (short waits required to stabilise power)
