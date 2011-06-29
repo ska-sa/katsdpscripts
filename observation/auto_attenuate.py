2e8506ec24a373caae4b45bc8bbde2fc82bc4144
Jasper Horrell <jasper@ska.ac.za>
2011-06-29 16:56:17 +0200

    minor syntax change
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@10228 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- observation/auto_attenuate.py
+++ observation/auto_attenuate.py
@@ -189,7 +189,7 @@ with verify_and_connect(opts) as kat:
     if opts.centre_freq:
         kat.rfe7.req.rfe7_lo1_frequency(4200.0 + opts.centre_freq, 'MHz')
         time.sleep(1.0)
-        user_logger.info('New centre frequency to: %s MHz' % (kat.rfe7.sensor.rfe7_lo1_frequency.get_value() / 1e6 - 4200.0,))
+        user_logger.info('Updated centre frequency: %s MHz' % (kat.rfe7.sensor.rfe7_lo1_frequency.get_value() / 1e6 - 4200.0,))
 
     # Move all antennas onto calibration source and wait for lock
     try:
