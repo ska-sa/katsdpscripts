2afa885e11c73ada97277231cc253f7f210548ba
Sean Passmoor <sean@ska.ac.za>
2011-07-12 16:20:50 +0200

    (sean,thomas) Added a elevation spacings option to tipping_curve.py default=1 degree and a option to scan up then down in elevation with option tip-both-directions
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@10354 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- observation/tipping_curve.py
+++ observation/tipping_curve.py
@@ -5,7 +5,7 @@
 from __future__ import with_statement
 
 from katuilib.observe import standard_script_options, verify_and_connect, start_session, user_logger
-
+import numpy as np
 # Set up standard script options
 parser = standard_script_options(usage="%prog [options]",
                                  description="Perform tipping curve scan for a specified azimuth position. \
@@ -13,6 +13,12 @@ parser = standard_script_options(usage="%prog [options]",
 # Add experiment-specific options
 parser.add_option('-z', '--az', type="float", default=168.0,
                   help='Azimuth angle along which to do tipping curve, in degrees (default="%default")')
+
+parser.add_option('--spacing', type="float", default=1.0,
+                  help='The Spacing along the elevation axis of the tipping curve that measuremnts are taken, in degrees (default="%default")')
+parser.add_option( '--tip-both-directions', action="store_true" , default=False,
+                  help='Do tipping curve from low to high elevation and then from high to low elevation')
+
 # Set default value for any option (both standard and experiment-specific options)
 parser.set_defaults(description='Tipping curve')
 # Parse the command line
@@ -21,7 +27,7 @@ opts, args = parser.parse_args()
 # Ensure that azimuth is in valid physical range of -185 to 275 degrees
 if (opts.az < -185.) or (opts.az > 275.):
     opts.az = (opts.az + 180.) % 360. - 180.
-
+user_logger.info("Tipping Curve at Azimuth=%f"%(opts.az,))
 with verify_and_connect(opts) as kat:
 
     with start_session(kat, **vars(opts)) as session:
@@ -29,6 +35,9 @@ with verify_and_connect(opts) as kat:
         session.capture_start()
 
         # Iterate through elevation angles
-        for el in [10, 12.5, 15, 17.5,  20, 22.5, 25, 27.5, 30, 32.5, 35, 37.5, 40, 42.5, 45, 47.5,  50, 52.5,  55, 57.5,  60, 62.5, 65, 67.5, 70, 72.5, 75, 77.5, 80, 82.5, 85, 87.5, 90]:
+        spacings = list(np.arange(10.0,90.1,opts.spacing))
+        if opts.tip_both_directions :
+            spacings += list(np.arange(90.0,9.,-opts.spacing))
+        for el in spacings:
             session.track('azel, %f, %f' % (opts.az, el), duration=15.0)
             session.fire_noise_diode('coupler', on=10, off=10)
