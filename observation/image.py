5eb859be8ce5db8fab3bcd1b13490cd1bf83511f
Ludwig Schwardt <ludwig@ska.ac.za>
2011-09-20 14:47:40 +0200

    Incorporate (infrequent) visits to the bandpass calibrator into the imaging script. Added a maximum experiment duration. The noise diode is now disabled by default (for now). This version of the script was used to produce the first PKS 1610-60 and Carina images.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@10997 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- observation/image.py
+++ observation/image.py
@@ -1,60 +1,67 @@
 #!/usr/bin/python
-# Track target and calibrator(s) for imaging.
+# Track target and calibrators for imaging.
 
 # The *with* keyword is standard in Python 2.6, but has to be explicitly imported in Python 2.5
 from __future__ import with_statement
 
+import time
 from katuilib.observe import standard_script_options, verify_and_connect, lookup_targets, start_session, user_logger
-import katpoint
 
 # Set up standard script options
-parser = standard_script_options(usage="%prog [options] <'target'> <'calsource'> [<'calsource'>*]",
-                                 description="Perform an imaging run of a specified target. " +
-                                             "A target and one or more cal sources are specified.")
+parser = standard_script_options(usage="%prog [options] <'target'> <'bandpass calibrator'> <'gain calibrator'> [<extra gain cals>*]",
+                                 description="Perform an imaging run of a specified target, visiting the bandpass " +
+                                             "and gain calibrators along the way.")
 # Add experiment-specific options
 parser.add_option('-t', '--target_duration', type='int', default=5*60,
                   help='Duration to track the imaging target per visit, in integer secs (default="%default")')
-parser.add_option('-c', '--cal_duration', type='int', default=1*60,
-                  help='Duration to track each cal source per visit, in integer secs (default="%default")')
+parser.add_option('-b', '--bpcal_duration', type='int', default=5*60,
+                  help='Duration to track bandpass calibrator per visit, in integer secs (default="%default")')
+parser.add_option('-i', '--bpcal_interval', type='int', default=60*60,
+                  help='Interval between bandpass calibrator visits, in integer secs (default="%default")')
+parser.add_option('-g', '--gaincal_duration', type='int', default=1*60,
+                  help='Duration to track gain calibrator per visit, in integer secs (default="%default")')
+parser.add_option('-m', '--max-duration', type='float', default=None,
+                  help='Maximum duration of script, in secs (default is no limit)')
+
 # Set default value for any option (both standard and experiment-specific options)
-parser.set_defaults(description='Imaging run.')
+parser.set_defaults(description='Imaging run.', nd_params='coupler,0,0,-1')
 # Parse the command line
 opts, args = parser.parse_args()
 
 # Check options and arguments, and build KAT configuration, connecting to proxies and devices
-if len(args) < 2:
-    raise ValueError("Please specify at least a target and one cal source")
+if len(args) < 3:
+    raise ValueError("Please specify the target, bandpass calibrator and at least one gain calibrator")
 
 with verify_and_connect(opts) as kat:
 
     targets = lookup_targets(kat, args)
-    target = targets[0]
-    target = target if isinstance(target, katpoint.Target) else katpoint.Target(target)
-    calibrators = targets[1:]
-    user_logger.info("Imaging target is '%s'" % (target.description,))
-    user_logger.info("Calibrator(s) are %s" % (calibrators,))
-    full_duration = opts.target_duration + len(calibrators) * opts.cal_duration
-
+    target, bpcal, gaincals = targets[0], targets[1], targets[2:]
+    user_logger.info("Imaging target is '%s'" % (target,))
+    user_logger.info("Bandpass calibrator is '%s'" % (bpcal,))
+    user_logger.info("Gain calibrators are [%s]" % (', '.join([("'%s'" % (gaincal,)) for gaincal in gaincals]),))
+    
     with start_session(kat, **vars(opts)) as session:
         session.standard_setup(**vars(opts))
         session.capture_start()
+        time_till_bpcal = opts.bpcal_interval
+        start_time = time.time()
 
         while True:
-            # Visit each calibrator
-            for cal in calibrators:
-                session.track(cal, duration=opts.cal_duration, label='cal_source')
+            if time_till_bpcal >= opts.bpcal_interval:
+                session.track(bpcal, duration=opts.bpcal_duration, label='bandpass_cal')
+                time_till_bpcal = opts.bpcal_duration
+            for gaincal in gaincals:
+                session.track(gaincal, duration=opts.gaincal_duration, label='gain_cal')
+                time_till_bpcal += opts.gaincal_duration
 
-            user_logger.info("Initiating %g-second track on target '%s'" % (opts.target_duration, target.name))
-            if not session.target_visible(target, full_duration, horizon=10.):
-                user_logger.warning("Stopping experiment, as imaging target will be below horizon")
+            if opts.max_duration and (time.time() > start_time + opts.max_duration):
+                user_logger.warning("Stopping experiment, as maximum script duration was exceeded")
                 break
 
-            # Split the total track on one target into segments lasting as long as the noise diode period
-            # This ensures the maximum number of noise diode firings
-            total_track_time = 0.
-            while total_track_time < opts.target_duration:
-                next_track = opts.target_duration - total_track_time
-                if opts.nd_params['period'] > 0:
-                    next_track = min(next_track, opts.nd_params['period'])
-                session.track(target, duration=next_track, label='', announce=False)
-                total_track_time += next_track
+            if not session.target_visible(target, opts.target_duration, horizon=5.) or \
+               not session.target_visible(gaincal, opts.target_duration + len(gaincals) * opts.gaincal_duration, horizon=5.):
+                user_logger.warning("Stopping experiment, as imaging target or following gain cal(s) will be below horizon")
+                break
+
+            session.track(target, duration=opts.target_duration, label='target')
+            time_till_bpcal += opts.target_duration
