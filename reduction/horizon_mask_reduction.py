affa0e0d50a3ea29fd689cdf43ca7ed12b827b76
Tshakule Nemalili <tnemalili@ska.ac.za>
2012-10-09 12:59:49 +0200

    modified to keep scape running to the infinity
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@15441 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- reduction/horizon_mask_reduction.py
+++ reduction/horizon_mask_reduction.py
@@ -56,99 +56,95 @@ def plot_horizon(xyz_data, xyz_titles, az_lims, el_lims, pow_lims):
     if pow_title:
         plt.title(pow_title)
 
-
-#def main():
-# Parse command-line options and arguments
-parser = optparse.OptionParser(usage='%prog [options] <data file> [<data file> ...]',
+def main():
+    # Parse command-line options and arguments
+    parser = optparse.OptionParser(usage='%prog [options] <data file> [<data file> ...]',
                                    description='Display a horizon mask from a set of data files.')
-parser.add_option('-a', '--baseline', dest='baseline', type="string", metavar='BASELINE', default='A1A1',
+    parser.add_option('-a', '--baseline', dest='baseline', type="string", metavar='BASELINE', default='A1A1',
                       help="Baseline to load (e.g. 'A1A1' for antenna 1), default is first single-dish baseline in file")
-parser.add_option('-o', '--output', dest='output', type="string", metavar='OUTPUTFILE', default=None,
+    parser.add_option('-o', '--output', dest='output', type="string", metavar='OUTPUTFILE', default=None,
                       help="Write out intermediate h5 file")
-parser.add_option('-s', '--split', dest='split', action="store_true", metavar='SPLIT', default=False,
+    parser.add_option('-s', '--split', dest='split', action="store_true", metavar='SPLIT', default=False,
                       help="Whether to split each horizon plot in half")
-parser.add_option('-z', '--azshift', dest='azshift', type='float', metavar='AZIMUTH_SHIFT', default=45.0,
+    parser.add_option('-z', '--azshift', dest='azshift', type='float', metavar='AZIMUTH_SHIFT', default=45.0,
                       help="Degrees to rotate azimuth window by.")
 
-(opts, args) = parser.parse_args()
+    (opts, args) = parser.parse_args()
 
 # Check arguments
-if len(args) < 1:
-    print 'Please specify the data file to reduce'
-    sys.exit(1)
+    if len(args) < 1:
+        print 'Please specify the data file to reduce'
+        sys.exit(1)
 
-# Load data set
-combined = None
-for filename in args:
-    print 'Loading baseline', opts.baseline, 'from data file', filename
-    d = scape.DataSet(filename, baseline=opts.baseline)
+    # Load data set
+    combined = None
+    for filename in args:
+        print 'Loading baseline', opts.baseline, 'from data file', filename
+        d = scape.DataSet(filename, baseline=opts.baseline)
 
-    if len(d.freqs) > 1:
-        # Only keep main scans (discard slew and cal scans) and restrict frequency band to Fringe Finder band
-        d = d.select(labelkeep='scan', freqkeep=range(90, 425))
+        if len(d.freqs) > 1:
+            # Only keep main scans (discard slew and cal scans) and restrict frequency band to Fringe Finder band
+            d = d.select(labelkeep='scan', freqkeep=range(90, 425))
+            # Average all frequency channels into one band
+            d.average()
 
-        # Average all frequency channels into one band
-        d.average()
+        if combined is None:
+            combined = d
+        else:
+            combined.scans.extend(d.scans)
+            combined.compscans.extend(d.compscans)
 
-    if combined is None:
-        combined = d
+    if not combined.scans:
+        print 'No scans found. Did you specify a data file?'
+        sys.exit(1)
+
+    if opts.output is not None:
+        combined.save(opts.output)
+
+    # Extract azimuth and elevation angle from (azel) target associated with scan, in degrees
+    azimuth, elevation, power_hh_db, power_vv_db = [], [], [], []
+    for s in d.scans:
+        #azimuth.extend(scape.stats.angle_wrap(rad2deg(s.pointing['az']) + opts.azshift, period=360.0) - opts.azshift)
+        azimuth.extend(rad2deg(s.pointing['az']))
+        elevation.extend(rad2deg(s.pointing['el']))
+        power_hh_db.extend(10.0 * np.log10(s.pol('HH')[:,0]))
+        power_vv_db.extend(10.0 * np.log10(s.pol('VV')[:,0]))
+    assert len(azimuth) == len(elevation) == len(power_hh_db), "sizes don't match"
+    print "Contour plotting horizon from %d points ..." % len(azimuth)
+
+    # Calculate and plot tipping curves
+    #plt.figure(1)
+    #plt.clf()
+    #plt.subplots_adjust(hspace=0.5)
+
+    hh_data = (azimuth, elevation, power_hh_db)
+    hh_titles = ('Azimuth (deg)', 'Elevation (deg)', 'Power (dB) for %s HH' % (opts.baseline,))
+    vv_data = (azimuth, elevation, power_vv_db)
+    vv_titles = ('Azimuth (deg)', 'Elevation (deg)', 'Power (dB) for %s VV' % (opts.baseline,))
+    az_max, az_min = np.ceil(max(azimuth)), np.floor(min(azimuth))
+    el_max, el_min = np.ceil(max(elevation)), np.floor(min(elevation))
+    pow_max, pow_min = max([max(power_hh_db), max(power_vv_db)]), min([min(power_hh_db), min(power_vv_db)])
+    az_mid = (az_max + az_min) / 2.0
+    el_mid = (el_max + el_min)/2.0
+
+    if opts.split:
+        plt.subplot(4, 1, 1)
+        plot_horizon(hh_data, hh_titles, (az_min, az_mid), (el_min, el_max), (pow_min, pow_max))
+        plt.subplot(4, 1, 2)
+        plot_horizon(hh_data, hh_titles, (az_mid, az_max), (el_min, el_max), (pow_min, pow_max))
+        plt.subplot(4, 1, 3)
+        plot_horizon(vv_data, vv_titles, (az_min, az_mid), (el_min, el_max), (pow_min, pow_max))
+        plt.subplot(4, 1, 4)
+        plot_horizon(vv_data, vv_titles, (az_mid, az_max), (el_min, el_max), (pow_min, pow_max))
     else:
-        combined.scans.extend(d.scans)
-        combined.compscans.extend(d.compscans)
+        plt.subplot(2, 1, 1)
+        plot_horizon(vv_data, vv_titles, (az_min, az_max), (el_min, el_max), (pow_min, pow_max))
+        plt.subplot(2, 1, 2 )
+        plot_horizon(hh_data, hh_titles, (az_min, az_max), (el_min, el_max), (pow_min, pow_max))
 
-if not combined.scans:
-    print 'No scans found. Did you specify a data file?'
-    sys.exit(1)
+    # Display plots - this should be called ONLY ONCE, at the VERY END of the script
+    # The script stops here until you close the plots...
+    plt.show()
 
-if opts.output is not None:
-    combined.save(opts.output)
-
-# Extract azimuth and elevation angle from (azel) target associated with scan, in degrees
-azimuth, elevation, power_hh_db, power_vv_db = [], [], [], []
-for s in d.scans:
-    azimuth.extend(scape.stats.angle_wrap(rad2deg(s.pointing['az']) + opts.azshift, period=360.0) - opts.azshift)
-    #azimuth.extend(rad2deg(s.pointing['az']))
-    elevation.extend(rad2deg(s.pointing['el']))
-    power_hh_db.extend(10.0 * np.log10(s.pol('HH')[:,0]))
-    power_vv_db.extend(10.0 * np.log10(s.pol('VV')[:,0]))
-
-assert len(azimuth) == len(elevation) == len(power_hh_db), "sizes don't match"
-print "Contour plotting horizon from %d points ..." % len(azimuth)
-
-# Calculate and plot tipping curves
-#plt.figure(1)
-#plt.clf()
-#plt.subplots_adjust(hspace=0.5)
-
-hh_data = (azimuth, elevation, power_hh_db)
-hh_titles = ('Azimuth (deg)', 'Elevation (deg)', 'Power (dB) for %s HH' % (opts.baseline,))
-vv_data = (azimuth, elevation, power_vv_db)
-vv_titles = ('Azimuth (deg)', 'Elevation (deg)', 'Power (dB) for %s VV' % (opts.baseline,))
-az_max, az_min = np.ceil(max(azimuth)), np.floor(min(azimuth))
-el_max, el_min = np.ceil(max(elevation)), np.floor(min(elevation))
-pow_max, pow_min = max([max(power_hh_db), max(power_vv_db)]), min([min(power_hh_db), min(power_vv_db)])
-az_mid = (az_max + az_min) / 2.0
-el_mid = (el_max + el_min)/2.0
-
-if opts.split:
-    plt.subplot(4, 1, 1)
-    plot_horizon(hh_data, hh_titles, (az_min, az_mid), (el_min, el_max), (pow_min, pow_max))
-    plt.subplot(4, 1, 2)
-    plot_horizon(hh_data, hh_titles, (az_mid, az_max), (el_min, el_max), (pow_min, pow_max))
-    plt.subplot(4, 1, 3)
-    plot_horizon(vv_data, vv_titles, (az_min, az_mid), (el_min, el_max), (pow_min, pow_max))
-    plt.subplot(4, 1, 4)
-    plot_horizon(vv_data, vv_titles, (az_mid, az_max), (el_min, el_max), (pow_min, pow_max))
-else:
-    plt.subplot(2, 1, 1)
-    plot_horizon(vv_data, vv_titles, (az_min, az_max), (el_min, el_max), (pow_min, pow_max))
-    plt.subplot(2, 1, 2 )
-    plot_horizon(hh_data, hh_titles, (az_min, az_max), (el_min, el_max), (pow_min, pow_max))
-
-# Display plots - this should be called ONLY ONCE, at the VERY END of the script
-# The script stops here until you close the plots...
-plt.show()
-
-
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
