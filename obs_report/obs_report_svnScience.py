3c32975e504dfbcd327801b88920e5448e4aea44
Some Fringe-Finder User <ffuser@ska.ac.za>
2013-02-14 17:45:21 +0000

    fixed the error which come if the script can't get a source tagged as bpcal, the part that plot fringes will be skipped if the bpcal tagged source is not found -siphelele
    git-svn-id: https://katfs.kat.ac.za/svnScience/KAT-7/comm/scripts/obs_report/obs_report_svnSciences@636 8b6c7b63-b928-0410-ae4f-8976809ff30d
    
    Signed-off-by: Tom Mauch <tmauch@ska.ac.za>

--- obs_report/obs_report_svnScience.py
+++ obs_report/obs_report_svnScience.py
@@ -37,8 +37,16 @@ def times(ant,pol,count):
     		f.select(channels=range(200,800))
 		if count==0:
 			plot(f.lst,10*np.log10(mean(abs(f.vis[:]),1)),label=(ant_x.name+'_'+pol+pol))
+			locs,labels=xticks()
+			for i in range(len(locs)):
+				labels[i]=("%2.0f:%2.0f"%(np.modf(locs[i])[1], np.modf(locs[i])[0]*60))
+			xticks(locs,labels)
 		elif count==1:
 			plot(f.lst,10*np.log10(mean(abs(f.vis[:]),1)),label=(ant_x.name+'_'+pol+pol))
+			locs,labels=xticks()
+			for i in range(len(locs)):
+				labels[i]=("%2.0f:%2.0f"%(np.modf(locs[i])[1], np.modf(locs[i])[0]*60))
+			xticks(locs,labels)
 	
 	legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=4, fancybox=True, shadow=False)
 	savefig(pp,format='pdf')
@@ -169,6 +177,10 @@ print "Getting wind and temperature sensors"
 fig=pl.figure(figsize=(10,10))
 ax1 = fig.add_subplot(211)
 ax1.plot(f.lst,f.sensor['Enviro/asc.air.temperature'],'g-')
+locs,labels=xticks()
+for i in range(len(locs)):
+	labels[i]=("%2.0f:%2.0f"%(np.modf(locs[i])[1], np.modf(locs[i])[0]*60))
+xticks(locs,labels)
 ax1.grid(axis='y', linewidth=0.15, linestyle='-', color='k')
 ax1.set_xlabel("LST on "+starttime, fontweight="bold")
 ax1.set_ylabel('Temperature (Deg C)', color='g',fontweight="bold")
@@ -191,6 +203,10 @@ for m in range(len(rh)):
 
 ax2=ax1.twinx()
 ax2.plot(f.lst,ah,'c-')
+locs,labels=xticks()
+for i in range(len(locs)):
+	labels[i]=("%2.0f:%2.0f"%(np.modf(locs[i])[1], np.modf(locs[i])[0]*60))
+xticks(locs,labels)
 ax2.grid(axis='y', linewidth=0.15, linestyle='-', color='k')
 #ylim(10,100)
 ax2.set_ylabel('Absolute Humidity g/m^3', fontweight="bold",color='c')
@@ -200,6 +216,9 @@ for tl in ax2.get_yticklabels():
 ax3=fig.add_subplot(212)
 ax3.grid(axis='y', linewidth=0.15, linestyle='-', color='k')
 ax3.plot(f.lst,f.sensor['Enviro/asc.wind.speed'],'b-')
+for i in range(len(locs)):
+	labels[i]=("%2.0f:%2.0f"%(np.modf(locs[i])[1], np.modf(locs[i])[0]*60))
+xticks(locs,labels)
 ylim(ymin=0)
 ax3.set_xlabel("LST on "+starttime,fontweight="bold")
 ax3.set_ylabel('Wind Speed (m/s)',fontweight="bold", color='b')
@@ -208,6 +227,9 @@ for tl in ax3.get_yticklabels():
 
 ax4=ax3.twinx()
 ax4.plot(f.lst ,f.sensor['Enviro/asc.air.pressure'],'r-')
+for i in range(len(locs)):
+	labels[i]=("%2.0f:%2.0f"%(np.modf(locs[i])[1], np.modf(locs[i])[0]*60))
+xticks(locs,labels)
 ax4.grid(axis='y', linewidth=0.15, linestyle='-', color='k')
 ax4.set_ylabel('Air Pressure (kPa)', fontweight="bold",color='r')
 for tl in ax4.get_yticklabels():
@@ -215,55 +237,66 @@ for tl in ax4.get_yticklabels():
 savefig(pp,format='pdf')
 
 
-print "Plotting fringes"
+# from kat@kat-imager:lindsay/fringes.py
 f.select()
 cor = f.corr_products
 ants = f.ants
 num_ants = len(ants)
 bp = np.array([t.tags.count('bpcal') for t in f.catalogue.targets]) == 1
-bp = np.arange(len(bp))[bp]
+for z in range(len(bp)):
+	if bp[z]==1:
+		a=1
+		break
+	else:
+		a=0
+if a==1:
+	print "Plotting fringes"
+	bp = np.arange(len(bp))[bp]
 #code to plot the cross phase ... fringes
-fig = plt.figure(figsize=(14,10))
-try:
-    j=0
+	fig = plt.figure(figsize=(14,10))
+	try:
+   		j=0
 #plt.figure()
-    n_channels = len(f.channels)
-    for pol in ('h','v'):
-        f.select(targets=bp,corrprods = 'cross',pol=pol)
-        crosscorr = [(f.inputs.index(inpA), f.inputs.index(inpB)) for inpA, inpB in f.corr_products]
-        #extract the fringes
-	fringes = np.angle(f.vis[:,:,:])
-        #For plotting the fringes
-        fig.subplots_adjust(wspace=0., hspace=0.)
-        #debug_here()
-        for n, (indexA, indexB) in enumerate(crosscorr):
-            subplot_index = (num_ants * indexA + indexB + 1) if pol == 'h' else (indexA + num_ants * indexB + 1)
-            ax = fig.add_subplot(num_ants, num_ants, subplot_index)
-            ax.imshow(fringes[:,:,n],aspect=fringes.shape[1]/fringes.shape[0])
-            ax.set_xticks([])
-            ax.set_yticks([])
-            if pol == 'h':
-                if indexA == 0:
-                    ax.xaxis.set_label_position('top')
-                    ax.set_xlabel(f.inputs[indexB][3:],size='xx-large')
-                if indexB == len(f.ants) - 1:
-                    ax.yaxis.set_label_position('right')
-                    ax.set_ylabel(f.inputs[indexA][3:], rotation='horizontal',size = 'xx-large')
-            else:
-                if indexA == 0:
-                    ax.set_ylabel(f.inputs[indexB][3:], rotation='horizontal',size='xx-large')
-                if indexB == len(f.ants) - 1:
-                    ax.set_xlabel(f.inputs[indexA][3:],size='xx-large')
+   		n_channels = len(f.channels)
+    		for pol in ('h','v'):
+        		f.select(targets=bp,corrprods = 'cross',pol=pol)
+        		crosscorr = [(f.inputs.index(inpA), f.inputs.index(inpB)) for inpA, inpB in f.corr_products]
+        		#extract the fringes
+			fringes = np.angle(f.vis[:,:,:])
+        		#For plotting the fringes
+        		fig.subplots_adjust(wspace=0., hspace=0.)
+        		#debug_here()
+        		for n, (indexA, indexB) in enumerate(crosscorr):
+            			subplot_index = (num_ants * indexA + indexB + 1) if pol == 'h' else (indexA + num_ants * indexB + 1)
+            			ax = fig.add_subplot(num_ants, num_ants, subplot_index)
+            			ax.imshow(fringes[:,:,n],aspect=fringes.shape[1]/fringes.shape[0])
+            			ax.set_xticks([])
+            			ax.set_yticks([])
+            			if pol == 'h':
+                			if indexA == 0:
+                    				ax.xaxis.set_label_position('top')
+                    				ax.set_xlabel(f.inputs[indexB][3:],size='xx-large')
+                			if indexB == len(f.ants) - 1:
+                    				ax.yaxis.set_label_position('right')
+                    				ax.set_ylabel(f.inputs[indexA][3:], rotation='horizontal',size = 'xx-large')
+            			else:
+                			if indexA == 0:
+                    				ax.set_ylabel(f.inputs[indexB][3:], rotation='horizontal',size='xx-large')
+                			if indexB == len(f.ants) - 1:
+                    				ax.set_xlabel(f.inputs[indexA][3:],size='xx-large')
     #plt.savefig(pp,format='pdf')
-except KeyError , error:
-    print 'Failed to read scans from File: ',fn,' with Key Error:',error
-except ValueError , error:
-    print 'Failed to read scans from File: ',fn,' with Value Error:',error
-plt.savefig(pp,format='pdf')
+	except KeyError , error:
+    		print 'Failed to read scans from File: ',fn,' with Key Error:',error
+	except ValueError , error:
+    		print 'Failed to read scans from File: ',fn,' with Value Error:',error
+	plt.savefig(pp,format='pdf')
+else:
+	print "No bandpass calibrators found, we wont plot fringes"	
 plt.close('all')
 pp.close()
 
-print 'The results are save on ~/comm/scripts/obs_report/obs_report_svnSciences/'+datafile[:-3]+'.pdf' 
+print 'The results are save on ~/comm/scripts/obs_report/obs_report_svnSciences/'+datafile[:-3]+'.pdf'
+print "Pool the pdf file and  upload it to the elog with necessary comments"
 
 
 #show()
