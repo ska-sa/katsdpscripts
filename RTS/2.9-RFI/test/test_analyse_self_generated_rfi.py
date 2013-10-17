fdf2c444debc65b562f403830a9473ede800190f
Thomas Bennett <thomas@ska.ac.za>
2013-10-17 14:09:10 +0200

    Added first cut of the test code for 2.9 RFI
    git-svn-id: https://katfs.kat.ac.za/svnScience/RTS@958 8b6c7b63-b928-0410-ae4f-8976809ff30d

---
+++ RTS/2.9-RFI/test/test_analyse_self_generated_rfi.py
@@ -0,0 +1,23 @@
+import katarchive
+from subprocess import check_output
+
+def put_test_file(test_dir):
+    prod = katarchive.search_archive(filename='1369128966.h5')[0]
+    prod.download_dir = test_dir
+    prod.path_to_file
+    return prod.path_to_file
+
+def build_analyse_self_generated_rfi_command(katfilename):
+    force_system_python = '/usr/bin/python'
+    my_exec = '/home/kat/RTS/svnScience/RTS/2.9-RFI/analyse_self_generated_rfi.py'
+    return '%s %s %s' % (force_system_python, my_exec, katfilename)
+
+#create output directory and get the test file
+test_dir =  '/home/kat/RTS/test_area/2.9-RFI/1382008836/'
+test_inputfile = put_test_inputfile(test_dir)
+
+#execute in a shell in the test directory
+cmd = build_analyse_self_generated_rfi_command(test_inputfile)
+std_output = check_output(cmd, shell=True, cwd=test_dir)
+
+#output viewable @ http://sp-test/RTS/2.9-RFI/blabla
