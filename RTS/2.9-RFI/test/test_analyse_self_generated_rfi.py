245be379c8f7219531333acd8048bd3f4b073242
Thomas Bennett <thomas@ska.ac.za>
2013-10-17 15:09:09 +0200

    Create a single place to store test files
    git-svn-id: https://katfs.kat.ac.za/svnScience/RTS@963 8b6c7b63-b928-0410-ae4f-8976809ff30d

--- RTS/2.9-RFI/test/test_analyse_self_generated_rfi.py
+++ RTS/2.9-RFI/test/test_analyse_self_generated_rfi.py
@@ -4,7 +4,7 @@ import os
 
 from subprocess import check_output
 
-def put_test_inputfile(test_dir):
+def put_test_inputfile(testdata_dir):
     prod = katarchive.search_archive(filename='1378901689.h5')[0]
     prod.download_dir = test_dir
     prod.path_to_file
@@ -15,18 +15,21 @@ def build_analyse_self_generated_rfi_command(katfilename):
     my_exec = '/home/kat/RTS/svnScience/RTS/2.9-RFI/analyse_self_generated_rfi.py'
     return '%s %s %s' % (force_system_python, my_exec, katfilename)
 
-#create output directory and get the test file
+#setup
+testdata_dir = '/home/kat/RTS/test_area/Test_Data/'
+test_inputfile = put_test_inputfile(testdata_dir)
+
 if True:
     test_dir = '/home/kat/RTS/test_area/2.9-RFI/%i' % int(time.time())
 else:
     test_dir =  '/home/kat/RTS/test_area/2.9-RFI/benchmark/'
 os.mkdir(test_dir)
-test_inputfile = put_test_inputfile(test_dir)
 
-#execute in a shell in the test directory
+#execute
 cmd = build_analyse_self_generated_rfi_command(test_inputfile)
 std_output = check_output(cmd, shell=True, cwd=test_dir)
 
+#output
 print 'Executed: %s' %(cmd)
 print 'Benchmark output viewable @ http://sp-test.kat.ac.za/RTS/2.9-RFI/benchmark/'
 print 'This run output viewable @ %s' % (test_dir.replace('/home/kat/RTS/test_area', 'http://sp-test.kat.ac.za/RTS'))
