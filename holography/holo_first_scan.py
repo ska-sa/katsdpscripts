3a56942ecd06f90c6a4240e22e98fd0cff9b91a3
Charles de Villiers <charles@ska.ac.za>
2013-10-22 22:35:10 +0200

    Reverted boilerplate commit
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@18970 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- holography/holo_first_scan.py
+++ holography/holo_first_scan.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python
-###############################################################################
-# SKA South Africa (http://ska.ac.za/)                                        #
-# Author: cam@ska.ac.za                                                       #
-# Copyright @ 2013 SKA SA. All rights reserved.                               #
-#                                                                             #
-# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
-# WRITTEN PERMISSION OF SKA SA.                                               #
-###############################################################################
-
 
 # IMPORTANT: update TLE's before doing this!
 #
