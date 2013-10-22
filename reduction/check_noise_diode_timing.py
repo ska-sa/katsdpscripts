86e6fc0a0e215080cadee9c3bc19a17e07642673
Charles de Villiers <charles@ska.ac.za>
2013-10-22 22:37:07 +0200

    Reverted boilerplate commit
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katscripts/trunk@18971 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- reduction/check_noise_diode_timing.py
+++ reduction/check_noise_diode_timing.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python
-###############################################################################
-# SKA South Africa (http://ska.ac.za/)                                        #
-# Author: cam@ska.ac.za                                                       #
-# Copyright @ 2013 SKA SA. All rights reserved.                               #
-#                                                                             #
-# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
-# WRITTEN PERMISSION OF SKA SA.                                               #
-###############################################################################
 #
 # This examines all noise diode firings in an HDF5 file and compares the timestamp
 # of each firing with one derived from the rise in recorded power.
--- reduction/correct_vanvleck.py
+++ reduction/correct_vanvleck.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python
-###############################################################################
-# SKA South Africa (http://ska.ac.za/)                                        #
-# Author: cam@ska.ac.za                                                       #
-# Copyright @ 2013 SKA SA. All rights reserved.                               #
-#                                                                             #
-# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
-# WRITTEN PERMISSION OF SKA SA.                                               #
-###############################################################################
 #
 # Apply Van Vleck (quantisation) correction to an HDF5 file. This uses the
 # online system itself in an offline mode to perform the correction.
