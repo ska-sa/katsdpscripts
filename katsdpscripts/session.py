684c56504240b4ede256fbe6db33b778579751b4
Charles de Villiers <charles@ska.ac.za>
2013-10-22 09:44:47 +0200

    Added boilerplate text
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katcorelib/trunk@18931 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katsdpscripts/session.py
+++ katsdpscripts/session.py
@@ -1,3 +1,11 @@
+###############################################################################
+# SKA South Africa (http://ska.ac.za/)                                        #
+# Author: cam@ska.ac.za                                                       #
+# Copyright @ 2013 SKA SA. All rights reserved.                               #
+#                                                                             #
+# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
+# WRITTEN PERMISSION OF SKA SA.                                               #
+###############################################################################
 """CaptureSession encompassing data capturing and standard observations with KAT.
 
 This defines the :class:`CaptureSession` class, which encompasses the capturing
