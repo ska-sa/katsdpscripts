5568eaee45fc942b78a6d8f019bf7ed92a72ddeb
Sean Passmoor <sean@ska.ac.za>
2014-01-10 00:57:27 +0200

    Test of a new script
    git-svn-id: https://katfs.kat.ac.za/svnScience/RTS@1131 8b6c7b63-b928-0410-ae4f-8976809ff30d

--- RTS/2.2-T_sys_T_nd/new_tempscan.py
+++ RTS/2.2-T_sys_T_nd/new_tempscan.py
@@ -103,9 +103,9 @@ with verify_and_connect(opts) as kat:
             # Keep going until the time is up
             keep_going = True
             kat.ants.req.sensor_sampling("lock","event")
-            kat.ants.req.sensor_sampling("scan_status","event")
+            kat.ants.req.sensor_sampling("scan.status","event")
             session.ants.set_sensor_strategies("lock", "event")
-            session.ants.set_sensor_strategies("scan_status", "event")
+            session.ants.set_sensor_strategies("scan.status", "event")
             skip_file.write("# Record of targets observed on %s by %s\n" % (datetime.datetime.now(), opts.observer))
             def  scan(target, duration=30.0, start=(-3.0, 0.0), end=(3.0, 0.0), index=-1,
                       projection=opts.projection, drive_strategy='shortest-slew', announce=True):
