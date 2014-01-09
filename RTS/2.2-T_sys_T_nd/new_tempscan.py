1a7b8c2c59bc0f1fbbad96397df1c71b347407c3
Sean Passmoor <sean@ska.ac.za>
2014-01-10 00:08:22 +0200

    Test of a new script
    git-svn-id: https://katfs.kat.ac.za/svnScience/RTS@1125 8b6c7b63-b928-0410-ae4f-8976809ff30d

--- RTS/2.2-T_sys_T_nd/new_tempscan.py
+++ RTS/2.2-T_sys_T_nd/new_tempscan.py
@@ -116,7 +116,7 @@ with verify_and_connect(opts) as kat:
                 # Iterate through source list, picking the next one that is up
                 for target in pointing_sources.iterfilter(el_limit_deg=opts.horizon):
                     if not np.isnan(target.flux_density(opts.centre_freq)): # check flux model
-                        ants.req.rfe3_rfe15_noise_source_on(diode, 1, time.time()+2.0, 0)
+                        session.ants.req.rfe3_rfe15_noise_source_on(diode, 1, time.time()+2.0, 0)
                         time.time.sleep(2)
                         rscan(target)
                         session.ants.req.rfe3_rfe15_noise_source_on(diode, 0, time.time()+2.0, 0)
