aaf28f49397a2b28cc73c6bf2784ce1c0d6d80ab
Ludwig Schwardt <ludwig@ska.ac.za>
2014-01-10 10:40:43 +0200

    Refactoring Sleeper to Bed
    The name Bed is more descriptive of the class, as it is actually the
    client thread doing the sleeping inside this object. A Dormitory is
    also a better analogy as a collection of beds. Tweak the method names
    to fit this terminology. Add a logger.

--- katsdpscripts/fake/update.py
+++ katsdpscripts/fake/update.py
@@ -1,7 +1,11 @@
 import time
 import threading
+import logging
 
 from katpoint import Timestamp
+
+
+logger = logging.getLogger(__name__)
 
 
 class RealClock(object):
@@ -9,8 +13,10 @@ class RealClock(object):
     def __init__(self, start_time=None):
         self.offset = 0.0 if start_time is None else \
                       Timestamp(start_time).secs - time.time()
+
     def time(self):
         return time.time() + self.offset
+
     def sleep(self, seconds):
         time.sleep(seconds)
 
@@ -19,19 +25,21 @@ class FakeClock(object):
     """Fake time source, mimicking the time module."""
     def __init__(self, start_time=None):
         self.timestamp = Timestamp(start_time).secs
+
     def time(self):
         return self.timestamp
+
     def sleep(self, seconds):
         self.timestamp += seconds
 
 
-class Sleeper(object):
-    """A point where one thread sleeps, to be awoken by another thread."""
+class Bed(object):
+    """A place where one thread sleeps, to be awoken by another thread."""
     def __init__(self, seconds):
         self.awake = threading.Event()
         self.seconds_left = seconds
 
-    def sleep(self):
+    def climb_in(self):
         self.awake.wait(self.seconds_left)
 
     def wake_up(self):
@@ -39,25 +47,27 @@ class Sleeper(object):
 
 
 class Dormitory(object):
-    """A collection of Sleepers."""
+    """A collection of Beds with a lock on the door."""
     def __init__(self):
         self.beds = []
-        self.bed_lock = threading.Lock()
+        self.door_lock = threading.Lock()
 
-    def sleep(self, seconds):
-        sleeper = Sleeper(seconds)
-        with self.bed_lock:
-            self.beds.append(sleeper)
-        sleeper.sleep()
+    def check_in(self, seconds):
+        # Add a new bed to the room
+        bed = Bed(seconds)
+        with self.door_lock:
+            self.beds.append(bed)
+        bed.climb_in()
 
     def run(self, seconds):
-        with self.bed_lock:
-            for sleeper in self.beds:
-                sleeper.seconds_left -= seconds
-                if sleeper.seconds_left <= 0.0:
-                    sleeper.wake_up()
+        with self.door_lock:
+            # Advance time and wake those beds which time is up
+            for bed in self.beds:
+                bed.seconds_left -= seconds
+                if bed.seconds_left <= 0.0:
+                    bed.wake_up()
             # Make the beds
-            self.beds = [s for s in self.beds if not s.awake.isSet()]
+            self.beds = [bed for bed in self.beds if not bed.awake.isSet()]
 
 
 class PeriodicUpdateThread(threading.Thread):
@@ -78,9 +88,12 @@ class PeriodicUpdateThread(threading.Thread):
             for component in self.components:
                 component.update(timestamp)
             self.last_update = timestamp
-            remaining_time = self.period - (self.clock.time() - timestamp)
-            # if remaining_time < 0:
-            #     logger.warn("Update thread is struggling: updates take longer than a period")
+            update_time = self.clock.time() - timestamp
+            remaining_time = self.period - update_time
+            if remaining_time < 0:
+                logger.warn("Update thread is struggling: updates take "
+                            "%g seconds but repeat every %g seconds" %
+                            (update_time, self.period))
             self.clock.sleep(remaining_time if remaining_time > 0 else 0)
             self.dorm.run(self.period)
 
@@ -93,4 +106,4 @@ class PeriodicUpdateThread(threading.Thread):
 
     def sleep(self, seconds):
         """Sleep for the requested duration in seconds."""
-        self.dorm.sleep(seconds)
+        self.dorm.check_in(seconds)
