1665d8a514a6636df923ed6201b7b539456a0927
Ludwig Schwardt <ludwig@ska.ac.za>
2014-01-29 01:11:50 +0200

    Update activity sensor and support stowing
    The antenna activity sensor served as the template and main driver for
    aggregate sensor support. Copy most of the activity logic from the
    antenna proxy model. Allow target to be set to None. Support stowing
    to zenith by creating a target on the fly in this mode.

--- katsdpscripts/fake/fake_models.py
+++ katsdpscripts/fake/fake_models.py
@@ -12,10 +12,9 @@ class AntennaPositionerModel(FakeModel):
                  real_el_min_deg, real_el_max_deg, max_slew_azim_dps,
                  max_slew_elev_dps, inner_threshold_deg, **kwargs):
         self.ant = Antenna(description)
-        self.req_target('Zenith, azel, 0, 90')
         self.mode = 'STOP'
-        self.activity = 'stow'
-        self.lock = True
+        self.req_target('')
+        self.activity = 'stop'
         self.lock_threshold = inner_threshold_deg
         self.pos_actual_scan_azim = self.pos_request_scan_azim = 0.0
         self.pos_actual_scan_elev = self.pos_request_scan_elev = 90.0
@@ -29,8 +28,11 @@ class AntennaPositionerModel(FakeModel):
 
     def req_target(self, target):
         self.target = target
-        self._target = Target(target)
-        self._target.antenna = self.ant
+        self._target = Target(target) if target else None
+        self.lock = False
+        self.scan_status = 'none'
+        if not self._target and self.mode in ('POINT', 'SCAN'):
+            self.req_mode('STOP')
 
     def req_mode(self, mode):
         self.mode = mode
@@ -38,20 +40,41 @@ class AntennaPositionerModel(FakeModel):
     def req_scan_asym(self):
         pass
 
+    def _aggregate_activity(self, mode, scan_status, lock):
+        if mode in ('ERROR', 'STOW', 'STOP'):
+            return mode.lower()
+        elif mode in ('POINT', 'SCAN'):
+            if scan_status == 'ready':
+                return 'scan_ready'
+            elif scan_status == 'during':
+                return 'scan'
+            elif scan_status == 'after':
+                return 'scan_complete'
+            elif lock:
+                return 'track'
+            else:
+                return 'slew'
+        else:
+            return 'unknown'
+
     def update(self, timestamp):
         elapsed_time = timestamp - self._last_update if self._last_update else 0.0
         self._last_update = timestamp
         if self.mode not in ('POINT', 'SCAN', 'STOW'):
             return
-        max_delta_az = self.max_slew_azim_dps * elapsed_time
-        max_delta_el = self.max_slew_elev_dps * elapsed_time
         az, el = self.pos_actual_scan_azim, self.pos_actual_scan_elev
-        requested_az, requested_el = self._target.azel(timestamp)
+        target = construct_azel_target(deg2rad(az), deg2rad(90.0)) \
+                 if self.mode == 'STOW' else self._target
+        if not target:
+            return
+        requested_az, requested_el = target.azel(timestamp, self.ant)
         requested_az = rad2deg(wrap_angle(requested_az))
         requested_el = rad2deg(requested_el)
         delta_az = wrap_angle(requested_az - az, period=360.)
         delta_el = requested_el - el
         # Truncate velocities to slew rate limits and update position
+        max_delta_az = self.max_slew_azim_dps * elapsed_time
+        max_delta_el = self.max_slew_elev_dps * elapsed_time
         az += min(max(delta_az, -max_delta_az), max_delta_az)
         el += min(max(delta_el, -max_delta_el), max_delta_el)
         # Truncate coordinates to antenna limits
@@ -59,7 +82,7 @@ class AntennaPositionerModel(FakeModel):
         el = min(max(el, self.real_el_min_deg), self.real_el_max_deg)
         # Check angular separation to determine lock
         dish = construct_azel_target(deg2rad(az), deg2rad(el))
-        error = rad2deg(self._target.separation(dish, timestamp))
+        error = rad2deg(target.separation(dish, timestamp, self.ant))
         self.lock = error < self.lock_threshold
         # Update position sensors
         self.pos_request_scan_azim = requested_az
