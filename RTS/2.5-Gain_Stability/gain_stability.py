7ffb0d7737a6100a902239f51b4e0bcbae3059cd
Sean Passmoor <sean@ska.ac.za>
2014-02-10 15:30:23 +0200

    Sean, Fixed to handel multiple files and detrending data and gaps in the data
    git-svn-id: https://katfs.kat.ac.za/svnScience/RTS@1227 8b6c7b63-b928-0410-ae4f-8976809ff30d

--- RTS/2.5-Gain_Stability/gain_stability.py
+++ RTS/2.5-Gain_Stability/gain_stability.py
@@ -7,7 +7,25 @@ import optparse
 import scape
 import katfile
 from matplotlib.backends.backend_pdf import PdfPages
+import pandas
 
+def polyfitstd(x, y, deg, rcond=None, full=False, w=None, cov=False):
+    """
+    Modified polyfit:Any nan values in x are 'masked' and std is returned .
+    """
+    if np.isnan(y).sum() >= y.shape[0]+1 : return 0.0
+    z = np.ma.array(data=np.nan_to_num(y),mask=np.isnan(y))
+    if x.shape[0] <= deg +1  :
+        z = np.zeros((deg+1))
+        z[-1] = np.ma.mean(x)
+        return z[0]
+    gg = np.ma.polyfit(x, z, deg, rcond, full, w, cov)
+    if np.isnan(gg[0]) : raise RuntimeError('NaN in polyfit, Error')
+    return np.ma.std(z-x*gg[0])
+
+def detrend(x):
+    return polyfitstd(np.arange(x.shape[0]),x,1)
+    
 
 def plot_figures(d_uncal, d_cal, time, gain,pol):
     """ This function plots the six graphs for A polarization
@@ -55,6 +73,8 @@ parser.add_option("-m", "--min_nd", dest="min_nd", type="float", default=10,
                   help="minimum samples of noise diode to use for calibration")
 parser.add_option("-t", "--time_width", dest="time_width", type ="float", default=240, 
                   help="time-width over which to smooth the gains")
+parser.add_option("-a", "--ant", dest="ant", type ="str", default='', 
+                  help="The antenna to examine the gain stability on")
 
 (opts, args) = parser.parse_args()
 
@@ -86,32 +106,43 @@ def rolling_window(a, window):
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 
-def calc_stats(timestamps,gainfunc,freq,pol='no polarizarion',windowtime=1200):
+def calc_stats(timestamps,gain,pol='no polarizarion',windowtime=1200,minsamples=1):
     """ calculate the Stats needed to evaluate the obsevation"""
-    #p_hh = np.polyfit(time,amp_hh,1)
-    #fit_hh = np.polyval(p_hh,time)
-    #amp_detrend_hh = amp_hh - fit_hh + amp_hh.mean()
-    #p_vv = np.polyfit(time,amp_vv,1)
-    #fit_vv = np.polyval(p_vv,time)
-    #amp_detrend_vv = amp_vv - fit_vv + amp_vv.mean()
     returntext = []
-    gain = gainfunc(timestamps,freq).mean(axis=1)
-    time = timestamps.max() - timestamps.min()
-    time_step = time/timestamps.shape[0]
-    window = int(round(windowtime/time_step) )
-    windowgainchange = rolling_window(gain, window).std(axis=1)/rolling_window(gain, window).mean(axis=1)
-    rms = np.sqrt((gain**2).mean())
-    returntext.append("Total time of obsevation : %f (seconds) with %i accumulations."%(time,timestamps.shape[0]))
-    returntext.append("The mean gain of %s is: %.5f"%(pol,gain.mean()))
-    returntext.append("The Std. dev of the gain of %s is: %.5f"%(pol,gain.std()))
-    returntext.append("The RMS of the gain of %s is : %.5f"%(pol,rms))
-    returntext.append("The Percentage variation of %s is: %.5f"%(pol,gain.std()/gain.mean()*100))
-    returntext.append("The mean Percentage variation over %i samples of %s is: %.5f    (req < 2 )"%(window,pol,windowgainchange.mean()*100))
-    returntext.append("The max  Percentage variation over %i samples of %s is: %.5f    (req < 2 )"%(window,pol,windowgainchange.max()*100))
+    gain_ts = pandas.Series(gain, pandas.to_datetime(np.round(timestamps), unit='s')).asfreq(freq='1s')
+    mean = pandas.rolling_mean(gain_ts,windowtime,minsamples)
+    std = pandas.rolling_std(gain_ts,windowtime,minsamples)
+    windowgainchange = std/mean*100
+    dtrend_std = pandas.rolling_apply(gain_ts,windowtime,detrend,minsamples)
+    #trend_std = pandas.rolling_apply(ts,5,lambda x : np.ma.std(x-(np.arange(x.shape[0])*np.ma.polyfit(np.arange(x.shape[0]),x,1)[0])),1)
+    detrended_windowgainchange = dtrend_std/mean*100
+    timeval = timestamps.max()-timestamps.min()
+    window_occ = pandas.rolling_count(gain_ts,windowtime)/float(windowtime)
+    
+    #rms = np.sqrt((gain**2).mean())
+    returntext.append("Total time of obsevation : %f (seconds) with %i accumulations."%(timeval,timestamps.shape[0]))
+    #returntext.append("The mean gain of %s is: %.5f"%(pol,gain.mean()))
+    #returntext.append("The Std. dev of the gain of %s is: %.5f"%(pol,gain.std()))
+    #returntext.append("The RMS of the gain of %s is : %.5f"%(pol,rms))
+    #returntext.append("The Percentage variation of %s is: %.5f"%(pol,gain.std()/gain.mean()*100))
+    returntext.append("The mean Percentage variation over %i seconds of %s is: %.5f    (req < 2 )"%(windowtime,pol,windowgainchange.mean()))
+    returntext.append("The Max  Percentage variation over %i seconds of %s is: %.5f    (req < 2 )"%(windowtime,pol,windowgainchange.max()))
+    returntext.append("The mean detrended Percentage variation over %i seconds of %s is: %.5f    (req < 2 )"%(windowtime,pol,detrended_windowgainchange.mean()))
+    returntext.append("The Max  detrended Percentage variation over %i seconds of %s is: %.5f    (req < 2 )"%(windowtime,pol,detrended_windowgainchange.max()))
+    #a - np.round(np.polyfit(b,a.T,1)[0,:,np.newaxis]*b + np.polyfit(b,a.T,1)[1,:,np.newaxis])
+    
     pltobj = plt.figure()
-    plt.title(" %s pol Gain"%(pol))
-    plt.plot(windowgainchange.mean(),'b',label='20 Min (std/mean)')
-    plt.plot(np.ones_like(windowgainchange.mean())*2.0,'r',label=' 2 level')
+    plt.title('Percentage Variation of %s pol, %i Second sliding Window'%(pol,windowtime,))
+    windowgainchange.plot(label='Orignal')
+    detrended_windowgainchange.plot(label='Detrended')
+    window_occ.plot(label='Window Occupancy')
+    plt.hlines(2, timestamps.min(), timestamps.max(), colors='k')
+    plt.ylabel('Percentage Variation')
+    plt.xlabel('Date/Time')
+    plt.legend(loc='best')
+    #plt.title(" %s pol Gain"%(pol))
+    #plt.plot(windowgainchange.mean(),'b',label='20 Min (std/mean)')
+    #plt.plot(np.ones_like(windowgainchange.mean())*2.0,'r',label=' 2 level')
     return returntext,pltobj  # a plot would be cool
     
 
@@ -120,41 +151,52 @@ def remove_rfi(d,width=3,sigma=5,axis=1):
         d.scans[i].data = scape.stats.remove_spikes(d.scans[i].data,axis=axis,spike_width=width,outlier_sigma=sigma)
     return d
 
+gain_hh = np.array(())
+gain_vv = np.array(())
+timestamps = np.array(())
+filename = args[0]
+nice_filename =  filename.split('/')[-1]+ '_' +opts.ant+'_gain_stability'
+pp = PdfPages(nice_filename+'.pdf')
 
-h5 = katfile.open(args[0])
-h5.select(ants='ant3')
-for ant in h5.ants:
-    d = scape.DataSet(args[0], baseline="A%sA%s" % (ant.name[3:], ant.name[3:]))
+for filename in args:
+    h5 = katfile.open(filename)
+    if opts.ant=='' : 
+        ant= h5.ants[0].name
+    else:
+        ant = opts.ant
+    #h5.select(ants=ant)
+    d = scape.DataSet(filename, baseline="A%sA%s" % (ant[3:], ant[3:]))
     d = d.select(freqkeep=range(start_freq_channel, end_freq_channel+1))
-    d = remove_rfi(d,width=7,sigma=5)  # rfi flaging
+    d = remove_rfi(d,width=21,sigma=5)  # rfi flaging
     #Leave the d dataset unchanged after this so that it can be examined interactively if necessary
     antenna = d.antenna
     d_uncal = d.select(copy = True)
     d_uncal.average()
     d_cal = d.select(copy=True)
-
+    print("do selects")
     #extract timestamps from data
-    timestamps = np.hstack([scan.timestamps for scan in d_cal.scans])
+    timestampfile = np.hstack([scan.timestamps for scan in d_cal.scans])
     #get a user-friendly time axis that will plot in the same way as plot_xyz
     time = scape.extract_scan_data(d_cal.scans, 'time')
     tmin = np.min(np.hstack(time.data))
     tmax = np.max(np.hstack(time.data))
     #Get the gain from the noise diodes
     g_hh, g_vv, delta_re_hv, delta_im_hv = scape.gaincal.estimate_gain(d_cal)
-    gain_hh = g_hh(timestamps, d.freqs).mean(axis=1)
-    gain_vv = g_vv(timestamps, d.freqs).mean(axis=1)
-
+    gain_hh = np.r_[gain_hh,g_hh(timestampfile, d.freqs).mean(axis=1)]
+    gain_vv = np.r_[gain_vv,g_vv(timestampfile, d.freqs).mean(axis=1)]
+    timestamps = np.r_[timestamps,timestampfile]
+    print("Applied gains")
+    print " gain_hh  %i, gain_vv %i, timestamps %i"%(gain_hh.shape[0],gain_vv.shape[0],timestamps.shape[0])
     #Apply noise diode calibration
-    d_cal.convert_power_to_temperature(min_samples=opts.min_nd, time_width=opts.time_width)
-    d_cal.average()
-    nice_filename =  args[0].split('/')[-1]+ '_' +d.antenna.name+'_gain_stability'
-    pp = PdfPages(nice_filename+'.pdf')
-    fig = plot_figures(d_uncal, d_cal, time, gain_hh, 'HH')
-    fig.savefig(pp,format='pdf') 
-    plt.close()
-    fig = plot_figures(d_uncal, d_cal, time, gain_vv, 'VV')
-    fig.savefig(pp,format='pdf')    
-    plt.close()
+    if False:
+        d_cal.convert_power_to_temperature(min_samples=opts.min_nd, time_width=opts.time_width)
+        d_cal.average()
+        fig = plot_figures(d_uncal, d_cal, time, gain_hh, 'HH')
+        fig.savefig(pp,format='pdf') 
+        plt.close()
+        fig = plot_figures(d_uncal, d_cal, time, gain_vv, 'VV')
+        fig.savefig(pp,format='pdf')    
+        plt.close()
     
     #extract data to look at stats
     #time, amp_hh, z =  scape.extract_xyz_data(d_uncal.select(flagkeep='~nd_on', copy=False), 'time', 'amp', pol = 'HH')
@@ -162,10 +204,12 @@ for ant in h5.ants:
     #time = np.hstack(time.data)
     #amp_hh = np.hstack(amp_hh.data)
     #amp_vv = np.hstack(amp_vv.data)
-    returntext,fig = calc_stats(timestamps,g_hh,d.freqs,'HH',1200)
+    
+if True :
+    returntext,fig = calc_stats(timestamps,gain_hh,'HH',1200)
     fig.savefig(pp,format='pdf')
     plt.close()
-    tmp,fig = calc_stats(timestamps,g_vv,d.freqs,'VV',1200)
+    tmp,fig = calc_stats(timestamps,gain_vv,'VV',1200)
     fig.savefig(pp,format='pdf')
     plt.close()
     returntext += tmp
@@ -173,7 +217,7 @@ for ant in h5.ants:
     fig = plt.figure(None,figsize = (10,16))
     plt.figtext(0.1,0.1,'\n'.join(returntext),fontsize=10)
     fig.savefig(pp,format='pdf')
-    pp.close()
-    plt.close()
-
+pp.close()
+plt.close()
+###
 
