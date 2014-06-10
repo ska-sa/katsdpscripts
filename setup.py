7b2b41362f235139e7fa94800028823b93cd3d60
Thomas Bennett <thomas@ska.ac.za>
2014-06-10 12:18:55 +0200

    Convert spectral baseline scripts to module in katsdpscripts.RTS and modify original scripts to use this module.

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup (
     include_package_data = True,
     scripts = [
         "RTS/Condition_Report/condition_report.py",
-	"RTS/RFI_Report/rfi_report.py",
-        "RTS/2.8-Strong_Sources/analyse_noise_diode.py"
+        "RTS/RFI_Report/rfi_report.py",
+        "RTS/2.8-Strong_Sources/analyse_noise_diode.py",
+	"RTS/2.10-Receptor_Spectral_Baseline/analyse_spectrum.py"
     ],
     url = 'http://ska.ac.za/',
     classifiers = [
