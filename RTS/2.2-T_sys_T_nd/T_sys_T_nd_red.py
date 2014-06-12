ae542d18903919156fd29f40c23943de869908f8
Lindsay Magnus <lindsay@ska.ac.za>
2014-06-12 07:28:58 +0200

    Added main method and output_dir commandline option

--- RTS/2.2-T_sys_T_nd/T_sys_T_nd_red.py
+++ RTS/2.2-T_sys_T_nd/T_sys_T_nd_red.py
@@ -7,15 +7,17 @@ def parse_arguments():
                                    description=" This produces a pdf file with graphs verifying the ND model and Tsys for each antenna in the file")
     parser.add_option("-f", "--frequency-bandwidth", dest="freq_band", type="float", default='256e6',
                       help="BAndwidth of frequency channels to keep. Default = %default")
-
+    parser.add_option("-o","--output_dir", default='.', help="Output directory for pdfs. Default is cwd")
     (opts, args) = parser.parse_args()
     if len(args) ==0:
         raise RuntimeError('Please specify an h5 file to load.')
     
     return opts,args
 
-opts, args = parse_arguments()
-diodelib.read_and_plot_data(args[0],opts.freq_band)
+
+if __name__ == "__main__":
+    opts, args = parse_arguments()
+    diodelib.read_and_plot_data(args[0],opts.output_dir,opts.freq_band)
 
 
 
