a215f25ce23111538b0532df710e5d44e0d74b8b
Thomas Bennett <thomas@lmzxq-mbp.local>
2014-07-14 15:51:04 +0200

    Updates for deployment

--- rts_deploy/rts_common_deploy.py
+++ rts_deploy/rts_common_deploy.py
@@ -6,6 +6,7 @@ import os
 GIT_BRANCH = 'master'
 
 # oodt install info
+VAR_KAT = '/var/kat'
 OODT_HOME = '/usr/local/oodt/'
 STAGING_HOME = '/var/kat/data'
 ARCHIVE_HOME = '/var/kat/archive'
@@ -27,9 +28,6 @@ ARCHIVE_DATA = os.path.join(ARCHIVE_HOME, "data")
 RTS_DATA = os.path.join(ARCHIVE_DATA, "RTS")
 
 #Solr and Tomcat
-SOLR_VER = "4.4.0"
-TOMCAT_VER = "7.0.42"
-TOMCAT_CONF = os.path.join(OODT_HOME, 'apache-tomcat/conf/server.xml')
 
 def install_deb_packages(packages):
     """Install debian packages listed in space-separated string"""
@@ -73,9 +71,9 @@ def retrieve_git_package(package, output_location=None, repo='ska-sa', login='ka
     sudo('rm -rf '+output_location)
     print '\n ---- Retrieve', package, 'to', output_location, ' ---- \n'
     if login:
-        sudo('git clone '+flags+' --branch '+branch+' https://'+login+'@github.com/'+repo+'/'+package+' '+output_location)
+        run('git clone '+flags+' --branch '+branch+' https://'+login+'@github.com/'+repo+'/'+package+' '+output_location)
     else:
-        sudo('git clone '+flags+' --branch '+branch+' https://github.com/'+repo+'/'+package+' '+output_location)
+        run('git clone '+flags+' --branch '+branch+' https://github.com/'+repo+'/'+package+' '+output_location)
 
 def remove_dir(rmdir):
     sudo("rm -rf %s" % (rmdir,))
@@ -95,14 +93,7 @@ def make_dc_directory_trees():
     make_directory(RTS_DATA)
     make_directory(SDP_MC)
 
-def configure_tomcat():
-    files.sed(TOMCAT_CONF,
-              '<Connector port="8080" protocol="HTTP/1.1"',
-              '<Connector port="8983" protocol="HTTP/1.1"',
-              use_sudo=True,
-              backup='.bak')
-
-def deploy_oodt_comp(comp_to_install, comp_ver):
+def deploy_tarball(comp_to_install, comp_ver):
     comp_tar = "%s-dist.tar.gz" % (comp_ver)
     run("rm -rf %s" % (os.path.join(OODT_HOME, comp_to_install)))
     run("wget -O /tmp/%s http://kat-archive.kat.ac.za/oodt_installs/%s" % (comp_tar, comp_tar))
@@ -111,39 +102,12 @@ def deploy_oodt_comp(comp_to_install, comp_ver):
     run("rm -f /tmp/%s" % (comp_tar))
 
 def deploy_oodt_comp_ver_06(comp_to_install):
-    deploy_oodt_comp(comp_to_install, "%s-0.6" % (comp_to_install))
+    deploy_tarball(comp_to_install, "%s-0.6" % (comp_to_install))
 
-def deploy_dc_oodt_comps():
-    deploy_oodt_comp_ver_06("cas-filemgr")
-    deploy_oodt_comp_ver_06("cas-crawler")
-    deploy_tomcat()
-    deploy_solr()
-
-def deploy_oodt_conf(branch):
-    """Deploy the oodt configuration for branch (i.e karoo/vkaroo/lab/comm VMs).
-
-    Parameters
-    ==========
-    branch: The OODT configuration svn branch to checkout. OODT configuration can be
-    found in '../svnDS/code/katconfig/{branch}/static/oodt/'. For example 'trunk' or
-    'branches/karoo'.
-    """
-    run("rm -rf %s" % (OODT_CONF))
-    run("svn co --username kat --password kat "
-        "https://katfs.kat.ac.za/svnDS/code/katconfig/%s/static/oodt/ "
-        "%s" % (branch, OODT_CONF))
-
-def deploy_tomcat(comp_to_install="apache-tomcat"):
-    deploy_oodt_comp(comp_to_install, "%s-%s" % (comp_to_install, TOMCAT_VER))
-
-def deploy_solr(comp_to_install="solr"):
-    deploy_oodt_comp(comp_to_install, "%s-%s" % (comp_to_install, SOLR_VER))
-
-def deploy_solr_in_tomcat():
-    run("cp %s/solr/dist/solr-%s.war %s/apache-tomcat/webapps/solr.war" % (OODT_HOME, SOLR_VER, OODT_HOME))
-    run("cp %s/solr/example/lib/ext/* %s/apache-tomcat/lib" % (OODT_HOME, OODT_HOME))
-    run("rsync -rv --progress %s/solr/ %s" % (OODT_CONF, SOLR_COLLECTIONS_HOME))
-    run("rm -rf %s/solr " % (OODT_HOME))
+# def deploy_dc_oodt_comps():
+#     deploy_oodt_comp_ver_06("cas-filemgr")
+#     deploy_oodt_comp_ver_06("cas-crawler")
+#     deploy_solr()
 
 def check_and_make_sym_link(L_src, L_dest):
     sudo("if [[ ! -L %s ]]; then ln -s %s %s; fi" % (L_dest, L_src, L_dest))
@@ -154,19 +118,41 @@ def export_dc_nfs_staging():
                  use_sudo=True)
     sudo('exportfs -a')
 
-def oodt_setup():
-    make_directory(OODT_HOME)
-    make_dc_directory_trees()
-    deploy_dc_oodt_comps()
-    deploy_oodt_conf('branches/oodt_0_6_upgrade')
-    configure_tomcat()
-    deploy_solr_in_tomcat()
-    export_dc_nfs_staging()
-
 def remove_oodt_directories():
     remove_dir(OODT_HOME)
     remove_dir(ARCHIVE_HOME)
     remove_dir(ARCHIVE_APACHE_HOME)
     remove_dir(STAGING_HOME)
     remove_dir(SDP_MC)
+
+def deploy_solr(comp_to_install="solr"):
+    SOLR_VER = "4.4.0"
+    deploy_tarball(comp_to_install, "%s-%s" % (comp_to_install, SOLR_VER))
+    sudo("cp %s/solr/dist/solr-%s.war /var/lib/tomcat7/webapps/solr.war" % (OODT_HOME, SOLR_VER,))
+    sudo("cp %s/solr/example/lib/ext/* /usr/share/tomcat7/lib" % (OODT_HOME,))
+    run("rsync -rv --progress %s/solr/ %s" % (OODT_CONF, SOLR_COLLECTIONS_HOME))
+    run("rm -rf %s/solr " % (OODT_HOME))
+
+def configure_tomcat():
+    sudo('/etc/init.d/tomcat7 stop')
+    files.sed('/etc/tomcat7/server.xml',
+              '<Connector port="8080" protocol="HTTP/1.1"',
+              '<Connector port="8983" protocol="HTTP/1.1"',
+              use_sudo=True)
+    files.append('/etc/tomcat7/tomcat-users.xml',
+                 '<role rolename="manager-gui"/><user username="kat" password="kat" roles="manager-gui"/>',
+                 use_sudo=True)
+    files.sed('/etc/default/tomcat7',
+              'TOMCAT7_USER=tomcat7',
+              'TOMCAT7_USER=kat',
+              use_sudo=True)
+    files.sed('/etc/default/tomcat7',
+              'TOMCAT7_GROUP=tomcat7',
+              'TOMCAT7_GROUP=kat',
+              use_sudo=True)
+    files.append('/etc/default/tomcat7',
+                 'CATALINA_OPTS="-Dsolr.solr.home=/var/kat/archive/catalogs/solr"', 
+                 use_sudo=True)
+    sudo('/etc/init.d/tomcat7 start')
+
 
--- rts_deploy/rts_imager.py
+++ rts_deploy/rts_imager.py
@@ -1,7 +1,12 @@
 from fabric.api import sudo, task, hosts, settings, env
 from fabric.contrib import files
 
-import rts_common_deploy
+from rts_common_deploy import install_deb_packages, install_pip_packages, install_git_package, retrieve_git_package, remove_deb_packages
+from rts_common_deploy import make_directory #, remove_dir
+from rts_common_deploy import deploy_oodt_comp_ver_06, deploy_solr, configure_tomcat, deploy_oodt_conf
+from rts_common_deploy import STAGING_HOME, STAGING_INGEST, STAGING_FAILED, ARCHIVE_DATA, SOLR_COLLECTIONS_HOME, RTS_DATA, OODT_HOME, OODT_CONF, VAR_KAT
+from rts_common_deploy import GIT_BRANCH
+import os
 
 #Set environment and hostnames
 env.hosts = ['kat@192.168.6.185']
@@ -11,21 +16,25 @@ env.password = 'kat'
 STAGING_AREA = '/data/staging_area'
 PROCESS_AREA = '/data/process_area'
 
-#Output location for the workflow manager
+#oodt flavoured directories specific to imager machine
 WORKFLOW_AREA = '/var/kat/katsdpworkflow'
+STAGING_NFS_INGEST = os.path.join(STAGING_HOME, "nfs_rts")
+
+#log directories that need kat ownership
+CELERY_LOG = '/var/log/celery'
+TOMCAT7_LOG = '/var/log/tomcat7'
 
 # Deb packages for rts-imager
-DEB_PKGS = [ 'python-dev',                                                                           #general
-                    'gfortran', 'libatlas-base-dev', 'libblas-dev', 'libexpat1-dev',
-                    'git', 'git-man',                                                                #git
-                    'python-pip', 'python-setuptools', 'python-pkg-resources',                       #pip
-                    'subversion', 'nfs-kernel-server',
-                    'python-celery', 'rabbitmq-server',
-                    'tree', 'pyflakes', 'openjdk-7-jre', 'htop',                                     #Other things
-                    'ipython', 'python-numpy', 'python-scipy', 'python-h5py', 
-                    'python-matplotlib', 'python-pyfits', 'python-pandas', 'python-nose',            #python stuff
-                    'tomcat7-common', 'tomcat7-admin'
-                    ]
+DEB_PKGS = [ 'vim', 'python-dev',                                                 #general
+             'gfortran', 'libatlas-base-dev', 'libblas-dev', 'libexpat1-dev',
+             'git', 'git-man',                                                    #git
+             'python-pip', 'python-setuptools', 'python-pkg-resources',           #pip
+             'subversion', 'nfs-kernel-server',
+             'python-celery', 'celeryd', 'rabbitmq-server',
+             'tree', 'pyflakes', 'openjdk-7-jre', 'htop',                         #Other things
+             'ipython', 'python-numpy', 'python-scipy', 'python-h5py',
+             'python-matplotlib', 'python-pyfits', 'python-pandas', 'python-nose',#python stuff
+             'tomcat7', 'nfs-common']
 
 # Pip packages for rts-imager
 PIP_PKGS = ['pyephem', 'scikits.fitting']
@@ -33,17 +42,65 @@ PIP_PKGS = ['pyephem', 'scikits.fitting']
 # SKA private git packages for rts imager
 SKA_PRIVATE_GIT_PKGS = ['katpoint', 'katdal', 'katholog', 'katsdpscripts', 'scape']
 
-def configure_celery():
-    CELERYD_CONF='/etc/default/celeryd'
+def mount_datadir():
+    make_directory('/data')
+    files.append('/etc/fstab',
+                 'UUID=88f7342e-177d-4a9d-af18-b7b669335412 /data ext4  defaults 0 0',
+                 use_sudo=True)
+    sudo('mount -a')
 
+def configure_celery():
+    sudo('/etc/init.d/celeryd stop')
+    remove_deb_packages('python-librabbitmq') #known celery bug
+    CELERYD_CONF='/etc/default/celeryd'
+    files.sed(CELERYD_CONF,
+                'ENABLED="false"',
+                'ENABLED="true"',
+                use_sudo=True)
     files.sed(CELERYD_CONF,
                 'CELERYD_USER="celery"',
                 'CELERYD_USER="kat"',
                 use_sudo=True)
     files.sed(CELERYD_CONF,
-                'CELERYD_GROUP="celery"'
-                'CELERYD_GROUP="kat"'
+                'CELERYD_GROUP="celery"',
+                'CELERYD_GROUP="kat"',
                 use_sudo=True)
+    files.sed(CELERYD_CONF,
+                'CELERYD_CHDIR="/opt/Myproject/"',
+                'CELERYD_CHDIR="/var/kat/katsdpworkflow/"',
+                use_sudo=True)
+    files.sed(CELERYD_CONF,
+                '#CELERYD_OPTS="--time-limit=300 --concurrency=8"',
+                'CELERYD_OPTS="--concurrency=1',
+                use_sudo=True)
+    sudo('/etc/init.d/celeryd start')
+
+def nfs_mount_archive_dir():
+    pass
+    
+def make_directory_trees():
+    make_directory(VAR_KAT)
+    make_directory(OODT_HOME)
+    make_directory(OODT_CONF)
+    make_directory(WORKFLOW_AREA)
+    make_directory(STAGING_INGEST)
+    make_directory(STAGING_NFS_INGEST)
+    make_directory(STAGING_FAILED)
+    make_directory(ARCHIVE_DATA)
+    make_directory(SOLR_COLLECTIONS_HOME)
+    make_directory(RTS_DATA)
+    make_directory(STAGING_AREA)
+    make_directory(PROCESS_AREA)
+    make_directory(CELERY_LOG) #change owner
+    make_directory(TOMCAT7_LOG) #change owner
+    
+
+def deploy_oodt():
+    deploy_oodt_comp_ver_06("cas-filemgr")
+    deploy_oodt_comp_ver_06("cas-crawler")
+    deploy_oodt_conf("branches/oodt_0_6_upgrade")
+    deploy_solr()
+    configure_tomcat()
 
 @task
 @hosts(env.hosts)
@@ -51,48 +108,46 @@ def deploy():
     # update the apt-get database. Warn, rather than abort, if repos are missing
     with settings(warn_only=True):
         sudo('apt-get -y update')
-
+    
     #install deb packages: thin plooging
     # install ubuntu deb packages
-    for pkg in DEB_PKGS: rts_common_deploy.install_deb_packages(pkg)
-
+    for pkg in DEB_PKGS: install_deb_packages(pkg)
+    
     #install pip packages: thin plooging
     # pip install python packages
-    for pkg in PIP_PKGS: rts_common_deploy.install_pip_packages(pkg,flags='-U --no-deps')
-
+    for pkg in PIP_PKGS: install_pip_packages(pkg,flags='-U --no-deps')
+    
     # install private ska-sa git packages
-    for pkg in SKA_PRIVATE_GIT_PKGS: rts_common_deploy.install_git_package(pkg, branch=rts_common_deploy.GIT_BRANCH)
+    for pkg in SKA_PRIVATE_GIT_PKGS: install_git_package(pkg, branch=GIT_BRANCH)
+    
+    mount_datadir()
+    make_directory_trees()
+    deploy_oodt()
+    # pip katsdpworkflow and oodt configuration in its final resting place
+    retrieve_git_package('oodt_conf',output_location=OODT_CONF)
+    retrieve_git_package('katsdpworkflow',output_location=WORKFLOW_AREA)
+    configure_celery()
 
-    # pup katsdpworkflow in its final resting place
-    rts_common_deploy.retrieve_git_package('katsdpworkflow',output_location=WORKFLOW_AREA)
-
-    # oodt setup and install
-    rts_common_deploy.oodt_setup()
-
-    # Create staging and processing dirs
-    rts_common_deploy.make_directory(STAGING_AREA)
-    rts_common_deploy.make_directory(PROCESS_AREA)
-
-@task
-@hosts(env.hosts)
-def clear():
-    # remove staging and processing dirs
-    rts_common_deploy.remove_dir(STAGING_AREA)
-    rts_common_deploy.remove_dir(PROCESS_AREA)
-
-    # remove oodt directories and packages
-    rts_common_deploy.remove_oodt_directories()
-    rts_common_deploy.remove_pip_packages('katoodt')
-
-    # remove ska-sa git packages
-    for pkg in reversed(SKA_GIT_PKGS): rts_common_deploy.remove_pip_packages(pkg)
-
-    # pip uninstall python packages
-    for pkg in reversed(PIP_PKGS + SKA_GIT_PKGS): rts_common_deploy.remove_pip_packages(pkg)
-    # hack to remove scikits, which is still hanging around
-    sudo('rm -rf /usr/local/lib/python2.7/dist-packages/scikits')
-
-    # remove ubuntu deb packages
-    for pkg in reversed(DEB_PKGS): rts_common_deploy.remove_deb_packages(pkg)
-
-    sudo('apt-get -y autoremove')
+# @task
+# @hosts(env.hosts)
+# def clear():
+#     # remove staging and processing dirs
+#     rts_common_deploy.remove_dir(STAGING_AREA)
+#     rts_common_deploy.remove_dir(PROCESS_AREA)
+# 
+#     # remove oodt directories and packages
+#     rts_common_deploy.remove_oodt_directories()
+#     rts_common_deploy.remove_pip_packages('katoodt')
+# 
+#     # remove ska-sa git packages
+#     for pkg in reversed(SKA_GIT_PKGS): rts_common_deploy.remove_pip_packages(pkg)
+# 
+#     # pip uninstall python packages
+#     for pkg in reversed(PIP_PKGS + SKA_GIT_PKGS): rts_common_deploy.remove_pip_packages(pkg)
+#     # hack to remove scikits, which is still hanging around
+#     sudo('rm -rf /usr/local/lib/python2.7/dist-packages/scikits')
+# 
+#     # remove ubuntu deb packages
+#     for pkg in reversed(DEB_PKGS): rts_common_deploy.remove_deb_packages(pkg)
+# 
+#     sudo('apt-get -y autoremove')
