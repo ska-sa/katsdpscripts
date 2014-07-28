b010c32f7a432b0509f4e52fefa1fdb609fef9e5
Mattieu de Villiers <mattieu@ska.ac.za>
2014-07-28 08:48:39 +0200

    Change timeplot ip address
    Change timeplot ip address to 10.98.2.10 in the deploy file

--- rts_deploy/rts_timeplot.py
+++ rts_deploy/rts_timeplot.py
@@ -2,7 +2,7 @@ from fabric.api import sudo, task, settings, env, hosts
 import rts_common_deploy
 
 #Set environment and hostnames
-env.hosts = ['kat@192.168.6.174']
+env.hosts = ['kat@10.98.2.10']#['kat@192.168.6.174']
 env.password = 'kat'
 
 # Deb packages
