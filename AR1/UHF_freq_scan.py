c60a41fdbfa2d1cec12559243020db8230d5e08a
Sean Passmoor <sean@ska.ac.za>
2015-07-06 11:51:00 +0200

    Short Frequency scan test script

--- AR1/UHF_freq_scan.py
+++ AR1/UHF_freq_scan.py
@@ -9,19 +9,18 @@ import time
 from katcorelib import standard_script_options, verify_and_connect, collect_targets, start_session, user_logger
 #import katpoint
 import scpi as SCPI
+import numpy as np
  
 # Set up standard script options
 parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]",
-                                 description='Track one or more sources for a specified time. At least one '
-                                             'target must be specified. Note also some **required** options below.')
+                                 description='Track one or more sources for a specified time. then change the frequency of'
+                                             'the signal generator. Note also some **required** options below.')
 # Add experiment-specific options
-parser.add_option('-t', '--track-duration', type='float', default=60.0,
+parser.add_option('-t', '--track-duration', type='float', default=2.0,
                   help='Length of time to track each source, in seconds (default=%default)')
 parser.add_option('-m', '--max-duration', type='float', default=None,
                   help='Maximum duration of the script in seconds, after which script will end '
                        'as soon as the current track finishes (no limit by default)')
-parser.add_option('--repeat', action="store_true", default=False,
-                  help='Repeatedly loop through the targets until maximum duration (which must be set for this)')
 parser.add_option('--no-delays', action="store_true", default=False,
                   help='Do not use delay tracking, and zero delays')
 
@@ -29,8 +28,8 @@ parser.add_option('--siggen-ip',  default='192.168.14.61',
                   help='Signal Generator IP adress (default=%default)')
 parser.add_option('--siggen-port', type='int', default=5025,
                   help='Signal Generator port (default=%default)')
-parser.add_option('--siggen-freq', type='float', default=600.0,
-                  help='Signal Generator frequency  in MHz(default=%default)')
+parser.add_option('--siggen-freq', type='str', default='300.0,600.0,0.208984375',
+                  help='Signal Generator frequency range in MHz, start,stop,step (default=%default)')
 parser.add_option('--siggen-power', type='float', default=-30.0,
                   help='Signal Generator power in dBm (default=%default)')
 parser.add_option('--force-siggen', action="store_true", default=False,
@@ -52,7 +51,11 @@ if  opts.siggen_power > -20.:
 
 siggen_ip = opts.siggen_ip
 siggen_port = opts.siggen_port
-siggen_freq = opts.siggen_freq
+siggen_freq_str = str(opts.siggen_freq).split(',')
+if len(siggen_freq_str) == 1 : siggen_freq_list = np.array(float(siggen_freq_str[0]))
+if len(siggen_freq_str) == 2 : siggen_freq_list = np.arange(float(siggen_freq_str[0]),float(siggen_freq_str[1]))
+if len(siggen_freq_str) == 3 : siggen_freq_list = np.arange(float(siggen_freq_str[0]),float(siggen_freq_str[1]),float(siggen_freq_str[2]))
+siggen_freq = siggen_freq_list[0]  # Set current frequency to first value in the list
 siggen_power = opts.siggen_power
 # Check options and build KAT configuration, connecting to proxies and devices
 with verify_and_connect(opts) as kat:
@@ -97,7 +100,7 @@ with verify_and_connect(opts) as kat:
                     user_logger.info("Signal Generator reset")
                     sig.outputOn()
                     user_logger.info("Signal Generator output on")
-                    sig.setFrequency(siggen_freq*1.0e6)
+                    sig.setFrequency(siggen_freq_list[0]*1.0e6)
                     user_logger.info("Signal Generator frequency is set to %7.3f MHz"%(sig.getFrequency()*1.0e-6 ))
                     siggen_freq = sig.getFrequency()
                     sig.setPower(siggen_power)
@@ -116,31 +119,43 @@ with verify_and_connect(opts) as kat:
             # Keep going until the time is up
             keep_going = True
             while keep_going:
-                keep_going = (opts.max_duration is not None) and opts.repeat
+                keep_going = (opts.max_duration is not None) 
                 targets_before_loop = len(targets_observed)
                 # Iterate through source list, picking the next one that is up
                 for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
-                    session.label('siggen,f=%f,p=%f,'%(siggen_freq,siggen_power ))
-                    user_logger.info("Initiating %g-second track on target '%s'" % (opts.track_duration, target.name,))
-                    # Split the total track on one target into segments lasting as long as the noise diode period
-                    # This ensures the maximum number of noise diode firings
-                    total_track_time = 0.
-                    while total_track_time < opts.track_duration:
-                        next_track = opts.track_duration - total_track_time
-                        # Cut the track short if time ran out
-                        if opts.max_duration is not None:
-                            next_track = min(next_track, opts.max_duration - (time.time() - start_time))
-                        if opts.nd_params['period'] > 0:
-                            next_track = min(next_track, opts.nd_params['period'])
-                        if next_track <= 0 or not session.track(target, duration=next_track, announce=False):
-                            break
-                        total_track_time += next_track
-                    if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
-                        user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
-                                            (opts.max_duration,))
-                        keep_going = False
-                        break
-                    targets_observed.append(target.name)
+                    for keyval,freq in enumerate(siggen_freq):
+                        if keep_going :
+                            if not kat.dry_run or opts.force_siggen : # prevent verifiing script from messing with things and failing to connect
+                                session.label('transition')
+                                sig.setFrequency(freq*1.0e6)
+                                user_logger.info("Signal Generator frequency is set to %7.3f MHz"%(sig.getFrequency()*1.0e-6 ))
+                                siggen_freq = sig.getFrequency()
+                                time.sleep(2.0)
+                            else :
+                                session.label('transition')
+                                time.sleep(2.0) # this is just a timing thing
+                                
+                            session.label('siggen,f=%f,p=%f,'%(siggen_freq,siggen_power ))
+                            user_logger.info("Initiating %g-second track on target '%s'" % (opts.track_duration, target.name,))
+                            # Split the total track on one target into segments lasting as long as the noise diode period
+                            # This ensures the maximum number of noise diode firings
+                            total_track_time = 0.
+                            while total_track_time < opts.track_duration:
+                                next_track = opts.track_duration - total_track_time
+                                # Cut the track short if time ran out
+                                if opts.max_duration is not None:
+                                    next_track = min(next_track, opts.max_duration - (time.time() - start_time))
+                                if opts.nd_params['period'] > 0:
+                                    next_track = min(next_track, opts.nd_params['period'])
+                                if next_track <= 0 or not session.track(target, duration=next_track, announce=False):
+                                    break
+                                total_track_time += next_track
+                            if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
+                                user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
+                                                    (opts.max_duration,))
+                                keep_going = False
+                                break
+                            targets_observed.append(target.name)
                 if keep_going and len(targets_observed) == targets_before_loop:
                     user_logger.warning("No targets are currently visible - stopping script instead of hanging around")
                     keep_going = False
