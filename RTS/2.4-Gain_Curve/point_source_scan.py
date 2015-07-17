b1515757ed0191c542fc7dd403b72556e64a84f1
Ludwig Schwardt <ludwig@ska.ac.za>
2015-07-17 12:27:46 +0200

    New raster scan --style option
    Unify all the options and flags influencing the raster scan style into
    a single option called --style. This sets the number of scans, scan duration,
    scan extent and scan spacing, and also the default dump rate. The old options
    are now deprecated and the user is warned when using them (although they still
    work as expected for now).

--- RTS/2.4-Gain_Curve/point_source_scan.py
+++ RTS/2.4-Gain_Curve/point_source_scan.py
@@ -13,6 +13,23 @@ import time
 from katcorelib import collect_targets, standard_script_options, verify_and_connect, start_session, user_logger
 import katpoint
 
+
+# Raster scan styles
+styles = {
+  # Sources at L-band, default
+  'strong': dict(dump_rate=1.0, num_scans=5, scan_duration=30, scan_extent=6.0, scan_spacing=0.25),
+  # Weaker sources at L-band, requiring more time on source
+  'weak': dict(dump_rate=1.0, num_scans=5, scan_duration=60, scan_extent=4.0, scan_spacing=0.25),
+  # Either 'strong' or 'weak', only evaluated once the source flux is known
+  'auto': dict(),
+  # Sources at L-band, quick check
+  'quick': dict(dump_rate=2.0, num_scans=3, scan_duration=15, scan_extent=5.0, scan_spacing=0.5),
+  # Sources at Ku-band
+  'fine': dict(dump_rate=1.0, num_scans=5, scan_duration=60, scan_extent=1.0, scan_spacing=4./60.),
+  # Sources at Ku-band, wider search area
+  'search-fine': dict(dump_rate=4.0, num_scans=25, scan_duration=30, scan_extent=2.0, scan_spacing=5./60.),
+}
+
 # Set up standard script options
 parser = standard_script_options(
     usage="%prog [options] [<'target/catalogue'> ...]",
@@ -25,33 +42,50 @@ parser.add_option('-e', '--scan-in-elevation', action="store_true", default=Fals
                   help="Scan in elevation rather than in azimuth (default=%default)")
 parser.add_option('-m', '--min-time', type="float", default=-1.0,
                   help="Minimum duration to run experiment, in seconds (default=one loop through sources)")
-# Set default value for any option (both standard and experiment-specific options)
 parser.add_option('-z', '--skip-catalogue',
                   help="Name of file containing catalogue of targets to skip (updated with observed targets). "
                        "The default is not to skip any targets.")
-parser.add_option('--source-strength', type='choice', default='auto', choices=('strong', 'weak', 'auto'),
-                  help="Scanning strategy based on source strength, one of 'strong', 'weak' or 'auto' (default). "
-                       "Auto is based on flux density specified in catalogue.")
-parser.add_option( '--quick', action="store_true" , default=False,
-                  help='Do a quick "Zorro" type scan, which is 3 5-degree scans lasting 15 seconds each and '
-                       'spaced 0.5 degrees apart with 2 Hz dump rate.')
-parser.add_option( '--fine', action="store_true" , default=False,
-                  help='Do a fine grained pointscan with an extent of 1 degree and a duration of 60 seconds.'
-                  'The intention of this is for use in Ku-band obsevations where the beam is 8 arc-min .')
-
-parser.add_option( '--search-fine', action="store_true" , default=False,
-                  help='Do a fine grained pointscan with an extent of 2 degree and a duration of 30 seconds.'
-                  'The intention of this is for use in Ku-band obsevations where the beam is 8 arc-min .')
-
 parser.add_option('--no-delays', action="store_true", default=False,
                   help='Do not use delay tracking, and zero delays')
-
-parser.set_defaults(description='Point source scan')
+parser.add_option('--style', type='choice', default='auto', choices=styles.keys(),
+                  help="Raster scan style determining number of scans, scan duration, scan extent, "
+                       "scan spacing and default dump rate. Most options are tuned for L-band, while "
+                       "the 'fine' options are meant for Ku-band. The 'auto' option is either 'strong' "
+                       "or 'weak', depending on the source flux. The available styles are: %s" % (styles,))
+# Deprecated options
+parser.add_option('--source-strength', type='choice', default='none', choices=('strong', 'weak', 'auto', 'none'),
+                  help="Scanning strategy based on source strength, one of 'strong', 'weak' or 'auto' (default). "
+                       "Auto is based on flux density specified in catalogue. *DEPRECATED*")
+parser.add_option( '--quick', action="store_true" , default=False,
+                  help='Do a quick "Zorro" type scan, which is 3 5-degree scans lasting 15 seconds each and '
+                       'spaced 0.5 degrees apart with 2 Hz dump rate. *DEPRECATED*')
+parser.add_option( '--fine', action="store_true" , default=False,
+                  help='Do a fine-grained point scan with an extent of 1 degree and a duration of 60 seconds.'
+                  'This is for Ku-band observations where the beam is 8 arcmin. *DEPRECATED*')
+parser.add_option( '--search-fine', action="store_true" , default=False,
+                  help='Do a fine-grained point scan with an extent of 2 degrees and a duration of 30 seconds.'
+                  'This is for Ku-band observations where the beam is 8 arcmin. *DEPRECATED*')
+# Set default value for any option (both standard and experiment-specific options)
+parser.set_defaults(description='Point source scan', dump_rate=None)
 # Parse the command line
 opts, args = parser.parse_args()
 
+# Handle deprecated options
 if opts.quick:
-    opts.dump_rate = 2.0
+    user_logger.warning("The --quick option is deprecated, use --style=quick instead")
+    opts.style == 'quick'
+elif opts.fine:
+    user_logger.warning("The --fine option is deprecated, use --style=fine instead")
+    opts.style == 'fine'
+elif opts.search_fine:
+    user_logger.warning("The --search-fine option is deprecated, use --style=search-fine instead")
+    opts.style == 'search-fine'
+elif opts.source_strength != 'none':
+    user_logger.warning("The --source-strength=blah option is deprecated, use --style=blah instead")
+    opts.style = opts.source_strength
+# If dump rate is not specified, take it from the raster scan style
+if not opts.dump_rate:
+    opts.dump_rate = styles[opts.style].pop('dump_rate', 1.0)
 
 with verify_and_connect(opts) as kat:
     if not kat.dry_run and kat.ants.req.mode('STOP') :
@@ -113,31 +147,17 @@ with verify_and_connect(opts) as kat:
                 # Iterate through source list, picking the next one that is up
                 for target in pointing_sources.iterfilter(el_limit_deg=opts.horizon+7.0):
                     session.label('raster')
-                    user_logger.info("Doing scan of %r with current azel (%s,%s) "%(target.description,target.azel()[0],target.azel()[1]))
-                    # Do different raster scan depending on selected raster style
-                    if opts.quick:
-                        session.raster_scan(target, num_scans=3, scan_duration=15, scan_extent=5.0,
-                                            scan_spacing=0.5, scan_in_azimuth=not opts.scan_in_elevation,
-                                            projection=opts.projection)
-                    elif opts.fine:
-                        session.raster_scan(target, num_scans=5, scan_duration=60, scan_extent=1.0,
-                                            scan_spacing=4./60., scan_in_azimuth=not opts.scan_in_elevation,
-                                            projection=opts.projection)
-                    elif opts.search_fine:
-                        session.raster_scan(target, num_scans=25, scan_duration=30, scan_extent=2.0,
-                                            scan_spacing=5./60., scan_in_azimuth=not opts.scan_in_elevation,
-                                            projection=opts.projection)
+                    az, el = target.azel()
+                    user_logger.info("Doing scan of target %r with current azel (%s, %s)" % (target.description, az, el))
+                    if opts.style == 'auto':
+                        style = 'strong' if target.flux_density(opts.centre_freq) > 10.0 else 'weak'
                     else:
-                        if opts.source_strength == 'strong' or \
-                           (opts.source_strength == 'auto' and target.flux_density(opts.centre_freq) > 10.0):
-                            session.raster_scan(target, num_scans=5, scan_duration=30, scan_extent=6.0,
-                                                scan_spacing=0.25, scan_in_azimuth=not opts.scan_in_elevation,
-                                                projection=opts.projection)
-                        else:
-                            session.raster_scan(target, num_scans=5, scan_duration=60, scan_extent=4.0,
-                                                scan_spacing=0.25, scan_in_azimuth=not opts.scan_in_elevation,
-                                                projection=opts.projection)
-
+                        style = opts.style
+                    kwargs = styles[style]
+                    # Get rid of keyword arguments that are not meant for session.raster_scan
+                    kwargs.pop('dump_rate', None)
+                    session.raster_scan(target, scan_in_azimuth=not opts.scan_in_elevation,
+                                        projection=opts.projection, **kwargs)
                     targets_observed.append(target.name)
                     skip_file.write(target.description + "\n")
                     # The default is to do only one iteration through source list
