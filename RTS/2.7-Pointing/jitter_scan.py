31ba7067ac758a0f51cf96513bd810523cdf6fe3
Sean Passmoor <sean@ska.ac.za>
2015-11-12 16:13:28 +0200

    INclude a quick raster scan to ensure poining info , Add offset tracking in 2D , Change scan default parameters .

--- RTS/2.7-Pointing/jitter_scan.py
+++ RTS/2.7-Pointing/jitter_scan.py
@@ -13,22 +13,22 @@ parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'
                                  description='Track one or more sources for a specified time. At least one '
                                              'target must be specified. Note also some **required** options below.')
 # Add experiment-specific options
-parser.add_option('-t', '--track-duration', type='float', default=60.0,
+parser.add_option('-t', '--track-duration', type='float', default=20.0,
                   help='Length of time to track each source, in seconds (default=%default)')
 parser.add_option('-m', '--max-duration', type='float', default=None,
                   help='Maximum duration of the script in seconds, after which script will end '
                        'as soon as the current track finishes (no limit by default)')
-parser.add_option( '--max-extent', type='float', default=3.0,
+parser.add_option( '--max-extent', type='float', default=15./60.,
                   help='Maximum extent in degrees, the script will scan ')
 parser.add_option( '--number-of-steps', type='int', default=10,
                   help='Number of pointings to do while scaning , the script will scan ')
-parser.add_option('--repeat', action="store_true", default=False,
-                  help='Repeatedly loop through the targets until maximum duration (which must be set for this)')
 parser.add_option('--no-delays', action="store_true", default=False,
                   help='Do not use delay tracking, and zero delays')
+parser.add_option('-e', '--scan-in-elevation', action="store_true", default=False,
+                  help="Scan in elevation rather than in azimuth (default=%default)")
 
 # Set default value for any option (both standard and experiment-specific options)
-parser.set_defaults(description='Target track',dump_rate=0.1)
+parser.set_defaults(description='Target track',dump_rate=2.0)
 # Parse the command line
 opts, args = parser.parse_args()
 
@@ -80,35 +80,24 @@ with verify_and_connect(opts) as kat:
 
             session.standard_setup(**vars(opts))
             session.capture_start()
-
-            start_time = time.time()
-            targets_observed = []
-            # Keep going until the time is up
-            keep_going = True
-            while keep_going:
-                keep_going = (opts.max_duration is not None) and opts.repeat
-                targets_before_loop = len(targets_observed)
-                # Iterate through source list, picking the next one that is up
-                for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
+            for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):    
+                for offset_y in np.linspace(-opts.max_extent,opts.max_extent,opts.number_of_steps):           
+                    session.label('raster')
+                    user_logger.info("Doing scan of '%s' with current azel (%s,%s) "%(target.description,target.azel()[0],target.azel()[1]))
+                    session.raster_scan(target, num_scans=7, scan_duration=20, scan_extent=2.0*opts.max_extent,
+                                                scan_spacing=2./60., scan_in_azimuth=not opts.scan_in_elevation,
+                                                projection=opts.projection)
+                    session.label('track')
                     session.set_target(target) # Set the target
-                    session.track(target, duration=0, announce=False) # Set the target & mode = point
-                    for offset in np.linspace(opts.max_extent,0,opts.number_of_steps):
+                    session.track(target, duration=opts.track_duration, announce=False) # Set the target & mode = point
+                    for offset_x in np.linspace(-opts.max_extent,opts.max_extent,opts.number_of_steps):
                         session.label('track')
                         user_logger.info("Initiating %g-second track on target '%s'" % (opts.track_duration, target.name,))
-                        user_logger.info("Offset of %f,%f degrees " %(offset,0.0))
+                        user_logger.info("Offset of %f,%f degrees " %(offset_x,offset_y))
                         session.set_target(target)
-                        session.ants.req.offset_fixed(offset,0,opts.projection)
+                        session.ants.req.offset_fixed(offset_x,offset_y,opts.projection)
                         nd_params = session.nd_params
                         session.fire_noise_diode(announce=True, **nd_params)
                         time.sleep(opts.track_duration) # Snooze
-                    targets_observed.append(target.name)
-                    if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
-                        user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
-                                            (opts.max_duration,))
-                        keep_going = False
-                        break
-            
-                if keep_going and len(targets_observed) == targets_before_loop:
-                    user_logger.warning("No targets are currently visible - stopping script instead of hanging around")
-                    keep_going = False
-            user_logger.info("Targets observed : %d (%d unique)" % (len(targets_observed), len(set(targets_observed))))
+                
+
