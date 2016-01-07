2883c4c337a1b0160c4cf9a812a5fae6ee95399a
Sean Passmoor <sean@ska.ac.za>
2016-01-07 11:12:05 +0200

    fix of phase yscale and included a check to see if enough baselines are present

--- AR1/Phase_Amplitude_Closure/phase_amp_closure.py
+++ AR1/Phase_Amplitude_Closure/phase_amp_closure.py
@@ -41,7 +41,8 @@ def plot_phase_freq(channel_freqs,a123,title=''):
     fig = plt.figure(figsize=(20,10))
     plt.title(title)
     plt.plot(channel_freqs/1e6,np.degrees(a123) )
-    plt.ylim(-5,5)
+    #plt.ylim(-5,5)
+    plt.ylim(np.degrees(np.nanpercentile(a123,2)), np.degrees(np.nanpercentile(a123,98) ))
     plt.grid(True)
     plt.ylabel('Mean Phase Closure angle(degrees)')
     plt.xlabel('Frequency (MHz)')
@@ -99,18 +100,21 @@ for scan in h5.scans() :
         for i,(x,y)  in enumerate(zip(full_antA,full_antB)): # make lookup table
             up[x,y]=i
             up[y,x]=i
-        title = "%s : pol %s  , target=%s , %s "%(args[0].split('/')[-1],pol,scan[2].name,scan[1])
+        title = "filename:%s   pol=%s  , %s:%s  target=%s   samples=%i"%(args[0].split('/')[-1],pol,scan[0],scan[1],scan[2].name,np.shape(full_vis)[0])
+        
         l1,l2,l3 = phase_combinations(full_antA,up)
-        a123 =  anglemean(np.rollaxis(np.angle(full_vis[:,:,l1])-np.angle(full_vis[:,:,l2]) +np.angle(full_vis[:,:,l3]),0,2  ).reshape(full_vis.shape[1],-1)  ,axis=1 ) 
-        fig = plot_phase_freq(h5.channel_freqs,a123,title)
-        fig.savefig(pp,format='pdf')
-        plt.close(fig)
+        if np.shape(l1)[0] > 0 : 
+            a123 =  anglemean(np.rollaxis(np.angle(full_vis[:,:,l1])-np.angle(full_vis[:,:,l2]) +np.angle(full_vis[:,:,l3]),0,2  ).reshape(full_vis.shape[1],-1)  ,axis=1 ) 
+            fig = plot_phase_freq(h5.channel_freqs,a123,title)
+            fig.savefig(pp,format='pdf')
+            plt.close(fig)
  
         l1,l2,l3,l4  = amp_combinations(full_antA,up)
-        a1234 =  np.nanmean(np.rollaxis((np.abs(full_vis[:,:,l1])*np.abs(full_vis[:,:,l4]))/(np.abs(full_vis[:,:,l2])*np.abs(full_vis[:,:,l3] ) ) ,0,2).reshape(full_vis.shape[1],-1),axis=-1) 
-        fig = plot_amp_freq(h5.channel_freqs,a1234,title)
-        fig.savefig(pp,format='pdf')
-        plt.close(fig)
+        if np.shape(l1)[0] > 0 : 
+            a1234 =  np.nanmean(np.rollaxis((np.abs(full_vis[:,:,l1])*np.abs(full_vis[:,:,l4]))/(np.abs(full_vis[:,:,l2])*np.abs(full_vis[:,:,l3] ) ) ,0,2).reshape(full_vis.shape[1],-1),axis=-1) 
+            fig = plot_amp_freq(h5.channel_freqs,a1234,title)
+            fig.savefig(pp,format='pdf')
+            plt.close(fig)
 
 
 if opts.print_description :
