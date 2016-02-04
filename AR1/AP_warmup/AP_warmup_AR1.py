f842a541c32e21a6fdaee2001fb17cd53484dd5a
rubyvanrooyen <ruby.vanrooyen@gmail.com>
2016-02-04 11:03:35 +0200

    Cleaned up version of script

--- AR1/AP_warmup/AP_warmup_AR1.py
+++ AR1/AP_warmup/AP_warmup_AR1.py
@@ -9,17 +9,8 @@ from katcorelib import standard_script_options, verify_and_connect, collect_targ
 import katpoint
 
 # Set up standard script options
-parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]",
-                                 description='Track one or more sources for a specified time. At least one '
-                                             'target must be specified. Note also some **required** options below.')
-# # Add experiment-specific options
-# parser.add_option('-t', '--track-duration', type='float', default=60.0,
-#                   help='Length of time to track each source, in seconds (default=%default)')
-# parser.add_option('-m', '--max-duration', type='float', default=None,
-#                   help='Maximum duration of the script in seconds, after which script will end '
-#                        'as soon as the current track finishes (no limit by default)')
-# parser.add_option('--repeat', action="store_true", default=False,
-#                   help='Repeatedly loop through the targets until maximum duration (which must be set for this)')
+parser = standard_script_options(usage="%prog [options]",
+                                 description='Warming up the antennas if they are to cold to move.')
 parser.add_option('--no-delays', action="store_true", default=False,
                   help='Do not use delay tracking, and zero delays')
 
@@ -27,10 +18,6 @@ parser.add_option('--no-delays', action="store_true", default=False,
 parser.set_defaults(description='Target track',dump_rate=1)
 # Parse the command line
 opts, args = parser.parse_args()
-
-#if len(args) == 0:
-#    raise ValueError("Please specify at least one target argument via name ('Cygnus A'), "
-#                     "description ('azel, 20, 30') or catalogue file name ('sources.csv')")
 
 # Check options and build KAT configuration, connecting to proxies and devices
 with verify_and_connect(opts) as kat:
@@ -40,12 +27,6 @@ with verify_and_connect(opts) as kat:
     else:
          user_logger.error("Unable to set Antenna mode to 'STOP'.")
 
-#    observation_sources = collect_targets(kat, args)
-    # Quit early if there are no sources to observe
-#    if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
-#        user_logger.warning("No targets are currently visible - please re-run the script later")
-#    else:
-        # Start capture session, which creates HDF5 file
     with start_session(kat, **vars(opts)) as session:
         if not opts.no_delays and not kat.dry_run :
             if session.data.req.auto_delay('on'):
@@ -63,14 +44,9 @@ with verify_and_connect(opts) as kat:
                 user_logger.error('Unable to zero delay values.')
 
         session.standard_setup(**vars(opts))
-#         session.capture_start()
-# 
-#         start_time = time.time()
-#         targets_observed = []
-#             # Keep going until the time is up
 
-#   General: 4 Hz dumps, full speed movement.
-#   Test elevation scan: Az=0, El=15 to 60. Stop at 70 for 10 seconds. El from 70 to 15.
+# General: 4 Hz dumps, full speed movement.
+# Test elevation scan: Az=0, El=15 to 60. Stop at 70 for 10 seconds. El from 70 to 15.
 	continue_test=True
 	if not kat.dry_run:
             # User position sensors to make sure the system is in a safe place before starting movement
@@ -104,8 +80,9 @@ with verify_and_connect(opts) as kat:
             
                 kat.ants.req.mode('STOP')
                 time.sleep(5)
-#                 target1 = katpoint.Target('slew - back to origin Az=0 El=16, azel, 0, 16')    
-#                 user_logger.info("Initiating '%s'" % (target1.name))	
-#                 session.track(target1, duration=0)
-#                 kat.ants.req.mode('STOP')
-        
+                target1 = katpoint.Target('slew - back to origin Az=0 El=16, azel, 0, 16')    
+                user_logger.info("Initiating '%s'" % (target1.name))	
+                session.track(target1, duration=0)
+                kat.ants.req.mode('STOP')
+
+# -fin-      
