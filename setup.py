bbe5626ddc2d5438b45f6715b276f430ca3e175a
Ludwig Schwardt <ludwig@ska.ac.za>
2016-02-15 18:29:41 +0200

    Make setup.py pass PEP8 (no special formatting!)

--- setup.py
+++ setup.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python
 from setuptools import find_packages
-from numpy.distutils.core import  setup, Extension
+from numpy.distutils.core import setup, Extension
 
-setup (
-    name = "katsdpscripts",
-    description = "Karoo Array Telescope observation and reduction scripts",
-    author = "MeerKAT SDP, CAM and Commissioning Teams",
-    author_email = "spt@ska.ac.za",
-    packages = find_packages(),
-    include_package_data = True,
-    scripts = [
-        "RTS/Condition_Report/condition_report.py",
-        "RTS/RFI_Report/rfi_report.py",
-	"RTS/2.2-T_sys_T_nd/T_sys_T_nd_red.py",
-        "RTS/2.8-Strong_Sources/analyse_noise_diode.py",
-	"RTS/2.10-Receptor_Spectral_Baseline/analyse_spectrum.py"
-    ],
-    url = 'https://github.com/ska-sa/katsdpscripts',
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: Other/Proprietary License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
-    platforms = [ "OS Independent" ],
-    install_requires = ['numpy', 'katpoint', 'katcp'],
-    keywords = "meerkat ska",
-    zip_safe = False,
-    ext_modules = [Extension(name='gsm', sources=['RTS/gsm/gsm.f', ],
-                             extra_f77_compile_args=['-std=legacy -ffixed-line-length-0'])],
-    package_data = {'': ['RTS/gsm/gsm.f']}, 
-    setup_requires=['katversion'],
-    use_katversion=True,
-)
+
+setup(name="katsdpscripts",
+      description="Karoo Array Telescope observation and reduction scripts",
+      author="MeerKAT SDP, CAM and Commissioning Teams",
+      author_email="spt@ska.ac.za",
+      packages=find_packages(),
+      include_package_data=True,
+      scripts=[
+          "RTS/Condition_Report/condition_report.py",
+          "RTS/RFI_Report/rfi_report.py",
+          "RTS/2.2-T_sys_T_nd/T_sys_T_nd_red.py",
+          "RTS/2.8-Strong_Sources/analyse_noise_diode.py",
+          "RTS/2.10-Receptor_Spectral_Baseline/analyse_spectrum.py"],
+      url='https://github.com/ska-sa/katsdpscripts',
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: Other/Proprietary License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Scientific/Engineering :: Astronomy"],
+      platforms=["OS Independent"],
+      keywords="meerkat ska",
+      zip_safe=False,
+      ext_modules=[
+          Extension(name='gsm', sources=['RTS/gsm/gsm.f'],
+                    extra_f77_compile_args=['-std=legacy -ffixed-line-length-0'])],
+      package_data={'': ['RTS/gsm/gsm.f']},
+      setup_requires=['katversion'],
+      use_katversion=True,
+      install_requires=['numpy', 'katpoint', 'katcp'])
