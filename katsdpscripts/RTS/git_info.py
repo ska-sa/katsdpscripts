6ba1c79eab3e3ba319ba99feb84be719f18b06c8
Lindsay Magnus <lindsay@ska.ac.za>
2016-02-18 17:30:57 +0000

    have set the default git_info to only return the katsdpscripts git info that way we mimic the current functionality as close as possible ... i suggest that we get the team to add git_info(short=False) to logfiles

--- katsdpscripts/RTS/git_info.py
+++ katsdpscripts/RTS/git_info.py
@@ -1,12 +1,14 @@
 import sys
 
-def git_info():
+def git_info(short=True):
     """
     Helper function to get information about a github commit
     that was used to build the default KAT reduction packages.
     """
     mods = ['katholog','katpoint','scape','katdal','katsdpscripts']
     git_info_str = ''
+    if short:
+        return "katsdpscripts: %s\n"%(sys.modules['katsdpscripts'].__version__)
     for m in mods:
         try:
             git_info_str += "%s: %s\n"%(sys.modules[m].__name__,sys.modules[m].__version__)
