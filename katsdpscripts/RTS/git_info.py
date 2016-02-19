8c92b4d0857846d42b902920b5d00f0bfb29b027
Lindsay Magnus <lindsay@ska.ac.za>
2016-02-19 05:46:09 +0000

    have made the git_info more dynamic ... with no arguments it returns the katsdpscripts version, with standard it will return the five KAT module versions, finally if given a list it will return the versions of the modules in the list

--- katsdpscripts/RTS/git_info.py
+++ katsdpscripts/RTS/git_info.py
@@ -1,18 +1,30 @@
 import sys
 
-def git_info(short=True):
+def git_info(mods='short'):
     """
     Helper function to get information about a github commit
     that was used to build the default KAT reduction packages.
     """
-    mods = ['katholog','katpoint','scape','katdal','katsdpscripts']
+    standard_mods = ['katholog','katpoint','scape','katdal','katsdpscripts']
     git_info_str = ''
-    if short:
-        return "katsdpscripts: %s\n"%(sys.modules['katsdpscripts'].__version__)
-    for m in mods:
+
+    if mods == 'short':
         try:
-            git_info_str += "%s: %s\n"%(sys.modules[m].__name__,sys.modules[m].__version__)
+            git_info_str += "katsdpscripts: %s\n"%(sys.modules['katsdpscripts'].__version__)
         except KeyError:
-            git_info_str += "Module %s not imported\n"%m
+            git_info_str += "Module katsdpscripts is not imported\n"
+    elif mods == 'standard':
+        for m in standard_mods:
+            try:
+                git_info_str += "%s: %s\n"%(sys.modules[m].__name__,sys.modules[m].__version__)
+            except KeyError:
+                git_info_str += "Module %s is not imported\n"%m
+    elif isinstance(mods,list):
+        for m in mods:
+            try:
+                git_info_str += "%s: %s\n"%(sys.modules[m].__name__,sys.modules[m].__version__)
+            except KeyError:
+                git_info_str += "Module %s is not imported\n"%m 
+
     return git_info_str
 
