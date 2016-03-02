7b74b96d6508162230bc4cd9aacacbf1ab1d031a
Tom Mauch <tmauch@ska.ac.za>
2016-03-02 09:49:09 +0200

    Try and import modules before calling git info.

--- RTS/get_git_version.py
+++ RTS/get_git_version.py
@@ -5,5 +5,14 @@
 #call which will import katsdpscripts from a new environment and correctly report the 
 #installed version.
 
+#always import katsdpscripts
 from katsdpscripts import git_info
+
+#Attempt to import packages so that git_info can obtain their installed version
+for module in ['katholog','katpoint','katdal','scape']:
+	try:
+		__import__(module)
+	except:
+		pass
+
 print git_info('standard')
