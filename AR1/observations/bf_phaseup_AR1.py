6222508cacfb67e33cdeb2ad03d42475aa7e6000
Ludwig Schwardt <ludwig@ska.ac.za>
2016-04-18 16:31:03 +0200

    Remove fixed bandpass option and other fluff

--- AR1/observations/bf_phaseup_AR1.py
+++ AR1/observations/bf_phaseup_AR1.py
@@ -4,18 +4,17 @@
 # Obtain calibrated gains and apply them to the F-engine afterwards.
 
 import time
-from katcorelib.observe import standard_script_options, verify_and_connect, collect_targets, user_logger,start_session
-#from katcorelib import ant_array,standard_script_options, verify_and_connect, collect_targets, start_session, user_logger
-import katpoint
 import pickle
-import numpy as np
 import StringIO
-import logging 
-#import fbf_katcp_wrapper as fbf
+
+import numpy as np
+from katcorelib.observe import (standard_script_options, verify_and_connect,
+                                collect_targets, start_session, user_logger)
+import katpoint
 
 
 # read the bandpass solutions from a pickle file
-raw_data,bpass_h,bpass_v=pickle.load(open('/home/kat/comm/scripts/bpass.pikl'))
+# raw_data,bpass_h,bpass_v=pickle.load(open('/home/kat/comm/scripts/bpass.pikl'))
 
 # Set up standard script options
 parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]",
@@ -45,9 +44,6 @@ with verify_and_connect(opts) as kat:
     # Find out which inputs are currently active
     reply = kat.data.req.cbf_label_inputs()
     inputs = [m.arguments[0] for m in reply.messages[3:]]
-    user_logger.info("Resetting F-engine gains to %g to allow phasing up" % (opts.default_gain,))
-    for inp in inputs:
-        kat.data.req.cbf_gain(inp, opts.default_gain)
 
     # Quit early if there are no sources to observe
     if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
@@ -66,63 +62,61 @@ with verify_and_connect(opts) as kat:
                 else:
                     user_logger.error('Unable to turn off delay tracking.')
 
+            user_logger.info("Resetting F-engine gains to %g to allow phasing up" % (opts.default_gain,))
+            for inp in inputs:
+                kat.data.req.cbf_gain(inp, opts.default_gain)
+                #because we are phasing in the f-engine set the b-engine weights to 1
+                bf_weights_str = ' '.join(1024 * ['1'])
+                if inp[-1] == 'h':
+                    kat.data.req.cbf_beam_weights('beam_0x', inp, bf_weights_str)
+                else:
+                    kat.data.req.cbf_beam_weights('beam_0y', inp, bf_weights_str)
+
             session.standard_setup(**vars(opts))
             session.capture_start()
 
             start_time = time.time()
-            targets_observed = []
-            # Keep going until the time is up
-            keep_going = True
-            while keep_going:
-                for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
-                    if target.flux_model is None:
-                        user_logger.warning("Target has no flux model - stopping script")
-                        keep_going=False
-                        break
-                    target.add_tags('bpcal')
-                    session.label('track')
-                    user_logger.info("Initiating %g-second track on target '%s'" %
-                                     (opts.track_duration, target.name,))
-                    session.track(target, duration=opts.track_duration + 5, announce=False)
-                    # get and set the weights
-                    for inp in inputs:
-                        if inp[:-1] not in obs_ants : continue
-                        if pol == 'v':
-                            gains = bpass_v[inp[:-1]]
-                        else:
-                            gains = bpass_h[inp[:-1]]
-                        # gains = np.hstack((np.zeros(1), gains))
-                        gains = np.r_[0.0, gains]
-                        weights = getattr(kat.data.sensor,'k7w_'+inp+'_gain_correction_per_channel').get_reading().value
-                        update = getattr(kat.data.sensor,'k7w_'+inp+'_gain_correction_per_channel').get_reading().timestamp
-                        user_logger.info("Gain sensors updated at %s"%katpoint.Timestamp(update).local())
-                        f = StringIO.StringIO(weights)
-                        orig_weights = np.loadtxt(f, dtype=np.complex,delimiter=' ')
-                        amp_weights = np.abs(orig_weights)
-                        phase_weights = orig_weights / amp_weights
-                        ind = np.repeat(False,1024)
-                        # here is where you hack things to get "fuller" band
-                        #ind[slice(10,1000)]=True # this will not work! but do not have time to repair that
-                        ind[slice(200,800)]=True
-                        gains[~ind] = 160.0
-                        N = phase_weights[ind].shape[0]
-                        z = np.polyfit(np.arange(N),np.unwrap(np.angle(phase_weights)[ind]),1)
-                        #print z
-                        phase = np.zeros(1024)
-                        #phase[ind] = np.angle(phase_weights[ind])
-                        phase[ind] = z[0]*np.arange(N)+z[1]
-                        new_weights = (160.0 / gains ) * np.exp(1j * phase)
-                        weights_str = ' '.join([('%+5.3f%+5.3fj' % (w.real,w.imag)) for w in new_weights])
-                        kat.data.req.cbf_gain(inp, weights_str)
-                        #because we are phasing in the f-engine set the b-engine weights to 1
-                        bf_weights_str = ' '.join(1024 * ['1'])
-                        if pol == 'h':
-                            kat.data.req.cbf_beam_weights('beam_0x', inp, bf_weights_str)
-                        else:
-                            kat.data.req.cbf_beam_weights('beam_0y', inp, bf_weights_str)
-                    user_logger.info("Initiating %g-second track on target '%s'" % (60,target.name,))
-                    session.track(target, duration=60, announce=False)
-                keep_going = False
+            for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
+                if target.flux_model is None:
+                    user_logger.warning("Target has no flux model - stopping script")
+                    break
+                target.add_tags('bpcal')
+                session.label('track')
+                user_logger.info("Initiating %g-second track on target '%s'" %
+                                 (opts.track_duration, target.name,))
+                session.track(target, duration=opts.track_duration + 5, announce=False)
+                # get and set the weights
+                for inp in inputs:
+                    ant, pol = inp[:-1], inp[-1]
+                    if ant not in obs_ants:
+                        continue
+                    # if pol == 'v':
+                    #     gains = bpass_v[inp[:-1]]
+                    # else:
+                    #     gains = bpass_h[inp[:-1]]
+                    # # gains = np.hstack((np.zeros(1), gains))
+                    # gains = np.r_[0.0, gains]
+                    weights = getattr(kat.data.sensor,'k7w_'+inp+'_gain_correction_per_channel').get_reading().value
+                    update = getattr(kat.data.sensor,'k7w_'+inp+'_gain_correction_per_channel').get_reading().timestamp
+                    user_logger.info("Gain sensors updated at %s"%katpoint.Timestamp(update).local())
+                    # f = StringIO.StringIO(weights)
+                    # orig_weights = np.loadtxt(f, dtype=np.complex,delimiter=' ')
+                    # amp_weights = np.abs(orig_weights)
+                    # phase_weights = orig_weights / amp_weights
+                    # ind = np.repeat(False,1024)
+                    # # here is where you hack things to get "fuller" band
+                    # #ind[slice(10,1000)]=True # this will not work! but do not have time to repair that
+                    # ind[slice(200,800)]=True
+                    # gains[~ind] = 160.0
+                    # N = phase_weights[ind].shape[0]
+                    # z = np.polyfit(np.arange(N),np.unwrap(np.angle(phase_weights)[ind]),1)
+                    # #print z
+                    # phase = np.zeros(1024)
+                    # #phase[ind] = np.angle(phase_weights[ind])
+                    # phase[ind] = z[0]*np.arange(N)+z[1]
+                    # new_weights = (160.0 / gains ) * np.exp(1j * phase)
+                    weights_str = ' '.join([('%+5.3f%+5.3fj' % (w.real,w.imag)) for w in new_weights])
+                    kat.data.req.cbf_gain(inp, weights_str)
             if opts.reset:
                 user_logger.info("Resetting F-engine gains to %g" % (opts.default_gain,))
                 for inp in inputs:
