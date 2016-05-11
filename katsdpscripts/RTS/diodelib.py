a66af4f997f736ce93d52596f6a76e2269a31600
Sean Passmoor <sean@ska.ac.za>
2016-05-11 14:18:21 +0200

    Revert diodelib.py

--- katsdpscripts/RTS/diodelib.py
+++ katsdpscripts/RTS/diodelib.py
@@ -8,110 +8,13 @@ import pickle
 from katsdpscripts import git_info
 from scipy.signal import medfilt
 import logging
+import scape
 
-
-def save_ND(diode_filename,file_base,freq,Tdiode_pol ):
-    outfilename = diode_filename.split('/')[-1]
-    outfile = file(outfilename, 'w')
-    outfile.write('#Data from %s\n# Frequency [Hz], Temperature [K]\n'%file_base)
-    # Write CSV part of file
-    outfile.write(''.join(['%s, %s\n' % (entry[0], entry[1]) for entry in zip(freq,medfilt(Tdiode_pol))]))
-    outfile.close()
-    return outfilename
-    
-
-def get_nd_on_off(h5,buff = 5,log=None): 
-    on = h5.sensor['Antennas/%s/nd_coupler'%(h5.ants[0].name)]
-    n_on = np.tile(False,on.shape[0])
-    n_off = np.tile(False,on.shape[0])
-    if not any(on):
-        if log is not None:
-            log.critical('No noise diode fired during track of %s'%target)
-        else :
-            print('No noise diode fired during track of %s'%target)
-    else:
-        jumps = (np.diff(on).nonzero()[0] + 1).tolist()
-        n_on[slice(jumps[0]+buff,jumps[1]-buff)] = True
-        n_off[slice(jumps[1]+buff,-buff)] = True
-    return n_on,n_off
-
-
-def plot_Tsys_eta_A(freq,Tsys,eta_A,TAc,Tsys_std=None,ant = '', file_base='.'):
-    fig = plt.figure(figsize=(20,5))
-    pols = ['v','h']
-    for p,pol in enumerate(pols) : 
-        fig.add_subplot(1,2,p+1)  
-        ax = plt.gca()
-        ax.text(0.95, 0.01,git_info(), horizontalalignment='right',fontsize=10,transform=ax.transAxes)
-        plt.title('%s $T_{sys}/eta_{A}$: %s pol: %s'%(ant,str(pol).upper(),file_base))
-        plt.ylabel("$T_{sys}/eta_{A}$ [K]")
-        plt.xlabel('f [MHz]')
-        #if p == ant_num * 2 -1: plt.ylabel(ant)
-        if Tsys_std is not None :
-            plt.errorbar(freq/1e6,Tsys,Tsys_std,color = 'b',linestyle = '.',label='Measurement')
-        plt.plot(freq/1e6,Tsys/eta_A,'b.',label='Measurement: Y-method')
-        if not(Ku): plt.plot(freq/1e6,TAc/eta_A,'c.',label='Measurement: ND calibration')
-        plt.axhline(np.mean(Tsys/eta_A),linewidth=2,color='k',label='Mean: Y-method')
-        if freq.min() < 2090e6:
-            D = 13.5
-            Ag = np.pi* (D/2)**2 # Antenna geometric area
-            spec_Tsys_eta = np.zeros_like(freq)
-            plt.ylim(15,50)
-            plt.xlim(900,1670)
-            spec_Tsys_eta[freq<1420e6] =  42 # [R.T.P095] == 220
-            spec_Tsys_eta[freq>=1420e6] =  46 # [R.T.P.096] == 200
-            plt.plot(freq/1e6, spec_Tsys_eta,'r',linewidth=2,label='PDR Spec')
-            plt.plot(freq/1e6,np.interp(freq/1e6,[900,1670],[(64*Ag)/275.0,
-                    (64*Ag)/410.0]),'g',linewidth=2,label="275-410 m^2/K at Receivers CDR")
-            plt.grid()
-            plt.legend(loc=2,fontsize=12)
-            
-    return fig   
-
-def plot_nd(freq,Tdiode,nd_temp,ant = '', file_base=''): 
-    fig = plt.figure(figsize=(20,5))
-    pols = ['v','h']
-    for p,pol in enumerate(pols) : 
-        fig.add_subplot(1,2,p+1) # 
-        ax = plt.gca()
-        ax.text(0.95, 0.01,git_info(), horizontalalignment='right',fontsize=10,transform=ax.transAxes)
-        plt.title('%s Coupler Diode: %s pol: %s'%(ant,str(pol).upper(),file_base))
-        plt.ylim(0,50)
-        plt.ylabel('$T_{ND}$ [K]')
-        plt.xlim(900,1670)
-        plt.xlabel('f [MHz]')
-        #plt.ylabel(ant)
-        plt.axhspan(14, 35, facecolor='g', alpha=0.5)
-        plt.plot(freq/1e6,Tdiode[pol],'b.',label='Measurement: Y-method')
-        plt.plot(freq/1e6,nd_temp[pol],'k.',label='Model: EMSS')
-        plt.grid()
-        plt.legend()
-    return fig   
-
-def plot_ts(h5,on_ts=None):
-    import scape
-    fig = plt.figure(figsize=(20,5))
-    a = h5.ants[0]
-    nd = scape.gaincal.NoiseDiodeModel(freq=[856,1712],temp=[20,20])
-    d = scape.DataSet(h5,nd_h_model = nd, nd_v_model=nd )
-    scape.plot_xyz(d,'time','amp',label='Average of the data')
-    if on_ts is not None :
-        on = on_ts
-    else :
-        on = h5.sensor['Antennas/'+a.name+'/nd_coupler']
-    ts = h5.timestamps - h5.timestamps[0]
-    plt.plot(ts,on*4000,'g',label='katdal ND sensor')
-    plt.title("Timeseries for antenna %s - %s"%(a.name,git_info()))
-    plt.legend()
-    return fig
-
-def read_and_plot_data(filename,output_dir='.',pdf=True,Ku = False,
-                        verbose = False,error_bars=False,target='off1',
-                        write_nd=False,rfi_mask='/var/kat/katsdpscripts/RTS/rfi_mask.pickle',**kwargs):
+def read_and_plot_data(filename,output_dir='.',pdf=True,Ku = False,verbose = False,error_bars=False,target='off1',write_nd=False,**kwargs):
     print 'inside',kwargs
     file_base = filename.split('/')[-1].split('.')[0]
     nice_filename =  file_base + '_T_sys_T_nd'
-    
+
     # Set up logging: logging everything (DEBUG & above), both to console and file
     logger = logging.root
     logger.setLevel(logging.DEBUG)
@@ -120,45 +23,54 @@ def read_and_plot_data(filename,output_dir='.',pdf=True,Ku = False,
     fh.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
     logger.addHandler(fh)
     logger.info('Beginning data processing with:\n%s'%git_info('standard'))
-    
-    if Ku:
-        logger.debug("Using Ku band ... unsetting L band RFI flags")
-        h5 = katfile.open(filename,centre_freq = 12500.5e6 , **kwargs)
-        # Don't subtract half a channel width as channel 0 is centred on 0 Hz in baseband
-        static_flags = np.tile(True,4096)
-    else :
-        h5 = katfile.open(filename,**kwargs)
-        pickle_file = open(rfi_mask)
-        rfi_static_flags = pickle.load(pickle_file)
-        pickle_file.close()
+
+    h5 = katfile.open(filename,**kwargs)
     if verbose: logger.debug(h5.__str__())
+    ants = h5.ants
+   
+    pickle_file = open('/var/kat/katsdpscripts/RTS/rfi_mask.pickle')
+    rfi_static_flags = pickle.load(pickle_file)
+    pickle_file.close()
     edge = np.tile(True,4096)
     edge[slice(211,3896)] = False
     static_flags = np.logical_or(edge,rfi_static_flags)
-    
-    ants = h5.ants
+    if Ku:
+        logger.debug("Using Ku band ... unsetting L band RFI flags")
+        h5.spectral_windows[0].centre_freq = 12500.5e6
+        # Don't subtract half a channel width as channel 0 is centred on 0 Hz in baseband
+        h5.spectral_windows[0].channel_freqs = h5.spectral_windows[0].centre_freq +  h5.spectral_windows[0].channel_width * (np.arange(h5.spectral_windows[0].num_chans) - h5.spectral_windows[0].num_chans / 2)
+        static_flags = edge    
 
-   
-    
+    n_ants = len(ants)
+    ant_ind = np.arange(n_ants)
     colour = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
     pols = ['v','h']
-    
+    diode= 'coupler'
     for a,col in zip(ants,colour):
-        if pdf:
+        if pdf: 
             pp = PdfPages(output_dir+'/'+nice_filename+'.'+a.name+'.pdf')
             logger.debug("Created output PDF file: %s"%output_dir+'/'+nice_filename+'.'+a.name+'.pdf')
 
-        #fig0 = plt.figure(0,figsize=(20,5))
+        if not(Ku): 
+            fig1 = plt.figure(2,figsize=(20,5))
+        fig2 = plt.figure(1,figsize=(20,5))
+        
+        fig0 = plt.figure(0,figsize=(20,5))
         h5.select()
         h5.select(ants = a.name,channels=~static_flags)
-        fig0 = plot_ts(h5)
-        Tdiode = {}
-        nd_temp = {}
+        d = scape.DataSet(h5)
+        scape.plot_xyz(d,'time','amp',label='Average of the data')
+        on = h5.sensor['Antennas/'+a.name+'/nd_coupler']
+        ts = h5.timestamps - h5.timestamps[0]
+        plt.plot(ts,on*4000,'g',label='katdal ND sensor')
+        plt.title("Timeseries for antenna %s - %s"%(a.name,git_info()))
+        plt.legend()
         for pol in pols:
             logger.debug("Processing: %s%s"%(a.name,pol))
-            ant = a.name            
-            Tsys_std = None
-            #air_temp = np.mean(h5.sensor['Enviro/air_temperature'])
+            ant = a.name
+            ant_num = int(ant[3])
+            
+            air_temp = np.mean(h5.sensor['Enviro/air_temperature'])
             if not(Ku):
                 try:
                     rx_sn = h5.receivers[ant]
@@ -174,31 +86,58 @@ def read_and_plot_data(filename,output_dir='.',pdf=True,Ku = False,
                     logger.error("Be sure to reprocess the data once the file is in the config")
                     nd = scape.gaincal.NoiseDiodeModel(freq=[856,1712],temp=[20,20])
             
+            s = h5.spectral_windows[0]
+            f_c = s.centre_freq
             #cold data
             logger.debug('Using off target %s'%target)
             h5.select(ants=a.name,pol=pol,channels=~static_flags, targets = target,scans='track')
             freq = h5.channel_freqs
-            
+            if not(Ku): nd_temp = nd.temperature(freq / 1e6)
             cold_data = h5.vis[:].real
-            cold_on,cold_off = get_nd_on_off(h5,log=logger)
+            on = h5.sensor['Antennas/'+ant+'/nd_coupler']
+            n_on = np.tile(False,on.shape[0])
+            n_off = np.tile(False,on.shape[0])
+            buff = 5
+            if not any(on):
+                logger.critical('No noise diode fired during track of %s'%target)
+            else:
+                jumps = (np.diff(on).nonzero()[0] + 1).tolist()
+                n_on[slice(jumps[0]+buff,jumps[1]-buff)] = True
+                n_off[slice(jumps[1]+buff,-buff)] = True
+
+            cold_off = n_off
+            cold_on = n_on
             #hot data
             h5.select(ants=a.name,pol=pol,channels=~static_flags,targets = 'Moon',scans='track')
-            hot_on,hot_off = get_nd_on_off(h5,log=logger)
             hot_data = h5.vis[:].real
+            on = h5.sensor['Antennas/'+ant+'/nd_coupler']
+            n_on = np.tile(False,on.shape[0])
+            n_off = np.tile(False,on.shape[0])
+            if not any(on):
+                logger.critical('No noise diode fired during track of %s'%target)
+            else:
+                jumps = (np.diff(on).nonzero()[0] + 1).tolist()
+                n_on[slice(jumps[0]+buff,jumps[1]-buff)] = True
+                n_off[slice(jumps[1]+buff,-buff)] = True
 
+            hot_off = n_off
+            hot_on = n_on
             cold_spec = np.mean(cold_data[cold_off,:,0],0)
             hot_spec = np.mean(hot_data[hot_off,:,0],0)
             cold_nd_spec = np.mean(cold_data[cold_on,:,0],0)
             hot_nd_spec = np.mean(hot_data[hot_on,:,0],0)
 
+            if error_bars:
+                cold_spec_std = np.std(cold_data[cold_off,:,0],0)
+                hot_spec_std = np.std(hot_data[hot_off,:,0],0)
+                cold_nd_spec_std = np.std(cold_data[cold_on,:,0],0)
+                hot_nd_spec_std = np.std(hot_data[hot_on,:,0],0)
+             
             if not(Ku):
-                nd_temp[pol] = nd.temperature(freq / 1e6)
-                # antenna temperature on the moon (from diode calibration)
-                TAh = hot_spec/(hot_nd_spec - hot_spec) * nd_temp[pol] 
-                # antenna temperature on cold sky (from diode calibration) (Tsys)
-                TAc = cold_spec/(cold_nd_spec - cold_spec) * nd_temp[pol] 
-                print("Mean TAh = %f  mean TAc = %f "%(TAh.mean(),TAc.mean()))
+                TAh = hot_spec/(hot_nd_spec - hot_spec) * nd_temp # antenna temperature on the moon (from diode calibration)
+                TAc = cold_spec/(cold_nd_spec - cold_spec) * nd_temp # antenna temperature on cold sky (from diode calibration) (Tsys)
             Y = hot_spec / cold_spec
+            if error_bars: Y_std = Y * np.sqrt((hot_spec_std/hot_spec)**2 + (cold_spec_std/cold_spec)**2)
             D = 13.5
             lam = 3e8/freq
             HPBW = 1.18 *(lam/D)
@@ -213,34 +152,82 @@ def read_and_plot_data(filename,output_dir='.',pdf=True,Ku = False,
             x = 2*R/HPBW # ratio of source to beam
             K = ((x/1.2)**2) / (1-np.exp(-((x/1.2)**2))) # correction factor for disk source from Baars 1973
             TA_moon = 225 * (Os*Ae/(lam**2)) * (1/K) # contribution from the moon (disk of constant brightness temp)
+            if error_bars: Thot_std = 2.25
             gamma = 1.0
+            if error_bars: gamma_std = 0.01
             Tsys = gamma * (TA_moon)/(Y-gamma) # Tsys from y-method ... compare with diode TAc
-            if error_bars:
-                cold_spec_std = np.std(cold_data[cold_off,:,0],0)
-                hot_spec_std = np.std(hot_data[hot_off,:,0],0)
-                cold_nd_spec_std = np.std(cold_data[cold_on,:,0],0)
-                hot_nd_spec_std = np.std(hot_data[hot_on,:,0],0)
-                Y_std = Y * np.sqrt((hot_spec_std/hot_spec)**2 + (cold_spec_std/cold_spec)**2)
-                Thot_std = 2.25
-                gamma_std = 0.01
-                 # This is not definded
-                raise NotImplementedError("The factor Thot  has not been defined ")
-                Tsys_std = Tsys * np.sqrt((Thot_std/Thot)**2 + (Y_std/Y)**2 + (gamma_std/gamma)**2)
-            else :
-                Tsys_std = None
+            if error_bars: Tsys_std = Tsys * np.sqrt((Thot_std/Thot)**2 + (Y_std/Y)**2 + (gamma_std/gamma)**2)
             if not(Ku):
                 Ydiode = hot_nd_spec / hot_spec
-                Tdiode[pol] = (TA_moon + Tsys)*(Ydiode/gamma-1)
-            if write_nd:
-                outfilename = save_ND(diode_filename,file_base,freq,Tdiode[pol] )
-                logger.info('Noise temp data written to file %s'%outfilename)
+                Tdiode = (TA_moon + Tsys)*(Ydiode/gamma-1)
+            
+            p = 1 if pol == 'v' else 2
+            if not(Ku):
+                plt.figure(2)
+                plt.subplot(1,2,p)
+                plt.ylim(0,50)
+                plt.ylabel('T_ND [K]')
+                plt.xlim(900,1670)
+                plt.xlabel('f [MHz]')
+                if p ==ant_num * 2-1: plt.ylabel(ant)
+                plt.axhspan(14, 35, facecolor='g', alpha=0.5)
+                plt.plot(freq/1e6,Tdiode,'b.',label='Measurement: Y-method')
+                if write_nd:
+                    outfilename = diode_filename.split('/')[-1]
+                    outfile = file(outfilename, 'w')
+                    outfile.write('#Data from %s\n# Frequency [Hz], Temperature [K]\n'%file_base)
+                    # Write CSV part of file
+                    outfile.write(''.join(['%s, %s\n' % (entry[0], entry[1]) for entry in zip(freq,medfilt(Tdiode))]))
+                    outfile.close()
+                    logger.info('Noise temp data written to file %s'%outfilename)
+                plt.plot(freq/1e6,nd_temp,'k.',label='Model: EMSS')
+                plt.grid()
+                plt.legend()
+                
+            plt.figure(1)
+            plt.subplot(1,2,p)
+            if not(Ku): plt.ylim(15,50)
+            plt.ylabel('Tsys/eta_A [K]')
+            if not(Ku): plt.xlim(900,1670)
+            plt.xlabel('f [MHz]')
+            if p == ant_num * 2 -1: plt.ylabel(ant)
+            if error_bars: plt.errorbar(freq/1e6,Tsys,Tsys_std,color = 'b',linestyle = '.',label='Measurement')
+            plt.plot(freq/1e6,Tsys/eta_A,'b.',label='Measurement: Y-method')
+            if not(Ku): plt.plot(freq/1e6,TAc/eta_A,'c.',label='Measurement: ND calibration')
+            plt.axhline(np.mean(Tsys/eta_A),linewidth=2,color='k',label='Mean: Y-method')
+            spec_Tsys_eta = 0*freq
+            spec_Tsys_eta[freq<1420e6] =  42 # [R.T.P095] == 220
+            spec_Tsys_eta[freq>=1420e6] =  46 # [R.T.P.096] == 200
+            if not(Ku): plt.plot(freq/1e6, spec_Tsys_eta,'r',linewidth=2,label='PDR Spec')
+            if not(Ku): plt.plot(freq/1e6,np.interp(freq/1e6,[900,1670],[(64*Ag)/275.0,(64*Ag)/410.0]),'g',linewidth=2,label="275-410 m^2/K at Receivers CDR")
 
-        fig2 = plot_nd(freq,Tdiode,nd_temp,ant = ant, file_base=file_base)
-        fig1 = plot_Tsys_eta_A(freq,Tsys,eta_A,TAc,Tsys_std=Tsys_std,ant = ant, file_base=file_base)
+            plt.grid()
+            plt.legend(loc=2,fontsize=12)
+        
+        if not(Ku):
+            plt.figure(2)
+            plt.subplot(1,2,1)
+            ax = plt.gca()
+            ax.text(0.95, 0.01,git_info(), horizontalalignment='right',fontsize=10,transform=ax.transAxes)
+            plt.title('%s Coupler Diode: V pol: %s'%(ant,file_base))
+            plt.subplot(1,2,2)
+            ax = plt.gca()
+            ax.text(0.95, 0.01,git_info(), horizontalalignment='right',fontsize=10,transform=ax.transAxes)
+            plt.title('%s Coupler Diode: H pol: %s'%(ant,file_base))
+
+        plt.figure(1)
+        plt.subplot(1,2,1)
+        ax = plt.gca()
+        ax.text(0.95, 0.01,git_info(), horizontalalignment='right',fontsize=10,transform=ax.transAxes)
+        plt.title('%s Tsys/eta_A: V pol: %s'%(ant,file_base))
+        plt.subplot(1,2,2)
+        ax = plt.gca()
+        ax.text(0.95, 0.01,git_info(), horizontalalignment='right',fontsize=10,transform=ax.transAxes)
+        plt.title('%s Tsys/eta_A: H pol: %s'%(ant,file_base))
         if pdf:
             if not(Ku):
-                fig2.savefig(pp,format='pdf')
-            fig1.savefig(pp,format='pdf')
+                fig1.savefig(pp,format='pdf')
+            fig2.savefig(pp,format='pdf')
             fig0.savefig(pp,format='pdf')
             pp.close() # close the pdf file
             plt.close("all")
@@ -251,20 +238,13 @@ def read_and_plot_data(filename,output_dir='.',pdf=True,Ku = False,
 # test main method for the library
 if __name__ == "__main__":
 #test the method with a know file
-    filename = '/var/kat/archive/data/RTS/telescope_products/2016/01/19/1453216690.h5'
+    filename = '/var/kat/archive/data/RTS/telescope_products/2015/04/22/1429706275.h5'
     pdf=True
     Ku=False
     verbose=False
     out = '.'
     error_bars = False
     target = 'off1'
-    write_nd = True
+    write_nd = False
     print 'Performing test run with: ' + filename
     read_and_plot_data(filename,out,pdf,Ku,verbose,error_bars,target,write_nd)
-# Output checsums of the noisediode files
-#md5sum rx.* 
-#7545907bbe621f4e5937de7536be21f3  rx.l.4002.h.csv
-#532332722c8bca9714f4e2d97978ccd6  rx.l.4002.v.csv
-#66678871aeaadc4178e4420bef9861aa  rx.l.4.h.csv
-#af113bd4ab10bb61386521f61eb4d716  rx.l.4.v.csv
-
