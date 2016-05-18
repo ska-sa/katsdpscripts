249c1a7370ed515346bc183bd5784ef87df4850f
neiljyoung <young.neiljames@gmail.com>
2016-05-18 12:22:25 +0200

    Tidied up fbank header creation

--- AR1/reductions/beamformer/fbankHDF5.py
+++ AR1/reductions/beamformer/fbankHDF5.py
@@ -10,6 +10,19 @@ from matplotlib.ticker import AutoMinorLocator
 #################################################
 # MeerKAT CBF HDF5 -> filterbank file converter.
 #################################################
+# Functions used in SIGPROC header creation
+def _write_string(key, value):
+    return "".join([struct.pack("I", len(key)), key, struct.pack("I", len(value)), value])
+
+def _write_int(key, value):
+    return "".join([struct.pack("I",len(key)), key, struct.pack("I", value)])
+
+def _write_double(key, value):
+    return "".join([struct.pack("I",len(key)), key, struct.pack("d", value)])
+
+def _write_char(key, value):
+    return "".join([struct.pack("I",len(key)), key, struct.pack("b", value)])
+
 def get_times(adc_count,sync_time):  # calculate time info
     """Define function to get MJD time from ADC samples."""
     sf = 1712.0e6
@@ -50,10 +63,12 @@ if __name__=="__main__":
     description='Convert HDF5 data to the SIGPROC filterbank format.')
     parser.formatter.max_help_position = 50
     parser.formatter.width = 200
-    parser.add_option("-c", "--chanOff", type="float", default=-0.208984, 
-    help="Channel bandwidth override (def = %default MHz).")
-    parser.add_option("-f", "--fch1", type="float", default=1444.4, 
-    help="Top-of-band channel centre frequency override (def = %default MHz).")
+    parser.add_option("-c", "--chbw", action='store_false', default=True, 
+    help="Channel bandwidth inversion override (def = %default).")
+    parser.add_option("-d", "--dec", type='string', default="-45:10:34.8751", 
+    help="DECJ override (def = %default).")   
+    parser.add_option("-f", "--freq", type="float", default=1391.0, 
+    help="Centre frequency override (def = %default MHz).")
     parser.add_option("-g", "--get-fig", action="store_false", default=True, 
     help="Figure output override switch (def = %default).")
     parser.add_option("--i0", type="string", default='/mnt/ramdisk0/1463411901.h5', 
@@ -64,6 +79,8 @@ if __name__=="__main__":
     help="Decimation factor override (def = %default).") 
     parser.add_option("-o", "--outfile", type="string", default=None, 
     help="Output file override (default = %default).")
+    parser.add_option("-r", "--ra", type='string', default="08:35:20.61149", 
+    help="DECJ override (def = %default).")   
     parser.add_option("-s", "--source", type="string", default='J0835-4510', 
     help="Source override (default = %default).")
     parser.add_option("-S", "--sync_time", type="float", default=None, 
@@ -137,9 +154,9 @@ if __name__=="__main__":
         print ' New Nsamp for H = %i and V = %i' %(t0s.size,t1s.size)
         print ' New Tobs = %.3f s' %(Nsamp*tsamp)    
 
-    #################################################################
-    # Check decimation factor and write out sigproc header to fbank.
-    #################################################################
+    ####################################################
+    # Check decimation factor and define header params.
+    ####################################################
     if ( opts.ndec > 1 ): 
         check = power_of_two(opts.ndec)
         if not check or opts.ndec > 256:
@@ -147,19 +164,57 @@ if __name__=="__main__":
             sys.exit(1)
     Nsamp = np.int_(float(Nsamp)/opts.ndec)
     tsamp *= opts.ndec
+
+    chBW = 1712/8192. # MHz
+    freqTop = opts.freq + (((nchan / 2) - 1) * chBW)
+    freqBottom = opts.freq - (((nchan / 2)) * chBW)
+    if opts.chbw: 
+        fBottom = opts.freq + (((nchan / 2) - 1) * chBW)
+        fTop = opts.freq - (((nchan / 2)) * chBW)
+        chBW *= -1
+    else:
+        fTop = opts.freq + (((nchan / 2) - 1) * chBW)
+        fBottom = opts.freq - (((nchan / 2)) * chBW)
+    print ' fBottom = %.3f MHz, fTop = %.3f MHz, chBW = %.5f MHz' %(fBottom,fTop,chBW)
     print ' tsamp to be used = %.6f ms' %(tsamp*1e3)
 
+    ########################################
+    # Write out file header to fbank file.
+    #########################################
+    RAJ = opts.ra
+    DECJ = opts.dec
     outfile = opts.outfile if opts.outfile != None else opts.i0.split('.h5')[0] + '.fil'
-    hdr_cmd = '/home/kat/software/mockHeader -tel 64 -mach 64 -type 1 -raw %s -source %s -tstart %.8f -nbits 32 ' %(opts.i0,opts.source,MJDstart)
-    hdr_cmd += '-nifs 1 -tsamp %.10f -fch1 %.6f -fo %.6f -nchans %i %s' %(tsamp,opts.fch1,opts.chanOff,nchan,outfile)
-    os.system(hdr_cmd)
+    f_handle = open(outfile, "ab+")
+    headerStart = "HEADER_START"
+    headerEnd = "HEADER_END"
+    header = "".join([struct.pack("I", len(headerStart)), headerStart])
+    header = "".join([header, _write_string("source_name", opts.source)])
+    header = "".join([header, _write_int("machine_id", 64)])
+    header = "".join([header, _write_int("telescope_id", 64)])
+    src_raj = float(RAJ.replace(":", ""))
+    header = "".join([header, _write_double("src_raj", src_raj)])
+    src_dej = float(DECJ.replace(":", ""))
+    header = "".join([header, _write_double("src_dej", src_dej)])
+    header = "".join([header, _write_int("data_type", 1)])
+    header = "".join([header, _write_double("fch1", fBottom)])
+    header = "".join([header, _write_double("foff", chBW)])
+    header = "".join([header, _write_int("nchans", nchan)])
+    header = "".join([header, _write_int("nbits", 32)])
+    header = "".join([header, _write_double("tstart", MJDstart)])
+    header = "".join([header, _write_double("tsamp", tsamp)])
+    header = "".join([header, _write_int("nifs", 1)])
+    header = "".join([header, struct.pack("I", len(headerEnd)), headerEnd])
+    f_handle.write(header)
+
+#    hdr_cmd = '/home/kat/software/mockHeader -tel 64 -mach 64 -type 1 -raw %s -source %s -tstart %.8f -nbits 32 ' %(opts.i0,opts.source,MJDstart)
+#    hdr_cmd += '-nifs 1 -tsamp %.10f -fch1 %.6f -fo %.6f -nchans %i %s' %(tsamp,fBottom,chBW,nchan,outfile)
+#    os.system(hdr_cmd)
 
     ##############################################################
     # Power detect, re-accumulate and combine H & V pol. streams.
     ##############################################################
     start = 0
     Nblock = 256/opts.ndec
-    f_handle = open(outfile,'ab')
     i0s = np.int_(np.arange(index0,Nend0,256)) # 256 spectra allows fast cache access c.f. 8192
     i1s = np.int_(np.arange(index1,Nend1,256))
     bf0pows = np.zeros((nchan,Nsamp),dtype=np.float32)
@@ -170,8 +225,8 @@ if __name__=="__main__":
         pow1 = bf1[:,i1s[index]:i1s[index]+256,:]
         pow0 = pow0[...,0] + pow0[...,1]*1j
         pow1 = pow1[...,0] + pow1[...,1]*1j
-        pow0 = (pow0 * pow0.conjugate()).real.astype(np.float32)
-        pow1 = (pow1 * pow1.conjugate()).real.astype(np.float32)
+        pow0 = (pow0 * pow0.conjugate()).real
+        pow1 = (pow1 * pow1.conjugate()).real
 
         if ( opts.ndec > 1 ):
             try:
@@ -182,7 +237,7 @@ if __name__=="__main__":
 
         totPow = pow0 + pow1
         totSpec = totPow.transpose().flatten()
-        bytesSpec = totSpec.tobytes(order="C")
+        bytesSpec = totSpec.astype(np.float32).tobytes(order="C")
         f_handle.write(bytesSpec) # write sigproc data fmt to file
         f_handle.seek(0,2)
     
