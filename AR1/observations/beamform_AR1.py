bbbf38c506a3a2ed9cc7cb143db7688ba750bd0f
Ludwig Schwardt <ludwig@ska.ac.za>
2016-06-02 14:02:20 +0200

    Turn obs_script_arguments back into a sensor
    Since multiple scripts can run on the same set of SP containers accepting
    the same subarray product, this key needs to be a sensor.

--- AR1/observations/beamform_AR1.py
+++ AR1/observations/beamform_AR1.py
@@ -100,7 +100,7 @@ with verify_and_connect(opts) as kat:
     telstate = get_telstate(kat.data, kat.sub)
     script_args = vars(opts)
     script_args['targets'] = args
-    telstate.add('obs_script_arguments', script_args, immutable=True)
+    telstate.add('obs_script_arguments', script_args)
 
     # Start capture session
     with start_session(kat, **vars(opts)) as session:
