3ecdd79c7c26cdd75b539b451904c1e44993f959
kat <kat@rts-imager.kat.ac.za>
2016-06-15 10:29:22 +0200

    Add user specification of input ipython notebook template.

--- AR1/obs_report/obs_report.py
+++ AR1/obs_report/obs_report.py
@@ -1,12 +1,12 @@
 import os
-from subprocess import check_output, STDOUT
+from subprocess import call
 from tempfile import mkdtemp
 import argparse
 
 import nbformat
 
-def build_ipynb_obs_report_command(datafile,temp_dir):
-    obs_report_template_filename = 'AR1_obs_report_template.ipynb'
+def build_ipynb_obs_report_command(datafile,template):
+    obs_report_template_filename = template
     filename_base = os.path.basename(datafile)
     obs_report_output_filename = 'obs_report_%s.ipynb'%filename_base
     file_in = open(obs_report_template_filename)
@@ -28,28 +28,23 @@ def build_ipynb_obs_report_command(datafile,temp_dir):
     file_out.close()
     return 'runipy -o -s %s' % obs_report_output_filename
 
-def generate_ipynb_obs_report(datafile):
-    # setup
-    temp_dir = '.'
-    text_log_filename = '%s.txt' % os.path.basename(datafile)
-    text_log_filename = os.path.join(temp_dir, text_log_filename)
-
+def generate_ipynb_obs_report(datafile,template):
     # call obs report
-    obs_report_command = build_ipynb_obs_report_command(datafile,temp_dir)
+    obs_report_command = build_ipynb_obs_report_command(datafile,template)
     print 'Command executed:'
     print obs_report_command
     # todo: we'll want to know know why the returncode was not 0
-    std_output = []
-    std_output.append(check_output(obs_report_command, shell=True))
-    print 'jupyter nbconvert --to PDF %s'%obs_report_command.split()[-1]
-    std_output.append(check_output('jupyter nbconvert --to PDF %s'%obs_report_command.split()[-1]), shell=True)
+    call(obs_report_command, shell=True)
+    print 'jupyter nbconvert --to html %s'%obs_report_command.split()[-1]
+    call('jupyter nbconvert --to html %s'%obs_report_command.split()[-1], shell=True)
     
 parser = argparse.ArgumentParser(description="Runs the docker ipython obs-report on a file")
 parser.add_argument("filename", nargs=1)
-
+parser.add_argument("--template", default='/var/kat/katsdpscripts/AR1/obs_report/AR1_obs_report_template.ipynb')
 args,unknown = parser.parse_known_args()
 
 if args.filename[0] == '':
     raise RuntimeError('Please specify an h5 file to load.')
-    
-generate_ipynb_obs_report(args.filename[0])
+if not os.path.exists(args.template):
+    raise IOError('Ipython template not found at %s.'%(args.template,)) 
+generate_ipynb_obs_report(args.filename[0],args.template)
