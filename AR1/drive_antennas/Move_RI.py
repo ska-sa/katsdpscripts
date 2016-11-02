20e2114246de108f75b35bcd49702b06c37489f4
lance <lance@ska.ac.za>
2016-11-02 16:02:07 +0200

    selectable ap

--- AR1/drive_antennas/Move_RI.py
+++ AR1/drive_antennas/Move_RI.py
@@ -31,66 +31,56 @@ def wait_until_sensor_equals(timeout, sensorname, value,
     return (success, lastval)
 
 
-def track(ants, target, ridx_position='l', duration=10, dry_run=False):
+def track(ant, target, ridx_position='l', duration=10, dry_run=False):
 
     # TODO: make the indexer timeout configurable parameter
     indexer_timeout = 120
     # send this target to the antenna.
-    ants.req.target(target.description)
+    ant.req.target(target.description)
     user_logger.info("Target description: '%s' " % target.description)
-    ant_names = ','.join([ant.name for ant in ants])
+    ant.req.mode("POINT")
 
-    ants.req.mode("POINT")
     if not dry_run:
         # Added sleep to wait for AP brakes to disengage
-        time.sleep(2)
+        time.sleep(8)
 
     # TODO: more meaningful indication of the target
     #       (where no name is specified)
-    user_logger.info("Slewing %s to target: %s" % (ant_names, target.name))
+    user_logger.info("Slewing %s to target: %s" % (ant.name, target.name))
     # Wait for antenna to lock onto target
-    locks = 0
     if not dry_run:
-        for ant_x in ants:
-            if ant_x.wait('lock', True, timeout=300):
-                user_logger.info("%s on target..." % ant_x.name)
-                locks += 1
-            user_logger.info("%s out of %s antennas on target" %
-                             (locks, len(ants)))
-    else:
-        locks = len(ants)
-    if len(ants) == locks:
-        user_logger.info("Target reached. Wait for %d seconds "
-                         "before starting indexer cycle" % duration)
-        if not dry_run:
+        if ant.wait('lock', True, timeout=300):
+            user_logger.info("%s on target... "
+                             "Wait for %d seconds "
+                             "before starting indexer cycle" %
+                             (ant.name, duration))
             time.sleep(duration)
-    else:
-        user_logger.error("Antennas failed to reach target.")
+        else:
+            user_logger.error("Antennas failed to reach target.")
 
-    ants.req.mode('STOP')
+    ant.req.mode('STOP')
     if not dry_run:
         # Added sleep to wait for AP brakes to engage
         time.sleep(2)
-        result = wait_until_sensor_equals(5.0, 'm036_mode', 'STOP')
+        result = wait_until_sensor_equals(5.0, ant.name + '_mode', 'STOP')
         if result[0] == False:
             user_logger.error("Failed to set AP to 'STOP' mode. "
                               "Indexer commands will not be processed.")
             return
 
     user_logger.info("Setting initial RI on %s to position : %s " %
-                     (ant_names, ridx_position.upper()))
+                     (ant.name, ridx_position.upper()))
 
-    ants.req.ap_set_indexer_position(ridx_position)
+    ant.req.ap_set_indexer_position(ridx_position)
     if not dry_run:
         result = wait_until_sensor_equals(indexer_timeout,
-                                          'm036_ap_indexer_position',
+                                          ant.name + '_ap_indexer_position',
                                           ridx_position)
     else:
         result = (True, ridx_position)
 
     if result[0] == False:
-        # TODO: support selectable AP, not just m036
-        ridx_position_raw = kat.sensors.get('m036_ap_indexer_position_raw', None).get_value()
+        ridx_position_raw = kat.sensors.get(ant.name + '_ap_indexer_position_raw', None).get_value()
         user_logger.error("Timed out while waiting %s seconds for "
                           "indexer to reach '%s' position. "
                           "Last position reading was %s degrees." %
@@ -106,24 +96,22 @@ def track(ants, target, ridx_position='l', duration=10, dry_run=False):
     # Cycling indexer positions
     if not dry_run:
         for pos in ridx_sequence:
-            # TODO: support selectable AP, not just m036
-            ridx_last_position = kat.sensors.get('m036_ap_indexer_position', None).get_value()
+            ridx_last_position = kat.sensors.get(ant.name + '_ap_indexer_position', None).get_value()
             ridx_movement_start_time = time.time()
             user_logger.info("--- Moving RI to position: '%s' ---" %
                              pos.upper())
-            ants.req.ap_set_indexer_position(pos)
+            ant.req.ap_set_indexer_position(pos)
 
             result = wait_until_sensor_equals(indexer_timeout,
-                                              'm036_ap_indexer_position',
+                                              ant.name + '_ap_indexer_position',
                                               pos)
             user_logger.debug("Request result: '%s', "
                               "last sensor reading: '%s'" %
                               (result[0], result[1]))
 
             if result[0] == False:
-                #  TODO: support selectable AP, not just m036
-                ridx_position_raw = kat.sensors.get('m036_ap_indexer_position_raw', None).get_value()
-                ridx_brakes_released = kat.sensors.get('m036_ap_ridx_brakes_released', None).get_value()
+                ridx_position_raw = kat.sensors.get(ant.name + '_ap_indexer_position_raw', None).get_value()
+                ridx_brakes_released = kat.sensors.get(ant.name + '_ap_ridx_brakes_released', None).get_value()
 
                 user_logger.error("Timed out while waiting %s seconds "
                                   "for indexer to reach '%s' position. "
@@ -133,8 +121,7 @@ def track(ants, target, ridx_position='l', duration=10, dry_run=False):
                                    ridx_position_raw,
                                    ridx_brakes_released))
 
-            # TODO: support selectable AP, not just m036
-            ridx_current_position = kat.sensors.get('m036_ap_indexer_position', None).get_value()
+            ridx_current_position = kat.sensors.get(ant.name + '_ap_indexer_position', None).get_value()
             time_to_index = time.time() - ridx_movement_start_time
             if ridx_current_position in ['undefined']:
                 user_logger.warning("Movement ended in undefined position")
@@ -181,8 +168,9 @@ if len(args) == 0:
     raise RuntimeError("No targets and indexer positions"
                        "provided to the script")
 
-
+receptor = None
 targetlist = []
+
 for argstr in args:
     temp, taz, tel, band = argstr.split(',')
     print "azimuth : %r elevation : %r indexer : %s" % (taz, tel, band)
@@ -190,15 +178,19 @@ for argstr in args:
 
 # Check options and build KAT configuration, connecting to proxies and devices
 with verify_and_connect(opts) as kat:
-    # TODO: Basic check for m036 added but will need to be improved later
-    if opts.ap not in [ant.name for ant in ants]:
+    
+    for ant in kat.ants:
+        if opts.ap in [ant.name]:
+            receptor = ant
+
+    if receptor is None:
         raise RuntimeError("Receptor under test is not in controlled array")
 
     # Set sensor strategies"
     kat.ants.set_sampling_strategy("lock", "event")
     kat.ants.set_sampling_strategy("ap.indexer-position", "event")
 
-    if not kat.dry_run and kat.ants.req.mode('STOP'):
+    if not kat.dry_run and receptor.req.mode('STOP'):
         user_logger.info("Setting Antenna Mode to 'STOP', "
                          "Powering on Antenna Drives.")
         time.sleep(2)
@@ -209,7 +201,7 @@ with verify_and_connect(opts) as kat:
     for taz, tel, band in targetlist:
         for i in range(int(opts.num_repeat)):
             target = katpoint.Target('Name, azel, %s, %s' % (taz, tel))
-            track(kat.ants, target, ridx_position=band,
+            track(receptor, target, ridx_position=band,
                   duration=opts.dwell_time, dry_run=kat.dry_run)
             user_logger.info("Elevation: '%s' "
                              "Patterns attempted: '%s' "
