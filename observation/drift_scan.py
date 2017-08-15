dd6c49a7ad57c305dc509bfd54a365b6c7fbaf79
Ludwig Schwardt <ludwig@ska.ac.za>
2017-08-15 15:09:17 +0200

    Move horizon check into session block (and dedent)
    This ensures that the requested sources are checked to be above the
    horizon during the dry run / schedule block verification. Previously
    the check was done before session instantiation and therefore used
    the unadulterated time.time(). By moving it inside the with-session
    block the check uses the appropriate time.time() and will therefore
    be valid even during dry runs.
    
    These older scripts also now gain the NoTargetsUpError exception,
    which allows a lot of code to be dedented by one level.
    
    This addresses JIRA ticket CAM-513.

--- observation/drift_scan.py
+++ observation/drift_scan.py
@@ -8,6 +8,11 @@ import time
 from katcorelib import standard_script_options, verify_and_connect, collect_targets, start_session, user_logger
 import katpoint
 
+
+class NoTargetsUpError(Exception):
+    """No targets are above the horizon at the start of the observation."""
+
+
 # Set up standard script options
 parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]",
                                  description='Perfrom a drift scan on one or more sources for a '
@@ -35,51 +40,50 @@ if len(args) == 0:
 # Check options and build KAT configuration, connecting to proxies and devices
 with verify_and_connect(opts) as kat:
     observation_sources = collect_targets(kat, args)
+    # Start capture session, which creates HDF5 file
+    with start_session(kat, **vars(opts)) as session:
+        # Quit early if there are no sources to observe
+        if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
+            raise NoTargetsUpError("No targets are currently visible - "
+                                   "please re-run the script later")
+        session.standard_setup(**vars(opts))
+        session.capture_start()
 
-    # Quit early if there are no sources to observe
-    if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
-        user_logger.warning("No targets are currently visible - please re-run the script later")
-    else:
-        # Start capture session, which creates HDF5 file
-        with start_session(kat, **vars(opts)) as session:
-            session.standard_setup(**vars(opts))
-            session.capture_start()
-
-            start_time = time.time()
-            targets_observed = []
-            # Keep going until the time is up
-            keep_going = True
-            while keep_going:
-                keep_going = (opts.max_duration is not None) and opts.repeat
-                targets_before_loop = len(targets_observed)
-                # Iterate through source list, picking the next one that is up
-                for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
-                    target_future_azel = target.azel(timestamp=time.time() + opts.track_duration / 2)
-                    target = katpoint.construct_azel_target(katpoint.wrap_angle(target_future_azel[0]),
-                                                            katpoint.wrap_angle(target_future_azel[1]))
-                    session.label('track')
-                    user_logger.info("Initiating %g-second drift scan on target '%s'" %
-                                     (opts.track_duration, target.name,))
-                    # Split the total track on one target into segments lasting as long as the noise diode period
-                    # This ensures the maximum number of noise diode firings
-                    total_track_time = 0.
-                    while total_track_time < opts.track_duration:
-                        next_track = opts.track_duration - total_track_time
-                        # Cut the track short if time ran out
-                        if opts.max_duration is not None:
-                            next_track = min(next_track, opts.max_duration - (time.time() - start_time))
-                        if opts.nd_params['period'] > 0:
-                            next_track = min(next_track, opts.nd_params['period'])
-                        if next_track <= 0 or not session.track(target, duration=next_track, announce=False):
-                            break
-                        total_track_time += next_track
-                    if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
-                        user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
-                                            (opts.max_duration,))
-                        keep_going = False
+        start_time = time.time()
+        targets_observed = []
+        # Keep going until the time is up
+        keep_going = True
+        while keep_going:
+            keep_going = (opts.max_duration is not None) and opts.repeat
+            targets_before_loop = len(targets_observed)
+            # Iterate through source list, picking the next one that is up
+            for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
+                target_future_azel = target.azel(timestamp=time.time() + opts.track_duration / 2)
+                target = katpoint.construct_azel_target(katpoint.wrap_angle(target_future_azel[0]),
+                                                        katpoint.wrap_angle(target_future_azel[1]))
+                session.label('track')
+                user_logger.info("Initiating %g-second drift scan on target '%s'" %
+                                 (opts.track_duration, target.name,))
+                # Split the total track on one target into segments lasting as long as the noise diode period
+                # This ensures the maximum number of noise diode firings
+                total_track_time = 0.
+                while total_track_time < opts.track_duration:
+                    next_track = opts.track_duration - total_track_time
+                    # Cut the track short if time ran out
+                    if opts.max_duration is not None:
+                        next_track = min(next_track, opts.max_duration - (time.time() - start_time))
+                    if opts.nd_params['period'] > 0:
+                        next_track = min(next_track, opts.nd_params['period'])
+                    if next_track <= 0 or not session.track(target, duration=next_track, announce=False):
                         break
-                    targets_observed.append(target.name)
-                if keep_going and len(targets_observed) == targets_before_loop:
-                    user_logger.warning("No targets are currently visible - stopping script instead of hanging around")
+                    total_track_time += next_track
+                if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
+                    user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
+                                        (opts.max_duration,))
                     keep_going = False
-            user_logger.info("Targets observed : %d (%d unique)" % (len(targets_observed), len(set(targets_observed))))
+                    break
+                targets_observed.append(target.name)
+            if keep_going and len(targets_observed) == targets_before_loop:
+                user_logger.warning("No targets are currently visible - stopping script instead of hanging around")
+                keep_going = False
+        user_logger.info("Targets observed : %d (%d unique)" % (len(targets_observed), len(set(targets_observed))))
--- observation/track.py
+++ observation/track.py
@@ -9,6 +9,10 @@ from katcorelib import (standard_script_options, verify_and_connect,
                         collect_targets, start_session, user_logger)
 
 
+class NoTargetsUpError(Exception):
+    """No targets are above the horizon at the start of the observation."""
+
+
 # Set up standard script options
 parser = standard_script_options(usage="%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]",
                                  description='Track one or more sources for a specified time. At least one '
@@ -35,60 +39,60 @@ if len(args) == 0:
 # Check options and build KAT configuration, connecting to proxies and devices
 with verify_and_connect(opts) as kat:
     observation_sources = collect_targets(kat, args)
-    # Quit early if there are no sources to observe
-    if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
-        user_logger.warning("No targets are currently visible - please re-run the script later")
-    else:
-        # Start capture session, which creates HDF5 file
-        with start_session(kat, **vars(opts)) as session:
-            # Set the gain to a single non complex number if needed
-            if opts.reset_gain is not None:
-                if not session.cbf.fengine.inputs:
-                    user_logger.error("Failed to get correlator input labels")
-                    raise RuntimeError("Failed to get correlator input labels, "
-                                       "cannot set the F-engine gains")
-                for inp in session.cbf.fengine.inputs:
-                    session.cbf.fengine.req.gain(inp, opts.reset_gain)
-                    user_logger.info("F-engine %s gain set to %g",
-                                     inp, opts.reset_gain)
+    # Start capture session, which creates HDF5 file
+    with start_session(kat, **vars(opts)) as session:
+        # Quit early if there are no sources to observe
+        if len(observation_sources.filter(el_limit_deg=opts.horizon)) == 0:
+            raise NoTargetsUpError("No targets are currently visible - "
+                                   "please re-run the script later")
+        # Set the gain to a single non complex number if needed
+        if opts.reset_gain is not None:
+            if not session.cbf.fengine.inputs:
+                user_logger.error("Failed to get correlator input labels")
+                raise RuntimeError("Failed to get correlator input labels, "
+                                   "cannot set the F-engine gains")
+            for inp in session.cbf.fengine.inputs:
+                session.cbf.fengine.req.gain(inp, opts.reset_gain)
+                user_logger.info("F-engine %s gain set to %g",
+                                 inp, opts.reset_gain)
 
-            session.standard_setup(**vars(opts))
-            session.capture_start()
+        session.standard_setup(**vars(opts))
+        session.capture_start()
 
-            start_time = time.time()
-            targets_observed = []
-            # Keep going until the time is up
-            keep_going = True
-            while keep_going:
-                keep_going = (opts.max_duration is not None) and opts.repeat
-                targets_before_loop = len(targets_observed)
-                # Iterate through source list, picking the next one that is up
-                for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
-                    session.label('track')
-                    user_logger.info("Initiating %g-second track on target %r",
-                                     opts.track_duration, target.name)
-                    # Split the total track on one target into segments lasting as long as the noise diode period
-                    # This ensures the maximum number of noise diode firings
-                    total_track_time = 0.
-                    while total_track_time < opts.track_duration:
-                        next_track = opts.track_duration - total_track_time
-                        # Cut the track short if time ran out
-                        if opts.max_duration is not None:
-                            next_track = min(next_track, opts.max_duration - (time.time() - start_time))
-                        if opts.nd_params['period'] > 0:
-                            next_track = min(next_track, opts.nd_params['period'])
-                        if next_track <= 0 or not session.track(target, duration=next_track, announce=False):
-                            break
-                        total_track_time += next_track
-                    if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
-                        user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
-                                            (opts.max_duration,))
-                        keep_going = False
+        start_time = time.time()
+        targets_observed = []
+        # Keep going until the time is up
+        keep_going = True
+        while keep_going:
+            keep_going = (opts.max_duration is not None) and opts.repeat
+            targets_before_loop = len(targets_observed)
+            # Iterate through source list, picking the next one that is up
+            for target in observation_sources.iterfilter(el_limit_deg=opts.horizon):
+                session.label('track')
+                user_logger.info("Initiating %g-second track on target %r",
+                                 opts.track_duration, target.name)
+                # Split the total track on one target into segments lasting as long as the noise diode period
+                # This ensures the maximum number of noise diode firings
+                total_track_time = 0.
+                while total_track_time < opts.track_duration:
+                    next_track = opts.track_duration - total_track_time
+                    # Cut the track short if time ran out
+                    if opts.max_duration is not None:
+                        next_track = min(next_track, opts.max_duration - (time.time() - start_time))
+                    if opts.nd_params['period'] > 0:
+                        next_track = min(next_track, opts.nd_params['period'])
+                    if next_track <= 0 or not session.track(target, duration=next_track, announce=False):
                         break
-                    targets_observed.append(target.name)
-                if keep_going and len(targets_observed) == targets_before_loop:
-                    user_logger.warning("No targets are currently visible - "
-                                        "stopping script instead of hanging around")
+                    total_track_time += next_track
+                if opts.max_duration is not None and (time.time() - start_time >= opts.max_duration):
+                    user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script" %
+                                        (opts.max_duration,))
                     keep_going = False
-            user_logger.info("Targets observed : %d (%d unique)",
-                             len(targets_observed), len(set(targets_observed)))
+                    break
+                targets_observed.append(target.name)
+            if keep_going and len(targets_observed) == targets_before_loop:
+                user_logger.warning("No targets are currently visible - "
+                                    "stopping script instead of hanging around")
+                keep_going = False
+        user_logger.info("Targets observed : %d (%d unique)",
+                         len(targets_observed), len(set(targets_observed)))
