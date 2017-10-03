d9367e39e0bd66fcf7cf3df0b9af026f20fecfa7
mserylak@gmail.com <mserylak@gmail.com>
2017-10-03 18:38:11 +0200

    Upgraded bf_folded_obs_report template and bf_obs_report.py

--- reduction/obs_report/bf_obs_report.py
+++ reduction/obs_report/bf_obs_report.py
@@ -6,11 +6,11 @@ import nbformat
 from subprocess import call
 from tempfile import mkdtemp
 
-def build_ipynb_bf_obs_report_command(directory, template):
+def build_ipynb_bf_folded_obs_report_command(directory, template):
     print directory
     bf_obs_report_template_filename = template
     directory_base = os.path.basename(directory)
-    bf_obs_report_output_filename = 'bf_obs_report_%s.ipynb' % directory_base
+    bf_obs_report_output_filename = 'bf_folded_obs_report_%s.ipynb' % directory_base
     file_in = open(bf_obs_report_template_filename)
     file_out = open(bf_obs_report_output_filename, 'w')
     nb = nbformat.reader.read(file_in)
@@ -30,23 +30,23 @@ def build_ipynb_bf_obs_report_command(directory, template):
     file_out.close()
     return 'runipy --overwrite --skip-exceptions %s' % bf_obs_report_output_filename
 
-def generate_ipynb_bf_obs_report(directory, template):
+def generate_ipynb_bf_folded_obs_report(directory, template):
     # call obs report
     obs_report_command = build_ipynb_bf_obs_report_command(directory, template)
     print 'Command executed:'
     print obs_report_command
     # todo: we'll want to know know why the returncode was not 0
-    call(obs_report_command, shell = True)
+    call(obs_report_command, shell=True)
     print 'jupyter nbconvert --to html --template full %s' % obs_report_command.split()[-1]
-    call('jupyter nbconvert --to html --template full %s'%obs_report_command.split()[-1], shell = True)
+    call('jupyter nbconvert --to html --template full %s' % obs_report_command.split()[-1], shell=True)
 
-parser = argparse.ArgumentParser(description = 'Runs the docker for IPython beamformer observation report.')
-parser.add_argument('directory', nargs = 1)
-parser.add_argument('--template', dest = 'template', default = '/home/kat/software/katsdpscripts/reduction/obs_report/bf_obs_report_template.ipynb')
+parser = argparse.ArgumentParser(description='Runs the docker for IPython beamformer observation report.')
+parser.add_argument('directory', nargs=1)
+parser.add_argument('--template', dest='template', default='/home/kat/software/katsdpscripts/reduction/obs_report/bf_folded_obs_report_template.ipynb')
 args, unknown = parser.parse_known_args()
 
 if args.directory[0] == '':
     raise RuntimeError('Please specify a directory from which files will be loaded.')
 if not os.path.isfile(args.template):
     raise IOError('IPython template not found at %s.' % args.template)
-generate_ipynb_bf_obs_report(args.directory[0], args.template)
+generate_ipynb_bf_folded_obs_report(args.directory[0], args.template)
