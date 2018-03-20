86ec88dc01a213e00d2872c6a569198e8620417c
Ludwig Schwardt <ludwig@ska.ac.za>
2018-03-21 01:03:59 +0200

    Remove unnecessary for loop
    We are iterating over a list with exactly one item. Just assign the
    item to `target` and be done with it. This dedents a good chunk of
    code.

--- observation/basic_health.py
+++ observation/basic_health.py
@@ -63,95 +63,96 @@ with verify_and_connect(opts) as kat:
         session.capture_init()
         session.cbf.correlator.req.capture_start()
         gains = {}
-        for target in [observation_sources.sort('el').targets[-1]]:
-            target.add_tags('bfcal single_accumulation')
-            # Calibration tests
-            user_logger.info("Performing calibration tests")
-            user_logger.info("Resetting F-engine gains to %g to allow phasing up",
-                             opts.default_gain)
-            for inp in session.cbf.fengine.inputs:
-                gains[inp] = opts.default_gain
-            session.set_fengine_gains(gains)
-            session.label('calibration')
-            user_logger.info("Initiating %g-second track on target '%s'",
-                             opts.track_duration, target.name)
-            session.track(target, duration=opts.track_duration, announce=False)
-            # Attempt to jiggle cal pipeline to drop its gains
-            session.stop_antennas()
-            user_logger.info("Waiting for gains to materialise in cal pipeline")
-            # Wait for the last bfcal product from the pipeline
-            gains = session.get_cal_solutions('product_G', timeout=300.)
-            bp_gains = session.get_cal_solutions('product_B')
-            delays = session.get_cal_solutions('product_K')
-            cal_channel_freqs = session.get_cal_channel_freqs()
-            user_logger.info("Setting F-engine gains to phase up antennas")
-            new_weights = {}
-            for inp in gains:
-                orig_weights = gains[inp]
-                bp = bp_gains[inp]
-                valid = ~np.isnan(bp)
-                if valid.any():  # not all flagged
-                    chans = np.arange(len(bp))
-                    bp = np.interp(chans, chans[valid], bp[valid])
-                    orig_weights *= bp
-                    delay_weights = np.exp(-2j * np.pi * delays[inp] * cal_channel_freqs)
-                    orig_weights *= delay_weights  # unwrap the delays
-                    amp_weights = np.abs(orig_weights)
-                    phase_weights = orig_weights / amp_weights
-                    new_weights[inp] = opts.default_gain * phase_weights.conj()
-            session.set_fengine_gains(new_weights)
-            if opts.verify_duration > 0:
-                user_logger.info("Revisiting target %r for %g seconds to verify phase-up",
-                                 target.name, opts.verify_duration)
-                session.track(target, duration=opts.verify_duration, announce=False)
+        # Pick source with the highest elevation as our target
+        target = observation_sources.sort('el').targets[-1]
+        target.add_tags('bfcal single_accumulation')
+        # Calibration tests
+        user_logger.info("Performing calibration tests")
+        user_logger.info("Resetting F-engine gains to %g to allow phasing up",
+                         opts.default_gain)
+        for inp in session.cbf.fengine.inputs:
+            gains[inp] = opts.default_gain
+        session.set_fengine_gains(gains)
+        session.label('calibration')
+        user_logger.info("Initiating %g-second track on target '%s'",
+                         opts.track_duration, target.name)
+        session.track(target, duration=opts.track_duration, announce=False)
+        # Attempt to jiggle cal pipeline to drop its gains
+        session.stop_antennas()
+        user_logger.info("Waiting for gains to materialise in cal pipeline")
+        # Wait for the last bfcal product from the pipeline
+        gains = session.get_cal_solutions('product_G', timeout=300.)
+        bp_gains = session.get_cal_solutions('product_B')
+        delays = session.get_cal_solutions('product_K')
+        cal_channel_freqs = session.get_cal_channel_freqs()
+        user_logger.info("Setting F-engine gains to phase up antennas")
+        new_weights = {}
+        for inp in gains:
+            orig_weights = gains[inp]
+            bp = bp_gains[inp]
+            valid = ~np.isnan(bp)
+            if valid.any():  # not all flagged
+                chans = np.arange(len(bp))
+                bp = np.interp(chans, chans[valid], bp[valid])
+                orig_weights *= bp
+                delay_weights = np.exp(-2j * np.pi * delays[inp] * cal_channel_freqs)
+                orig_weights *= delay_weights  # unwrap the delays
+                amp_weights = np.abs(orig_weights)
+                phase_weights = orig_weights / amp_weights
+                new_weights[inp] = opts.default_gain * phase_weights.conj()
+        session.set_fengine_gains(new_weights)
+        if opts.verify_duration > 0:
+            user_logger.info("Revisiting target %r for %g seconds to verify phase-up",
+                             target.name, opts.verify_duration)
+            session.track(target, duration=opts.verify_duration, announce=False)
 
-            # interferometric pointing
-            user_logger.info("Performing interferometric pointing tests")
-            session.label('interferometric_pointing')
-            session.track(target, duration=opts.track_duration, announce=False)
-            for direction in {'x', 'y'}:
-                for offset in np.linspace(-1, 1, 10 // 2):
-                    if direction == 'x':
-                        offset_target = [offset, 0.0]
-                    else:
-                        offset_target = [0.0, offset]
-                    user_logger.info("Initiating %g-second track on target '%s'",
-                                     opts.track_duration, target.name)
-                    user_logger.info("Offset of (%f, %f) degrees", *offset_target)
-                    session.set_target(target)
-                    if not kat.dry_run:
-                        session.ants.req.offset_fixed(offset_target[0], offset_target[1], opts.projection)
-                    nd_params = session.nd_params
-                    session.fire_noise_diode(announce=True, **nd_params)
-                    if kat.dry_run:
-                        session.track(target, duration=opts.track_duration, announce=False)
-                    else:
-                        time.sleep(opts.track_duration)  # Snooze
-            session.ants.req.offset_fixed(0, 0, opts.projection)  # reset any dangling offsets
-            # Tsys and averaging
-            user_logger.info("Performing Tsys and averaging tests")
-            session.nd_params = nd_off
-            # 10 second track so that the antenna profiler does not run away
-            session.track(target, duration=10)
-            user_logger.info("Now capturing data - diode %s on", nd_on['diode'])
-            session.label('%s' % (nd_on['diode'],))
-            if not session.fire_noise_diode(announce=True, **nd_on):
-                user_logger.error("Noise diode %s did not fire", nd_on['diode'])
-            session.nd_params = nd_off
-            user_logger.info("Now capturing data - noise diode off")
-            session.track(target, duration=320)  # get 5 mins of data to test averaging
+        # interferometric pointing
+        user_logger.info("Performing interferometric pointing tests")
+        session.label('interferometric_pointing')
+        session.track(target, duration=opts.track_duration, announce=False)
+        for direction in {'x', 'y'}:
+            for offset in np.linspace(-1, 1, 10 // 2):
+                if direction == 'x':
+                    offset_target = [offset, 0.0]
+                else:
+                    offset_target = [0.0, offset]
+                user_logger.info("Initiating %g-second track on target '%s'",
+                                 opts.track_duration, target.name)
+                user_logger.info("Offset of (%f, %f) degrees", *offset_target)
+                session.set_target(target)
+                if not kat.dry_run:
+                    session.ants.req.offset_fixed(offset_target[0], offset_target[1], opts.projection)
+                nd_params = session.nd_params
+                session.fire_noise_diode(announce=True, **nd_params)
+                if kat.dry_run:
+                    session.track(target, duration=opts.track_duration, announce=False)
+                else:
+                    time.sleep(opts.track_duration)  # Snooze
+        session.ants.req.offset_fixed(0, 0, opts.projection)  # reset any dangling offsets
+        # Tsys and averaging
+        user_logger.info("Performing Tsys and averaging tests")
+        session.nd_params = nd_off
+        # 10 second track so that the antenna profiler does not run away
+        session.track(target, duration=10)
+        user_logger.info("Now capturing data - diode %s on", nd_on['diode'])
+        session.label('%s' % (nd_on['diode'],))
+        if not session.fire_noise_diode(announce=True, **nd_on):
+            user_logger.error("Noise diode %s did not fire", nd_on['diode'])
+        session.nd_params = nd_off
+        user_logger.info("Now capturing data - noise diode off")
+        session.track(target, duration=320)  # get 5 mins of data to test averaging
 
-            # Single dish pointing ... to compare with interferometric
-            user_logger.info("Performing single dish pointing tests")
-            session.label('raster')
-            user_logger.info("Doing scan of '%s' with current azel (%s, %s)",
-                             target.description, *target.azel())
-            # Do different raster scan on strong and weak targets
-            session.raster_scan(target, num_scans=5, scan_duration=80, scan_extent=6.0,
-                                scan_spacing=0.25, scan_in_azimuth=True,
-                                projection=opts.projection)
-            # reset the gains always
-            user_logger.info("Resetting F-engine gains to %g", opts.default_gain)
-            for inp in gains:
-                gains[inp] = opts.default_gain
-            session.set_fengine_gains(gains)
+        # Single dish pointing ... to compare with interferometric
+        user_logger.info("Performing single dish pointing tests")
+        session.label('raster')
+        user_logger.info("Doing scan of '%s' with current azel (%s, %s)",
+                         target.description, *target.azel())
+        # Do different raster scan on strong and weak targets
+        session.raster_scan(target, num_scans=5, scan_duration=80, scan_extent=6.0,
+                            scan_spacing=0.25, scan_in_azimuth=True,
+                            projection=opts.projection)
+        # reset the gains always
+        user_logger.info("Resetting F-engine gains to %g", opts.default_gain)
+        for inp in gains:
+            gains[inp] = opts.default_gain
+        session.set_fengine_gains(gains)
