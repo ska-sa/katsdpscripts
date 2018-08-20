9ad53db8934d264bef8de53269f6f1f4bd5a80fd
Marisa Geyer <marisa.geyer@gmail.com>
2018-08-20 12:37:09 +0000

    Changed capture_start in calibrate_delays.py

--- observation/calibrate_delays.py
+++ observation/calibrate_delays.py
@@ -76,7 +76,7 @@ with verify_and_connect(opts) as kat:
         session.set_fengine_gains(gains)
         user_logger.info("Zeroing all delay adjustments for starters")
         session.set_delays(delays)
-        session.capture_start()
+        session.cbf.correlator.req.capture_start()
         user_logger.info("Initiating %g-second track on target %r",
                          opts.track_duration, target.description)
         session.label('un_corrected')
