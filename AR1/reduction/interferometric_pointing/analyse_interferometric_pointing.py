e9b7746f838abf7f75768e5e0c45b3f98169b0ec
Sean Passmoor <sean@ska.ac.za>
2018-09-12 15:11:29 +0200

    Add debug option to provide additional infomation

--- AR1/reduction/interferometric_pointing/analyse_interferometric_pointing.py
+++ AR1/reduction/interferometric_pointing/analyse_interferometric_pointing.py
@@ -27,7 +27,7 @@ def activity(h5,state = 'track'):
 def w_average(arr,axis=None, weights=None):
     return np.nansum(arr*weights,axis=axis)/np.nansum(weights,axis=axis)
 
-def reduce_compscan_inf(h5 ,channel_mask = None,chunks=16,return_raw=False,use_weights=False,compscan_index=None):
+def reduce_compscan_inf(h5 ,channel_mask = None,chunks=16,return_raw=False,use_weights=False,compscan_index=None,debug=False):
     """Break the band up into chunks"""
     chunk_size = chunks
     rfi_static_flags = np.tile(False, h5.shape[0])
@@ -73,6 +73,23 @@ def reduce_compscan_inf(h5 ,channel_mask = None,chunks=16,return_raw=False,use_w
     gaussian_width_std  = np.zeros((chunk_size* 2,2,len(h5.ants)) )
     gaussian_height     = np.zeros((chunk_size* 2,len(h5.ants)) )
     gaussian_height_std = np.zeros((chunk_size* 2,len(h5.ants)) )
+    if debug :#debug_text
+        debug_text = []
+        line = []
+        line.append("#AntennaPol")
+        line.append("Target")
+        line.append("Freq(MHz)") #MHz
+        line.append("Centre Az")
+        line.append("Centre El")
+        line.append("Centre Az Std")
+        line.append("Centre El Std")
+        line.append("Centre Az Width")
+        line.append("Centre El Width")
+        line.append("Centre Az Width Std")
+        line.append("Centre El Width Std")
+        line.append("Height")
+        line.append("Height Std")
+        debug_text.append(','.join(line) )
     pols = ["H","V"] # Put in logic for Intensity
     for i,pol in enumerate(pols) :
         gains_p[pol] = []
@@ -141,12 +158,30 @@ def reduce_compscan_inf(h5 ,channel_mask = None,chunks=16,return_raw=False,use_w
         pol_ind['VV'] = np.arange(1.0*chunk_size,2.0*chunk_size,dtype=int) 
         pol_ind['I']  = np.arange(0.0*chunk_size,2.0*chunk_size,dtype=int) 
         for ant in range(len(h5.ants)):
-            h_pol = ~np.isnan(gaussian_centre[:,0,ant]) & ~np.isnan(1./gaussian_centre_std[:,0,ant])
-            v_pol = ~np.isnan(gaussian_centre[:,1,ant]) & ~np.isnan(1./gaussian_centre_std[:,1,ant])
-            valid_solutions = np.count_nonzero(h_pol & v_pol)
-            #raise RuntimeError("Stop")
+            h_pol = ~np.isnan(gaussian_centre[pol_ind['HH'],:,ant]) & ~np.isnan(1./gaussian_centre_std[pol_ind['HH'],:,ant])
+            v_pol = ~np.isnan(gaussian_centre[pol_ind['VV'],:,ant]) & ~np.isnan(1./gaussian_centre_std[pol_ind['VV'],:,ant])
+            valid_solutions = np.count_nonzero(h_pol & v_pol) # Note this is twice the number of solutions because of the Az & El parts
+            print("%i valid solutions out of %s for %s on %s at %s "%(valid_solutions//2,chunks,h5.ants[ant].name,target.name,str(katpoint.Timestamp(middle_time))))
+            if debug :#debug_text
+                for pol_i,pol in enumerate( ["H","V"]):
+                    line = []
+                    for chunk in range(chunks*pol_i,chunks*(pol_i+1)):
+                        freq = h5.channel_freqs[slice(chunk*(h5.shape[1]//chunks),(chunk+1)*(h5.shape[1]//chunks))].mean()
+                        line.append(h5.ants[ant].name+pol)
+                        line.append(target.name)
+                        line.append(str(freq/1e6)) #MHz
+                        line.append(str(gaussian_centre[chunk,0,ant]))
+                        line.append(str(gaussian_centre[chunk,1,ant]))
+                        line.append(str(gaussian_centre_std[chunk,0,ant]))
+                        line.append(str(gaussian_centre_std[chunk,1,ant]))
+                        line.append(str(gaussian_width[chunk,0,ant]))
+                        line.append(str(gaussian_width[chunk,1,ant]))
+                        line.append(str(gaussian_width_std[chunk,0,ant]))
+                        line.append(str(gaussian_width_std[chunk,1,ant]))
+                        line.append(str(gaussian_height[chunk,ant]))
+                        line.append(str(gaussian_height_std[chunk,ant]))
+                    debug_text.append(','.join(line) )
             if valid_solutions > 1 : # a bit overboard
-                #print("%i valid solutions found "%())
                 name = h5.ants[ant].name
                 ant_pointing[name] = {}
                 ant_pointing[name]["antenna"] = h5.ants[ant].name
@@ -197,6 +232,12 @@ def reduce_compscan_inf(h5 ,channel_mask = None,chunks=16,return_raw=False,use_w
                     ant_pointing[name]["elevation_%s_std"%(pol)] =np.sqrt(np.nansum(1./gaussian_centre_std[pol_ind[pol],1,ant]**2) )
             else:
                 print("No (%i) solutions for %s on %s at %s "%(valid_solutions,h5.ants[ant].name,target.name,str(katpoint.Timestamp(middle_time))))
+        if debug :#debug_text
+            debug_text.append('')
+            base = "%s_%s"%(h5.name.split('/')[-1].split('.')[0], "interferometric_pointing_DEBUG")
+            g = file('%s:Scan%i:%s'%(base,compscan_index,target.name),'w')
+            g.write("\n".join(debug_text))
+            g.close()
         return ant_pointing
 
 
@@ -213,6 +254,9 @@ parser.add_option( "--exclude-ants", dest="ex_ants",default=None,
                        "default is None of the antennas in the data set")
 parser.add_option( "--use-weights",action="store_true",
                   default=False, help="Use SDP visability weights ")
+parser.add_option( "--debug",action="store_true",
+                  default=False, help="Produce a debug file with fitting infomation for each frequency ")
+
 parser.add_option("-c", "--channel-mask", default="/var/kat/katsdpscripts/RTS/rfi_mask.pickle", help="Optional pickle file with boolean array specifying channels to mask (default is no mask)")
 parser.add_option("-o", "--output", dest="outfilebase",default=None,
                   help="Base name of output files (*.csv for output data and *.log for messages, "
@@ -259,7 +303,7 @@ for ant in range(len(h5.ants)):
     f[name].write(', '.join(output_field_names) + '\n')
 for compscan_index  in h5.compscan_indices :
     print("Compound scan %i  "%(compscan_index) )
-    offset_data = reduce_compscan_inf(h5,channel_mask,use_weights=opts.use_weights,compscan_index=compscan_index)
+    offset_data = reduce_compscan_inf(h5,channel_mask,use_weights=opts.use_weights,compscan_index=compscan_index,debug=opts.debug)
     if len(offset_data) > 0 : # if not an empty set
         print("Valid data obtained from the Compound scan")
         for antname in offset_data:
