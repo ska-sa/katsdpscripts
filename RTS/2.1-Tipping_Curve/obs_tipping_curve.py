Fix the calculation of the clear azmith
This fixes the calculation for a clear azimuth. Now a much smaller number of timestamps are used by a factor of about 40. Now the predicted time is used when calculating the gap not just now. The pulsars are removed from the catalogue , to speed up the process.

RTS/2.1-Tipping_Curve/obs_tipping_curve.py
@@ -32,9 +32,9 @@ with verify_and_connect(opts) as kat:
     if opts.az is None:
         user_logger.info("No Azimuth selected , selecting clear Azimith")
         if not kat.dry_run:
-            timestamp = [katpoint.Timestamp(time.time()) for i in range(int((np.arange(15.0,90.1,opts.spacing).shape[0]*(on_time+20.0+1.0))))]
+            timestamp = [katpoint.Timestamp(time.time()+i) for i in  ((np.arange(15.0,90.1,1)-15.0)*(15+20.0+1.0))]
             #load the standard KAT sources ... similar to the SkyPlot of the katgui
-            observation_sources = kat.sources
+            observation_sources = kat.sources.filter(tags=['~Pulsars'])
             source_az = []
             for source in observation_sources.targets:
                 az, el = np.degrees(source.azel(timestamp=timestamp))   # was rad2deg