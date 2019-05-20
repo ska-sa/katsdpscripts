d4e56d88b1f3c4e77d4806d0ce7a8385514515c0
Bruce Merry <bmerry@ska.ac.za>
2019-05-20 15:51:25 +0200

    Make RTS/__init__.py use explicitly relative imports
    Needed to make it work under Python 3.

--- katsdpscripts/RTS/__init__.py
+++ katsdpscripts/RTS/__init__.py
@@ -1,5 +1,5 @@
-from weatherlib import weather_report
-from rfilib import generate_flag_table, generate_rfi_report
-import diodelib                         # For QT 2_2
-import spectral_baseline                # For QT 2.10,3.8
-import strong_sources                   # For QT 2.8    
+from .weatherlib import weather_report
+from .rfilib import generate_flag_table, generate_rfi_report
+from . import diodelib                         # For QT 2_2
+from . import spectral_baseline                # For QT 2.10,3.8
+from . import strong_sources                   # For QT 2.8    
