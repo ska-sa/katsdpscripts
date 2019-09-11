9646f610cb36b82c77fb4dbb456b1aafea776068
Ludwig Schwardt <ludwig@ska.ac.za>
2019-09-11 23:02:26 +0200

    Upgrade F-engine interaction in calibrate_delays
    Remove default F-engine gains stored in the script and rely on the
    new 'default' gain feature in CBF instead, as supported by the recent
    ska-sa/katcorelib#325 PR. The same goes for the FFT shift.
    
    Gain and shift are set to 'default' by the --fengine-gain and
    --fft-shift script options, respectively, but can still be overridden.
    These options are of the new 'int_or_default' type introduced by the
    katcorelib PR.
    
    Use the new `adjust_fengine_delays` method as well. This takes care of
    any NaNs and also provides a clean and simple way to zero the delays.
    Improve docs and comments.
    
    This addresses JIRA ticket SR-1952.

--- observation/calibrate_delays.py
+++ observation/calibrate_delays.py
@@ -7,42 +7,35 @@
 # 5 April 2017
 #
 
-import numpy as np
 from katcorelib.observe import (standard_script_options, verify_and_connect,
-                                collect_targets, start_session, user_logger,
-                                CalSolutionsUnavailable)
+                                collect_targets, start_session, user_logger)
 
 
 class NoTargetsUpError(Exception):
     """No targets are above the horizon at the start of the observation."""
 
 
-# Default F-engine gain as a function of number of channels
-DEFAULT_GAIN = {1024: 1856, 4096: 1120, 32768: 5760}
-
 # Set up standard script options
 usage = "%prog [options] <'target/catalogue'> [<'target/catalogue'> ...]"
-description = 'Track the source with the largest flux density which is above ' \
-              'the horizon and calibrate delays based on it. At least one ' \
-              'target must be specified.'
+description = 'Track the first source above the horizon and calibrate ' \
+              'delays based on it. At least one target must be specified.'
 parser = standard_script_options(usage, description)
 # Add experiment-specific options
 parser.add_option('-t', '--track-duration', type='float', default=32.0,
                   help='Length of time to track the source for calibration, '
                        'in seconds (default=%default)')
 parser.add_option('--verify-duration', type='float', default=64.0,
-                  help='Length of time to revisit source for verification, '
+                  help='Length of time to revisit the source for verification, '
                        'in seconds (default=%default)')
-parser.add_option('--fengine-gain', type='int', default=0,
-                  help='Override correlator F-engine gain, using the default '
-                       'gain value for the mode if 0')
-parser.add_option('--fft-shift', type='int',
-                  help='Set correlator F-engine FFT shift (default=leave as is)')
+parser.add_option('--fengine-gain', type='int_or_default', default='default',
+                  help='Set correlator F-engine gain')
+parser.add_option('--fft-shift', type='int_or_default', default='default',
+                  help='Set correlator F-engine FFT shift')
 parser.add_option('--reset-delays', action='store_true', default=False,
-                  help='Zero the delay adjustments afterwards')
+                  help='Zero the delay adjustments afterwards (i.e. check only)')
 # Set default value for any option (both standard and experiment-specific options)
 parser.set_defaults(observer='comm_test', nd_params='off', project_id='COMMTEST',
-                    description='Delay calibration observation')
+                    description='Delay calibration observation that adjusts delays')
 # Parse the command line
 opts, args = parser.parse_args()
 
@@ -69,29 +62,17 @@ with verify_and_connect(opts) as kat:
         target = sources_above_horizon.targets[0]
         target.add_tags('bfcal single_accumulation')
         session.standard_setup(**vars(opts))
-        if opts.fft_shift is not None:
-            session.cbf.fengine.req.fft_shift(opts.fft_shift)
-        if opts.fengine_gain <= 0:
-            num_channels = session.cbf.fengine.sensor.n_chans.get_value()
-            try:
-                opts.fengine_gain = DEFAULT_GAIN[num_channels]
-            except KeyError:
-                raise KeyError("No default gain available for F-engine with "
-                               "%i channels - please specify --fengine-gain"
-                               % (num_channels,))
-        cal_inputs = session.get_cal_inputs()
-        gains = {inp: opts.fengine_gain for inp in cal_inputs}
-        delays = {inp: 0.0 for inp in cal_inputs}
-        session.set_fengine_gains(gains)
-        user_logger.info("Zeroing all delay adjustments for starters")
-        session.set_delays(delays)
+        session.set_fengine_fft_shift(opts.fft_shift)
+        session.set_fengine_gains(opts.fengine_gain)
+        session.adjust_fengine_delays(0)
         session.capture_init()
         user_logger.info("Only calling capture_start on correlator stream directly")
         session.cbf.correlator.req.capture_start()
         user_logger.info("Initiating %g-second track on target %r",
                          opts.track_duration, target.description)
         session.label('un_corrected')
-        session.track(target, duration=0)  # get onto the source
+        # Get onto the source
+        session.track(target, duration=0)
         # Fire noise diode during track
         session.fire_noise_diode(on=opts.track_duration, off=0)
         # Attempt to jiggle cal pipeline to drop its delay solutions
@@ -99,31 +80,20 @@ with verify_and_connect(opts) as kat:
         user_logger.info("Waiting for delays to materialise in cal pipeline")
         hv_delays = session.get_cal_solutions('KCROSS_DIODE', timeout=300.)
         delays = session.get_cal_solutions('K')
-        # Add hv_delay to total delay
-        for inp in sorted(delays):
+        # Add HV delay to total delay
+        for inp in delays:
             delays[inp] += hv_delays[inp]
-            if np.isnan(delays[inp]):
-                user_logger.warning("Delay fit failed on input %s (all its "
-                                    "data probably flagged)", inp)
-        # XXX Remove any NaNs due to failed fits (move this into set_delays)
-        delays = {inp: delay for inp, delay in delays.items()
-                  if not np.isnan(delay)}
-        if not delays and not kat.dry_run:
-            raise CalSolutionsUnavailable("No valid delay fits found "
-                                          "(is everything flagged?)")
-        session.set_delays(delays)
+        # The main course
+        session.adjust_fengine_delays(delays)
         if opts.verify_duration > 0:
             user_logger.info("Revisiting target %r for %g seconds "
                              "to see if delays are fixed",
                              target.name, opts.verify_duration)
             session.label('corrected')
-            session.track(target, duration=0)  # get onto the source
-            # Fire noise diode during track
+            session.track(target, duration=0)
             session.fire_noise_diode(on=opts.verify_duration, off=0)
         if opts.reset_delays:
-            user_logger.info("Zeroing all delay adjustments on CBF proxy")
-            delays = {inp: 0.0 for inp in delays}
-            session.set_delays(delays)
+            session.adjust_fengine_delays(0)
         else:
             # Set last-delay-calibration script sensor on the subarray.
             session.sub.req.set_script_param('script-last-delay-calibration',
