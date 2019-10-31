2082a8b7a2f35e1c704524600a74cb39b5c41ea0
Tiyani Baloyi <tbaloyi@ska.ac.za>
2019-10-31 07:27:14 +0200

    added a move indexer option prior to lowering the antennas

--- utility/flight_stow.py
+++ utility/flight_stow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Script to lower antennas in preparation for a flight arrival or departure from site
+# Script to lower antennas in preparation for flight arrivals and departures from site
 
 from katcorelib import (standard_script_options, verify_and_connect, user_logger)
 import time
@@ -14,14 +14,37 @@ def startAnts(ants, dry_run=False):
         except:
             user_logger.warn("not all antennas were in 'STOP' mode.")
 
-def point_ants(ants, dry_run=False):
+def moveRI(ants, ridx_pos, dry_run=False):
+    if ridx_pos not in ['u', 'l', 's', 'x']:
+        raise ValueError("indexer position {} does not exist. active bands are 'u', 'l', 's', and 'x'".format(ridx_pos))
+    user_logger.info("Moving receiver indexers to '{}' position".format(ridx_pos))
+    ants.set_sampling_strategy("ap.ridx-brakes-released","period 0.5")
+    ants.req.ap_set_indexer_position(ridx_pos)
+    time.sleep(10)
+    if not dry_run:
+        try:
+    	    # Wait for indexer brakes to engage again
+            ants.wait('ap.ridx-brakes-released', False, timeout=60)
+	    user_logger.info('{} position reached'.format(ridx_pos))
+	except:
+	    not_on_pos = []
+	    not_on_pos = [ant.name for ant in ants if ant.sensor.ap_indexer_position.get_value() != ridx_pos]
+            user_logger.error("Indexer brakes did not engage on: {}".format(', '.join(not_on_pos)))
+        finally:
+            # allow brekes to engage and put antennas to stop
+            time.sleep(3)
+	    ants.req.mode('STOP')
+            time.sleep(2)
+
+def pointAnts(ants, dry_run=False):
+    ants.set_sampling_strategy('lock', 'event')
     ant_diff = ['m057', 'm058', 'm059']
     for ant in ants:
 	if ant.name in ant_diff:
-	    user_logger.info('slewing %s to az: 30, el: 18' % ant.name)
+	    user_logger.info('slewing %s to azim: 30,   elev: 18' % ant.name)
 	    ant.req.target_azel(30, 18)
 	else:
-	    user_logger.info('slewing %s to az: 217, el: 18' % (ant.name))
+	    user_logger.info('slewing %s to azim: 217,  elev: 18' % (ant.name))
 	    ant.req.target_azel(217, 18)
 	ant.req.mode('POINT')
 
@@ -32,8 +55,8 @@ def point_ants(ants, dry_run=False):
 	except:
 	    not_locked = []
 	    not_locked = [ant.name for ant in ants if ant.sensor.lock.get_value() == False]
-	    user_logger.warn('{} did not reach target'.format(not_locked))
-	time.sleep(5)
+	    user_logger.warn('{} did not reach target'.format(', '.join(not_locked)))
+	time.sleep(3)
 	ants.req.mode('STOP')
 
 # Set up standard script options
@@ -41,17 +64,27 @@ usage = '%prog'
 description = 'receptor flight stow'
 parser = standard_script_options(usage, description)
 # Add experiment-specific options
+parser.add_option('--ridx-pos',
+                  type="string",
+                  default='x',
+                  help="move receiver indexers to specified position. default=%default")
+parser.add_option('--move-indexer',
+                  action="store_true",
+                  default= False,
+                  help="enables the 'move receiver' routine. default=%default")
 parser.set_defaults(observer='Tiyani', description='Receptor flight stow')
 # Parse the command line
 opts, args = parser.parse_args()
 
 # Check options and build KAT configuration, connecting to proxies and devices
 with verify_and_connect(opts) as kat:
-    user_logger.info('receptor flight stow START')
+    user_logger.info('Receptor flight stow START')
     # start antenna drives
     startAnts(kat.ants, dry_run=kat.dry_run)
 
     # point antennas
-    kat.ants.set_sampling_strategy('lock', 'event')
-    point_ants(kat.ants, dry_run=kat.dry_run)
-    user_logger.info('receptor flight stow completed')
+    # could be redundant code: kat.ants.set_sampling_strategy('lock', 'event')
+    if opts.move_indexer:
+        moveRI(kat.ants, opts.ridx_pos, dry_run=kat.dry_run)
+    pointAnts(kat.ants, dry_run=kat.dry_run)
+    user_logger.info('Receptor flight stow completed')
