554e89b96877aff559a526201ad4e392a9821f8f
pkotze <pkotze@ska.ac.za>
2019-11-01 13:42:26 +0200

    Modified Tsys reduction to allow single receptor processing

--- RTS/2.2-T_sys_T_nd/T_sys_T_nd_red.py
+++ RTS/2.2-T_sys_T_nd/T_sys_T_nd_red.py
@@ -2,8 +2,8 @@
 import matplotlib
 matplotlib.use('Agg')
 import argparse
-#from katsdpscripts.RTS import diodelib
-from katsdpscripts.reduction import diodelib
+#from katsdpscripts.RTS import diodeib
+import diodelib
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description=" This produces a pdf file with graphs verifying the ND model and Tsys for each antenna in the file")
@@ -14,23 +14,22 @@ def parse_arguments():
     parser.add_argument("--error_bars", action='store_true',default=False, help="Include error bars - Still in development")
     parser.add_argument("--off_target", default='off1', help="which of the two off targets to use")
     parser.add_argument("--write_nd", action='store_true', default=False, help="Write the Noise Diode temp to a file")
+    parser.add_argument("--rfi_mask",default="/var/kat/katsdpscripts/RTS/rfi_mask.pickle",help="Default is /var/kat/katsdpscripts/RTS/rfi_mask.pickle")
+    parser.add_argument("--receptor",default='',help="Receptor number to process e.g. 'm053', default is all")
     parser.add_argument("filename", nargs=1)
-    
+
     args,unknown = parser.parse_known_args()
 
     if args.filename[0] == '':
         raise RuntimeError('Please specify an h5 file to load.')
-    
+
     return args,unknown
 
 
 
 if __name__ == "__main__":
     args,unknown = parse_arguments()
-    print(unknown)
+    print unknown
     kwargs = dict(zip(unknown[0::2],unknown[1::2]))
-    diodelib.read_and_plot_data(args.filename[0],args.output_dir,args.pdf,args.Ku,args.verbose,args.error_bars,args.off_target,args.write_nd,**kwargs)
-
-
-
+    diodelib.read_and_plot_data(args.filename[0],args.output_dir,args.pdf,args.Ku,args.verbose,args.error_bars,args.off_target,args.write_nd,args.rfi_mask,args.receptor,**kwargs)
 
